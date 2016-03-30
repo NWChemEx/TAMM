8351427383adf34ff7fed1c1bded35760e2e4d7b
Nitin Gawande <gawa722@glogin1.(none)>
2016-03-30 15:17:21 -0700

    old work put in folder ctce_v6.5
    Former-commit-id: 16e549ab5c671935c3bf724298f21f35c3c3f69a
    Former-commit-id: fec3e243dc2b9f572eef0ece79e82190eadee7cf
    Former-commit-id: 7d846ead6af00726d6318ac92463f863a0313821
    Former-commit-id: b3223a0b2a728f4989dc4e4160043401674e75c2 [formerly ad303049db5b841bcf7a9eda249cd3f1c3ac9b9a]
    Former-commit-id: ce25002446bb866866b06faa7200b06291db5e54

--- ctce-paiwei/src/unpatch.sh
+++
@@ -1,5 +0,0 @@
-# restore original nwchem files
-cp ../src/tce/ccsd_t/ccsd_t.F.old ../src/tce/ccsd_t/ccsd_t.F
-cp ../src/tce/ccsd/ccsd_t1.F.old ../src/tce/ccsd/ccsd_t1.F
-cp ../src/tce/ccsd/ccsd_t2.F.old ../src/tce/ccsd/ccsd_t2.F
-

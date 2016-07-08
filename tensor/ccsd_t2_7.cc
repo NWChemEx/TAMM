4d34924d5379c79b52800cd4a2fab1dc0d1324cd
Nitin Gawande <gawa722@glogin2.(none)>
2016-07-07 17:00:04 -0700

    Modifications for ccsd_t2_7*

--- tensor/ccsd_t2_7.cc
+++ tensor/ccsd_t2_7.cc
@@ -8,7 +8,7 @@
 namespace ctce {
 
   static Assignment a_t2_7_1;
-  static Multiplication m_t2_7_2;
+  static Multiplication m_t2_7_2, m_t2_7_3, m_t2_7;
   static Tensor t2_7_1 = Tensor4(H6B,P3B,H1B,P5B,0,1,2,3,iV_tensor, dist_nw, dim_ov);;
 
   extern "C" {
@@ -23,15 +23,29 @@ namespace ctce {
         //std::cout << "set t2 7.\n";
 
         /* i1 ( h6 p3 h1 p5 )_v + = 1 * v ( h6 p3 h1 p5 )_v */
-        tC = Tensor4(P3B,H1B,P5B,H6B,0,1,2,3,iV_tensor, dist_nw, dim_ov);
+        tC = Tensor4(H6B,P3B,H1B,P5B,0,1,2,3,iV_tensor, dist_nw, dim_ov);
         tA = Tensor4(H6B,P3B,H1B,P5B,0,1,2,3,V_tensor, idist, dim_n);
-        a_t2_7_1 = Assignment(tC,tA,1.0, ivec(P3B,H1B,P5B,H6B), ivec(H6B,P3B,H1B,P5B));
+        a_t2_7_1 = Assignment(tC,tA,1.0, ivec(H6B,P3B,H1B,P5B), ivec(H6B,P3B,H1B,P5B));
 
         /* i1 ( h6 p3 h1 p5 )_vt + = -1 * Sum ( p7 ) * t ( p7 h1 )_t * v ( h6 p3 p5 p7 )_v */
-        tC = Tensor4(P3B,H1B,P5B,H6B,0,1,2,3,iVT_tensor, dist_nw, dim_ov);
+        tC = Tensor4(H6B,P3B,H1B,P5B,0,1,2,3,iVT_tensor, dist_nw, dim_ov);
         tA = Tensor2(P7B,H1B,0,1,T_tensor, dist_nwma, dim_ov);
+        //tA = Tensor2(P7B,H1B,0,1,T_tensor, idist, dim_n);
         tB = Tensor4(H6B,P3B,P5B,P7B,0,1,2,2,V_tensor, idist, dim_n);
         m_t2_7_2 = Multiplication(tC,tA,tB,-1.0);
+
+        /* i1 ( h6 p3 h1 p5 )_vt + = -1/2 * Sum ( h8 p7 ) * t ( p3 p7 h1 h8 )_t * v ( h6 h8 p5 p7 )_v */
+        tC = Tensor4(H6B,P3B,H1B,P5B,0,1,2,3,iVT_tensor, dist_nw, dim_ov);
+        tA = Tensor4(P3B,H1B,P7B,H8B,0,1,2,3,T_tensor, idist, dim_n);
+        tB = Tensor4(H6B,P5B,H8B,P7B,0,1,2,3,V_tensor, idist, dim_n);
+        m_t2_7_3 = Multiplication(tC,tA,tB,-0.50);
+
+        /* i0 ( p3 p4 h1 h2 )_vt + = -1 * P( 4 ) * Sum ( h6 p5 ) * t ( p3 p5 h1 h6 )_t * i1 ( h6 p4 h2 p5 )_v */
+        tC = Tensor4(H1B,P3B,H2B,P4B,0,1,2,3,iVT_tensor, dist_nw, dim_ov);
+        //tA = Tensor2(P7B,H1B,0,1,T_tensor, dist_nwma, dim_ov);
+        tA = Tensor4(H6B,P3B,H1B,P5B,0,1,2,3,T_tensor, idist, dim_n);
+        tB = Tensor4(H6B,P4B,H2B,P5B,0,1,2,3,V_tensor, idist, dim_n);
+        m_t2_7 = Multiplication(tC,tA,tB,-1.0);
 
         //ccsd_t2_7_1_createfile_cxx: i1 ( h6 p3 h1 p5 )_v
         t2_7_1 = Tensor4(H6B,P3B,H1B,P5B,0,1,2,3,iV_tensor, dist_nw, dim_ov);
@@ -73,6 +87,22 @@ namespace ctce {
       //Multiplication m = Multiplication(tC,tA,tB,-1.0);
       t_mult4(d_a, k_a_offset, d_b, k_b_offset, d_c, k_c_offset, m_t2_7_2);
     } // t2_7_2
+
+    /* i1 ( h6 p3 h1 p5 )_vt + = -1/2 * Sum ( h8 p7 ) * t ( p3 p7 h1 h8 )_t * v ( h6 h8 p5 p7 )_v */
+    void ccsd_t2_7_3_cxx_(Integer *d_a, Integer *k_a_offset,
+        Integer *d_b, Integer *k_b_offset, Integer *d_c, Integer *k_c_offset) {
+      std::cout << "ccsd_t2_7_3!" << std::endl;
+      t_mult4(d_a, k_a_offset, d_b, k_b_offset, d_c, k_c_offset, m_t2_7_3);
+    } // t2_7_3
+
+    /* i0 ( p3 p4 h1 h2 )_vt + = -1 * P( 4 ) * Sum ( h6 p5 ) * t ( p3 p5 h1 h6 )_t * i1 ( h6 p4 h2 p5 )_v */
+    void ccsd_t2_7_cxx_(Integer *d_a, Integer *k_a_offset,
+        Integer *d_b, Integer *k_b_offset, Integer *d_c, Integer *k_c_offset) {
+      t_mult4(d_a, k_a_offset, d_b, k_b_offset, d_c, k_c_offset, m_t2_7);
+    } // t2_7
+
+
+
 
   } // extern C
 }; // namespace ctce

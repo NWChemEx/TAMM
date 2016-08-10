fcd3cc45678399587770e22e461e23771f8a3145
Sriram Krishnamoorthy <sriram@pnnl.gov>
2016-08-10 00:33:14 -0700

    timers
    Former-commit-id: 908938e452fcdce21cf650429531e69558a55f0f
    Former-commit-id: a93fce8c95aa47bd08c0dcfb1a2bbb3264dbca24
    Former-commit-id: 9ed59c916010d7437218a6347bfd1d70184ba1d7
    Former-commit-id: eb4a2a27ba8157cadd4810ebac7410cc42b1d614 [formerly bdefa354871be31dd90ac5bc8e6c57cb95a948dc]
    Former-commit-id: 5bf1c3e8322fe52610711780f9f7a58928b0197d

---
+++ tensor/stats.cc
@@ -0,0 +1,18 @@
+#include "stats.h"
+#include <iostream>
+
+using namespace std;
+using namespace ctce;
+
+namespace ctce {
+  Profiler iterTimer, assignTimer, multTimer, getTimer, addTimer, dgemmTimer;
+
+  void printStats() {
+    cout<<"Iter time ="<<iterTimer.time()<<endl;
+    cout<<"Assign time ="<<assignTimer.time()<<endl;
+    cout<<"Mult time ="<<multTimer.time()<<endl;
+    cout<<"get time ="<<getTimer.time()<<endl;
+    cout<<"add time ="<<addTimer.time()<<endl;
+    cout<<"dgemm time ="<<dgemmTimer.time()<<endl;
+  }
+}
---
+++ tensor/stats.h
@@ -0,0 +1,58 @@
+#ifndef __ctce_stats_h__
+#define __ctce_stats_h__
+
+#include <cassert>
+#include <mpi.h>
+#include <string>
+
+#define TIMER() MPI_Wtime()
+
+namespace ctce {
+
+  class Profiler {
+  public:
+    Profiler() {
+      cnt = 0;
+      ttime = 0;
+      in_phase = false;
+    }
+
+    void start() {
+      stime = TIMER();
+      assert(!in_phase);
+      in_phase = true;
+    }
+
+    void stop() {
+      etime = TIMER();
+      assert(in_phase);
+      cnt += 1;
+      ttime += etime - stime;
+      in_phase = false;
+    }
+
+    ~Profiler() {
+      assert(!in_phase);
+    }
+
+    int count() {
+      return cnt;
+    }
+    
+    double time() {
+      return ttime;
+    }
+
+  private:
+    int cnt;
+    double ttime;
+    double stime, etime;
+    bool in_phase;
+  };
+
+  extern Profiler iterTimer, assignTimer, multTimer, getTimer, addTimer, dgemmTimer;
+  void printStats();
+}
+
+#endif /* __ctce_stats_h__ */
+

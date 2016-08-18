265228a290e441a6b49c91670974c8f52410f865
Sriram Krishnamoorthy <sriram@pnnl.gov>
2016-08-18 11:08:11 -0700

    cleanup
    Former-commit-id: 39c2a4125970241289c899ebcef4fda6c0dcd750
    Former-commit-id: bda50b806c66458cc92ab3ea7b4c8f8ae0dfd40b
    Former-commit-id: 93b60bb56418fadece4f3a9509285dc92c034f52
    Former-commit-id: f961feccb1ab4105c48c4f5984567792bb16b210 [formerly ec17e260e4e46d2830337dbee2eeea99ae17303c]
    Former-commit-id: ea283e2225b795fc9a3fe3f7d53f9e80114e5b91

--- tensor/capi.h
+++ tensor/capi.h
@@ -1,52 +1,77 @@
-#ifndef __tce_capi_h__
-#define __tce_capi_h__
+#ifndef __ctce_capi_h__
+#define __ctce_capi_h__
 
-#include "fapi.h"
-#include "typesf2c.h"
-#include "index.h"
-#include "variables.h"
+#include "common.h"
 #include <vector>
-#include <iostream>
+
+/**
+ * C/C++ wrappers to Fortran routines invoked by ctce
+ */
 
 namespace ctce {
 
-  /**
-   * CXX function that wraps FORTRAN tce_sort2 and tce_sort4
-   */
-  void tce_sort(double *sbuf, double *dbuf, const std::vector<size_t>& ids,
-      std::vector<size_t>& iv, double alpha);
+/**
+ * C++ function that wraps FORTRAN tce_sort2 and tce_sort4
+ */
+void
+tce_sort(double *sbuf, double *dbuf, const std::vector<size_t>& ids,
+         std::vector<size_t>& iv, double alpha);
 
-  /**
-   * CXX function that wraps FORTRAN fdgemm
-   */
-  void cdgemm(char transa, char transb, size_t m, size_t n, size_t k,
-      double alpha, double *a, size_t lda, double *b, size_t ldb, 
-      double beta, double *c, size_t ldc);
+/**
+ * C++ function that wraps FORTRAN dgemm
+ */
+void
+cdgemm(char transa, char transb, size_t m, size_t n, size_t k,
+       double alpha, double *a, size_t lda, double *b, size_t ldb,
+       double beta, double *c, size_t ldc);
 
-  /**
-   * CXX function that wraps FORTRAN add_hash_block
-   */
-  /* void tce_add_hash_block_(size_t *d_c,double *buf_a,size_t size,size_t k_c_offset,  */
-  /*     const std::vector<size_t>& is, const std::vector<IndexName>& ns); */
+/**
+ * C++ function that wraps FORTRAN add_hash_block
+ */
+void
+cadd_hash_block(size_t d_c, double *buf_a, size_t size,
+                Fint *hash, size_t key);
 
-  void cadd_hash_block(size_t d_c, double *buf_a, size_t size, Integer *hash, size_t key);
+
+/**
+ * C++ function that wraps FORTRAN tce_sortacc*
+ */
+void
+sortacc(double *sbuf, double *dbuf, const std::vector<size_t>& ids,
+        std::vector<size_t>& perm, double alpha);
 
 
-  /**
-   * CXX function that wraps FORTRAN tce_sortacc6
-   */
-  void sortacc(double *sbuf, double *dbuf, const std::vector<size_t>& ids, 
-      std::vector<size_t>& perm, double alpha);
+/**
+ * C++ function that wraps FORTRAN tce_restricted
+ */
+void
+ctce_restricted(int dim, int nupper,
+                const std::vector<size_t> &value,
+                std::vector<size_t> &pvalue_r);
 
-  void ctce_restricted(int dim, int nupper,
-                       const std::vector<size_t> &value,
-                       std::vector<size_t> &pvalue_r);
+/**
+ * C++ function that wraps FORTRAN get_hash_block_i
+ */
+void
+cget_hash_block_i(size_t d_a, double *buf, size_t size,
+                  size_t d_a_offset,
+                  size_t key, std::vector<size_t> &is);
 
-  void cget_hash_block_i(size_t d_a, double *buf, size_t size, size_t d_a_offset,
-                         size_t key, std::vector<size_t> &is);
-  void cget_hash_block_ma(size_t d_a, double *buf, size_t size, size_t d_a_offset, size_t key);
-  void cget_hash_block(size_t d_a, double *buf, size_t size, size_t d_a_offset, size_t key);
+/**
+ * C++ function that wraps FORTRAN get_hash_block_ma
+ */
+void
+cget_hash_block_ma(size_t d_a, double *buf, size_t size,
+                   size_t d_a_offset, size_t key);
 
-}; // namespace ctce
+/**
+ * C++ function that wraps FORTRAN get_hash_block
+ */
+void
+cget_hash_block(size_t d_a, double *buf, size_t size,
+                size_t d_a_offset, size_t key);
 
-#endif
+} /*namespace ctce*/
+
+#endif /*__ctce_capi_h__*/
+

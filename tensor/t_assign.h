d3c2b1764cc7f8550f60b07d083add02e90d4b82
Sriram Krishnamoorthy <sriram@pnnl.gov>
2016-08-18 16:08:51 -0700

    cleanup

--- tensor/t_assign.h
+++ tensor/t_assign.h
@@ -1,5 +1,5 @@
-#ifndef _ASSIGN_H_
-#define _ASSIGN_H_
+#ifndef __ctce_assign_h__
+#define __ctce_assign_h__
 
 #include "func.h"
 #include "preprocess.h"
@@ -9,35 +9,25 @@
 #include "ga.h"
 
 namespace ctce {
+/**
+ * CCSD assignment computation, ADD_HASH_BLOCK
+ * @param[in] d_a, k_a_offset, d_c, k_c_offset from FORTRAN
+ * @param[in] tC += tA * coef
+ * @param[in] out_itr outer iterator group
+ */
+void t_assign2(
+  Tensor& tC, const vector<IndexName> &c_ids,
+  Tensor& tA, const vector<IndexName> &a_ids,
+  IterGroup<triangular>& out_itr, double coef,
+  int sync_ga=0, int spos=0);
 
-  extern "C" {
+/**
+ * Simply wrap t_assign2
+ * @param[in] d_a, k_a_offset, d_c, k_c_offset from FORTRAN
+ * @param[in] a Assignment store all the input data needed for t_assign2
+ */
+void t_assign3(Assignment& a, int sync_ga=0, int spos=0);
 
-    /**
-     * CCSD assignment computation, ADD_HASH_BLOCK
-     * @param[in] d_a, k_a_offset, d_c, k_c_offset from FORTRAN
-     * @param[in] tC += tA * coef
-     * @param[in] out_itr outer iterator group
-     */
-    void t_assign2(
-        Tensor& tC, const vector<IndexName> &c_ids,
-				Tensor& tA, const vector<IndexName> &a_ids,
-				IterGroup<triangular>& out_itr, double coef,
-        int sync_ga=0, int spos=0);
-
-    /**
-     * Simply wrap t_assign2
-     * @param[in] d_a, k_a_offset, d_c, k_c_offset from FORTRAN
-     * @param[in] a Assignment store all the input data needed for t_assign2
-     */
-    /* void t_assign3(Integer* d_a, Integer* k_a_offset, */
-    /*                Integer* d_c, Integer* k_c_offset, Assignment& a, int sync_ga=0, int spos=0); */
-    void t_assign3(Assignment& a, int sync_ga=0, int spos=0);
-
-    /* void t_assign4(Integer* d_a, Integer* k_a_offset, */
-    /*     Integer* d_c, Integer* k_c_offset, Assignment& a); */
-
-  }
-
-}; // namespace ctce
+} // namespace ctce
 
 #endif

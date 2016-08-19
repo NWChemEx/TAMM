3818da228d09ac3679af11aa1aefbf388bc1d5f1
Sriram Krishnamoorthy <sriram@pnnl.gov>
2016-08-18 22:46:01 -0700

    some cleanup and refactoring
    Former-commit-id: c06c777f1b52d938c340c20b458cd2d9676fd184
    Former-commit-id: cd82ebffb3909de8181b2e3a12de2df1c85fba65
    Former-commit-id: da55fcb34c7cfda84800e4eb1897937af6b02e3c
    Former-commit-id: 05ff966a1d4ff3a841c636316b95fe3070c3093e [formerly 5b275515767936f400d91b3cfcf996611bccd803]
    Former-commit-id: 6682c3355dc817d77ffac129bfa4afa2d36a5ebc

--- tensor/index.h
+++ tensor/index.h
@@ -1,82 +1,90 @@
 #ifndef __ctce_index_h__
 #define __ctce_index_h__
 
-#include "typesf2c.h"
+#include "common.h"
+//#include "typesf2c.h"
 #include "define.h"
 
 namespace ctce {
 
-  class Index {
+/**
+ * @FIXME @BUG: ext_sym_group_ and probably this whole class needs to
+ * be removed.
+ */
 
-    private:
-      IndexName name_;  /*< name of this index */
-      Integer value_;     /*< value of this index */
-      Integer value_r_;   /*< value(restricted) of this index */
-      int ext_sym_group_; /*< external symmetry group of this index, from lhs of the expr (tC) */
+class Index {
+public:
+  /**
+   * Constructor
+   */
+  Index() {}
 
-    public:
-      /**
-       * Constructor
-       */
-      Index() {};
+  /**
+   * Constructor. Assign data to this index.
+   * @param[in] name Name of the index
+   * @param[in] range Range of the index
+   * @param[in] esg External symmetry group of the index
+   */
+  Index(const IndexName& name, const int& esg)
+    : name_(name),
+      value_(0),
+      value_r_(0),
+      ext_sym_group_(esg) {}
 
-      /**
-       * Constructor. Assign data to this index.
-       * @param[in] name Name of the index
-       * @param[in] range Range of the index
-       * @param[in] esg External symmetry group of the index
-       */
-      Index(const IndexName& name, const int& esg) 
-        : name_(name),
-        value_(0), 
-        value_r_(0), 
-        ext_sym_group_(esg) {}
+  /**
+   * Destructor
+   */
+  ~Index() {}
 
-      /**
-       * Destructor
-       */
-      ~Index() {};
+  /**
+   * Get the name of the index
+   * @return name as a string
+   */
+  inline IndexName name() const { return name_; }
 
-      /**
-       * Get the name of the index
-       * @return name as a string
-       */
-      inline const IndexName& name() const { return name_; }
+  /**
+   * Get the value of the index
+   * @return value as a Fint
+   */
+  inline Fint value() const { return value_; }
 
-      /**
-       * Get the value of the index
-       * @return value as a Integer
-       */
-      inline const Integer& value() const { return value_; }
+  /**
+   * Get the value(restricted) of the index
+   * @return value_r as an Fint
+   */
+  inline Fint value_r() const { return value_r_; }
 
-      /**
-       * Get the value(restricted) of the index
-       * @return value_r as an Integer
-       */
-      inline const Integer& value_r() const { return value_r_; }
+  /**
+   * Set the value of this index
+   * @param[in] val value of this index
+   */
+  inline void setValue(const Fint& val) { value_=val; }
 
-      /**
-       * Get the external symmetry group id of the index
-       * @return ext_sym_group as an int
-       */
-      inline const int& ext_sym_group() const { return ext_sym_group_; }
+  /**
+   * Set the restricted value of this index
+   * @param[in] val_r restricted value of this index
+   */
+  inline void setValueR(const Fint& val_r) { value_r_=val_r; }
 
-      /**
-       * Set the value of this index
-       * @param[in] val value of this index
-       */
-      inline void setValue(const Integer& val) { value_=val; }
+private:
+  /**
+   * Get the external symmetry group id of the index
+   * @return ext_sym_group as an int
+   */
+  inline int ext_sym_group() const { return ext_sym_group_; }
 
-      /**
-       * Set the restricted value of this index
-       * @param[in] val_r restricted value of this index
-       */
-      inline void setValueR(const Integer& val_r) { value_r_=val_r; }
+  IndexName name_;  /*< name of this index */
+  Fint value_;     /*< value of this index */
+  Fint value_r_;   /*< value(restricted) of this index */
+  int ext_sym_group_; /*< external symmetry group of this index, from lhs of the expr (tC) */
 
-  };
+  friend
+  bool compareExtSymGroup(const Index& lhs, const Index& rhs);
+};
 
-  inline bool compareValue(const Index& lhs, const Index& rhs) { return (lhs.value() < rhs.value()); }
-  inline bool compareExtSymGroup(const Index& lhs, const Index& rhs) { return (lhs.ext_sym_group() < rhs.ext_sym_group()); }
+inline bool compareValue(const Index& lhs, const Index& rhs) { return (lhs.value() < rhs.value()); }
+inline bool compareExtSymGroup(const Index& lhs, const Index& rhs) { return (lhs.ext_sym_group() < rhs.ext_sym_group()); }
+
 
 } /* namespace ctce */
 

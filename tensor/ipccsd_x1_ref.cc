807945c0212295facc85f19156d5de14fbe2335e
Sriram Krishnamoorthy <sriram@pnnl.gov>
2016-09-11 22:41:53 -0700

    matching ipccsd x1 ref with eq file
    Former-commit-id: f47800144513403c4f6742957d977728ad758b8a
    Former-commit-id: 62aa3227bc5b12ff33e25e75935ef8fbddbfc210
    Former-commit-id: 23b0d8e5c26281f9d622652f2fddfc4ef5d2600d
    Former-commit-id: 3966df5a0582f5404bafc09176a97e96bdfb30df [formerly b7c14b644001dc24e3513f1a783de267832db2e0]
    Former-commit-id: 85c0d54a298b02a71f5e7d27a1dbd155e4ed2594

--- tensor/ipccsd_x1_ref.cc
+++ tensor/ipccsd_x1_ref.cc
@@ -108,17 +108,17 @@ void ipccsd_x1_cxx_(Fint *d_f1, Fint *d_i0, Fint *d_t1, Fint *d_t2, Fint *d_v2,
   std::vector <Operation> ops;
   tensors_and_ops(eqs, tensors, ops);
 
-  Tensor *x1_1_1 = &tensors[0];
-  Tensor *f = &tensors[1];
-  Tensor *x1_1_2_1 = &tensors[2];
+  Tensor *i0 = &tensors[0];
+  Tensor *x_o = &tensors[1];
+  Tensor *f = &tensors[2];
   Tensor *t1 = &tensors[3];
   Tensor *v = &tensors[4];
   Tensor *t2 = &tensors[5];
-  Tensor *i0 = &tensors[6];
-  Tensor *x_o = &tensors[7];
-  Tensor *x1_2_1 = &tensors[8];
-  Tensor *x_voo = &tensors[9];
-  Tensor *x1_3_1 = &tensors[10];
+  Tensor *x_voo = &tensors[6];
+  Tensor *x1_2_1 = &tensors[7];
+  Tensor *x1_1_2_1 = &tensors[8];
+  Tensor *x1_3_1 = &tensors[9];
+  Tensor *x1_1_1 = &tensors[10];
 
   v->set_dist(idist);
   t1->set_dist(dist_nw);
@@ -152,6 +152,7 @@ void ipccsd_x1_cxx_(Fint *d_f1, Fint *d_i0, Fint *d_t1, Fint *d_t2, Fint *d_v2,
   op_x1_3_2 = ops[11].mult;
   op_x1_3 = ops[12].mult;
 
+#if 0
   CorFortran(1, x1_1_1, offset_ipccsd_x1_1_1_);
   CorFortran(1, op_x1_1_1, ipccsd_x1_1_1_);
   CorFortran(1, x1_1_2_1, offset_ipccsd_x1_1_2_1_);
@@ -173,6 +174,29 @@ void ipccsd_x1_cxx_(Fint *d_f1, Fint *d_i0, Fint *d_t1, Fint *d_t2, Fint *d_v2,
   CorFortran(1, op_x1_3_2, ipccsd_x1_3_2_);
   CorFortran(0, op_x1_3, ipccsd_x1_3_); /*@BUG @FIXME Does not work in C mode*/
   destroy(x1_3_1);
+#else
+  CorFortran(0, x1_1_1, offset_ipccsd_x1_1_1_);
+  CorFortran(0, op_x1_1_1, ipccsd_x1_1_1_);
+  CorFortran(0, x1_1_2_1, offset_ipccsd_x1_1_2_1_);
+  CorFortran(0, op_x1_1_2_1, ipccsd_x1_1_2_1_);
+  CorFortran(0, op_x1_1_2_2, ipccsd_x1_1_2_2_);
+  CorFortran(0, op_x1_1_2, ipccsd_x1_1_2_);
+  destroy(x1_1_2_1);
+  CorFortran(0, op_x1_1_3, ipccsd_x1_1_3_);
+  CorFortran(0, op_x1_1_4, ipccsd_x1_1_4_);
+  CorFortran(0, op_x1_1, ipccsd_x1_1_);
+  destroy(x1_1_1);
+  CorFortran(0, x1_2_1, offset_ipccsd_x1_2_1_);
+  CorFortran(0, op_x1_2_1, ipccsd_x1_2_1_);
+  CorFortran(0, op_x1_2_2, ipccsd_x1_2_2_);
+  CorFortran(0, op_x1_2, ipccsd_x1_2_); /*@BUG @FIXME Does not work in C mode*/
+  destroy(x1_2_1);
+  CorFortran(0, x1_3_1, offset_ipccsd_x1_3_1_);
+  CorFortran(0, op_x1_3_1, ipccsd_x1_3_1_);
+  CorFortran(0, op_x1_3_2, ipccsd_x1_3_2_);
+  CorFortran(0, op_x1_3, ipccsd_x1_3_); /*@BUG @FIXME Does not work in C mode*/
+  destroy(x1_3_1);
+#endif
 #endif
 
   f->detach();

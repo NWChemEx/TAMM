66ffe226589705eb48ad9822ceeab76a6bf83d1b
Joshua Landwehr <joshua.landwehr@pnnl.gov>
2016-09-21 13:18:11 -0700

    1) Tabbing to spaces for ga_abstract stuff
    Former-commit-id: e8b41cdd8b196194f963f7935aa739ea12c98f7f
    Former-commit-id: e6c9e6bab49f40a5833069aebff08585c00d675c
    Former-commit-id: 763efa7a9f8061eea83e59f6e81505efbd59aa54
    Former-commit-id: 37989e102f8d3359d1feeddcd17623cba1b8d238 [formerly 35eabc2edb0ff891432a70e6474379e5055e10cc]
    Former-commit-id: 0b0ff9ff885c7a9c526a710c374ff691e15a9f32

--- tensor/ga_abstract.h
+++ tensor/ga_abstract.h
@@ -7,19 +7,19 @@ using namespace std;
 
 namespace ctce {
 extern "C" {
-	int nga_Create(int type, int ndim, int dims[], char *array_name, int chunk[]);
-	long nga_Read_inc(int id, int subscript[], long inc);
-	void nga_Zero(int id);
-	void nga_Destroy(int id);
-	void nga_Acc(int id, int lo[], int hi[], void* buf, int ld[], void* alpha);
-	void nga_Get(int id, int lo[], int hi[], void* buf, int ld[]);
-	void nga_NbGet(int id, int lo[], int hi[], void* buf, int ld[], ga_nbhdl_t* nbhandle);
-	void nga_NbWait(ga_nbhdl_t* nbhandle);
-	int ga_Nnodes();
-	void ga_Dgop(double x[], int n, char *op);
-	void ga_Zero(int id);
-	void ga_Sync();
-	void ga_Destroy(int id);
+  int nga_Create(int type, int ndim, int dims[], char *array_name, int chunk[]);
+  long nga_Read_inc(int id, int subscript[], long inc);
+  void nga_Zero(int id);
+  void nga_Destroy(int id);
+  void nga_Acc(int id, int lo[], int hi[], void* buf, int ld[], void* alpha);
+  void nga_Get(int id, int lo[], int hi[], void* buf, int ld[]);
+  void nga_NbGet(int id, int lo[], int hi[], void* buf, int ld[], ga_nbhdl_t* nbhandle);
+  void nga_NbWait(ga_nbhdl_t* nbhandle);
+  int ga_Nnodes();
+  void ga_Dgop(double x[], int n, char *op);
+  void ga_Zero(int id);
+  void ga_Sync();
+  void ga_Destroy(int id);
 }
 }
 

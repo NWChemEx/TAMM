14023dae13b643f153d937ab96f969b3e6a1ad2a
Ajay Panyala <ajay.panyala@gmail.com>
2016-11-12 15:26:17 -0800

    replace printf with cout
    Former-commit-id: faef439f2445bcb486b3d0d05835a0e9cc10de16
    Former-commit-id: 5eddf47b96be92a882b5902c040bfdf0c6cbcb25
    Former-commit-id: 90645fdbcd479826b043f2a3a614597bf712d796
    Former-commit-id: 14eaf5b77f61e514e881249904a3091d9bf56a13 [formerly a4ef0b0737dac501cf57f5c9be1bbccabb11746e]
    Former-commit-id: 3e6ae6de2f3aa87542238aff7de70d9342556c23

--- tamm_parser/test.cc
+++ tamm_parser/test.cc
@@ -19,69 +19,69 @@ int main(int argc, char **argv) {
 
     unsigned int i = 0;
     RangeEntry rent;
-    printf("\nRANGE ENTRIES... \n");
+    std::cout << "\nRANGE ENTRIES... \n";
     for (i = 0; i < genEq.range_entries.size(); i++) {
         rent = (RangeEntry) genEq.range_entries.at(i);
-        printf("At position %d -> %s\n", i, rent->name);
+        std::cout << "At position " << i << " -> " <<  rent->name << std::endl;
     }
 
     IndexEntry ient;
-    printf("\nINDEX ENTRIES... \n");
+    std::cout << "\nINDEX ENTRIES... \n";
     for (i = 0; i < genEq.index_entries.size(); i++) {
         ient = (IndexEntry) genEq.index_entries.at(i);
-        printf("At position %d -> %s %d\n", i, ient->name, ient->range_id);
+        std::cout << "At position " << i << " -> " <<  ient->name << " " << ient->range_id << std::endl;
     }
 
-    printf("\nTENSOR ENTRIES... \n");
+    std::cout << "\nTENSOR ENTRIES... \n";
     TensorEntry tent;
     unsigned int j = 0;
     for (i = 0; i < genEq.tensor_entries.size(); i++) {
         tent = (TensorEntry) genEq.tensor_entries.at(i);
-        printf("At position %d -> {%s, {", i, tent->name);
+        std::cout << "At position " << i << " -> {" << tent->name << ", {";
         for (j = 0; j < tent->ndim; j++) {
-            if (tent->range_ids[j] == 0) printf("O,");
-            if (tent->range_ids[j] == 1) printf("V,");
-            if (tent->range_ids[j] == 2) printf("N,");
+            if (tent->range_ids[j] == 0) std::cout << "O,";
+            if (tent->range_ids[j] == 1) std::cout << "V,";
+            if (tent->range_ids[j] == 2) std::cout << "N,";
         }
-        printf("}, %d, ", tent->ndim);
-        printf("%d}\n", tent->nupper);
+        std::cout << "}, " << tent->ndim << ", ";
+        std::cout << tent->nupper << "}\n";
     }
 
-    printf("\nOP ENTRIES... \n");
+    std::cout << "\nOP ENTRIES... \n";
     OpEntry oent;
     std::vector<TensorEntry> &tensor_entries = genEq.tensor_entries;
     std::vector<IndexEntry> &index_entries = genEq.index_entries;
 
     for (i = 0; i < genEq.op_entries.size(); i++) {
         oent = (OpEntry) genEq.op_entries.at(i);
-        if (oent->optype == OpTypeAdd) printf("op%d: OpTypeAdd, ", oent->op_id);
-        else printf("op%d: OpTypeMult, ", oent->op_id);
+        if (oent->optype == OpTypeAdd) std::cout << "op" << oent->op_id << ": OpTypeAdd, ";
+        else std::cout << "op" << oent->op_id << ": OpTypeMult, ";
         unsigned int j;
+        std::cout << std::fixed;
         if (oent->add != nullptr) {
-            printf("%s, %s, %lf, {", getTensorName(tensor_entries, oent->add->tc),
-                   getTensorName(tensor_entries, oent->add->ta), oent->add->alpha);
+            std::cout << getTensorName(tensor_entries, oent->add->tc) << ", " << getTensorName(tensor_entries, oent->add->ta)
+                                                                              << ", " << oent->add->alpha << ", {";
             for (j = 0; j < MAX_TENSOR_DIMS; j++)
-                if (oent->add->tc_ids[j] != -1) printf("%s,", getIndexName(index_entries, oent->add->tc_ids[j]));
-            printf("}, {");
+                if (oent->add->tc_ids[j] != -1) std::cout << getIndexName(index_entries, oent->add->tc_ids[j]) << ",";
+            std::cout << "}, {";
             for (j = 0; j < MAX_TENSOR_DIMS; j++)
-                if (oent->add->ta_ids[j] != -1) printf("%s,", getIndexName(index_entries, oent->add->ta_ids[j]));
-            printf("}");
+                if (oent->add->ta_ids[j] != -1) std::cout << getIndexName(index_entries, oent->add->ta_ids[j]) << ",";
+            std::cout << "}";
         }
         else if (oent->mult != nullptr) {
-            printf("%s, %s, %s, %lf, {", getTensorName(tensor_entries, oent->mult->tc),
-                   getTensorName(tensor_entries, oent->mult->ta), getTensorName(tensor_entries, oent->mult->tb),
-                   oent->mult->alpha);
+            std::cout << getTensorName(tensor_entries, oent->mult->tc) << ", " << getTensorName(tensor_entries, oent->mult->ta)
+                    << ", " << getTensorName(tensor_entries, oent->mult->tb) << ", " << oent->mult->alpha << ", {";
             for (j = 0; j < MAX_TENSOR_DIMS; j++)
-                if (oent->mult->tc_ids[j] != -1) printf("%s,", getIndexName(index_entries, oent->mult->tc_ids[j]));
-            printf("}, {");
+                if (oent->mult->tc_ids[j] != -1) std::cout << getIndexName(index_entries, oent->mult->tc_ids[j]) << ",";
+            std::cout << "}, {";
             for (j = 0; j < MAX_TENSOR_DIMS; j++)
-                if (oent->mult->ta_ids[j] != -1) printf("%s,", getIndexName(index_entries, oent->mult->ta_ids[j]));
-            printf("}, {");
+                if (oent->mult->ta_ids[j] != -1) std::cout << getIndexName(index_entries, oent->mult->ta_ids[j]) << ",";
+            std::cout << "}, {";
             for (j = 0; j < MAX_TENSOR_DIMS; j++)
-                if (oent->mult->tb_ids[j] != -1) printf("%s,", getIndexName(index_entries, oent->mult->tb_ids[j]));
-            printf("}");
+                if (oent->mult->tb_ids[j] != -1) std::cout << getIndexName(index_entries, oent->mult->tb_ids[j]) << ",";
+            std::cout << "}";
         }
-        printf("\n");
+        std::cout << std::endl;
     }
 
     return 0;

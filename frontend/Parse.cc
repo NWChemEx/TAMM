7123ae664d09c2b6464cf41f0e93d1a2503377b4
Ajay Panyala <ajay.panyala@gmail.com>
2017-03-30 22:25:36 -0700

    Add Better syntax error reporting

--- frontend/Parse.cc
+++ frontend/Parse.cc
@@ -10,12 +10,53 @@
 //------------------------------------------------------------------------------
 
 #include "Parse.h"
+#include <regex>
 #include "Intermediate.h"
 #include "Semant.h"
 
 namespace tamm {
 
 namespace frontend {
+
+class TAMMErrorListener : public BaseErrorListener {
+ public:
+  void syntaxError(Recognizer *recognizer, Token *offendingSymbol, size_t line,
+                   size_t charPositionInLine, const std::string &msg,
+                   std::exception_ptr e) {
+    // std::vector<std::string> stack =
+    //    ((Parser *)recognizer)->getRuleInvocationStack();
+    // std::reverse(stack.begin(), stack.end());
+    // std::cout << "rule stack: ";
+    // for (auto &x: stack)  std::cout << x << ",";
+    // std::cout << std::endl;
+
+    std::cout << "Syntax error at line " << line << ":" << charPositionInLine 
+              //<< " at " << offendingSymbol->getText()
+               << ": " << msg << std::endl;
+
+    underlineError(recognizer, offendingSymbol, line, charPositionInLine);
+  }
+
+  void underlineError(Recognizer *recognizer, Token *offendingToken,
+                      size_t line, size_t charPositionInLine) {
+    CommonTokenStream *tokens =
+        ((CommonTokenStream *)(recognizer)->getInputStream());
+    const std::string input = tokens->getTokenSource()->getInputStream()->toString();
+    std::regex rnewline = std::regex("\n");
+    const std::vector<std::string> lines{
+        std::sregex_token_iterator(input.begin(), input.end(), rnewline, -1),
+        std::sregex_token_iterator()};
+    const std::string errorLine = lines[line - 1];
+    std::cout << errorLine << std::endl;
+    for (int i = 0; i < charPositionInLine; i++) std::cout << " ";
+    const int start = offendingToken->getStartIndex();
+    const int stop = offendingToken->getStopIndex();
+    if (start >= 0 && stop >= 0) {
+      for (int i = start; i <= stop; i++) std::cout << "^";
+    }
+    std::cout << std::endl;
+  }
+};
 
 void tamm_frontend(const std::string input_file,
                    Equations *const tamm_equations) {
@@ -25,6 +66,8 @@ void tamm_frontend(const std::string input_file,
   TAMMLexer lexer(&tamminput);
   CommonTokenStream tokens(&lexer);
   TAMMParser parser(&tokens);
+  parser.removeErrorListeners();
+  parser.addErrorListener(new TAMMErrorListener());
 
   tree::ParseTree *tree = parser.translation_unit();
 

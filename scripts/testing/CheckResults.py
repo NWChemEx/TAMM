9dd69f696f1583808f3421efcbf4cedc7757d441
Ajay Panyala <ajay.panyala@gmail.com>
2017-03-31 16:07:28 -0700

    Add python script and a json spec file for checking results of tamm runs
    Former-commit-id: 2ecc227c676947217d9c020b7532ecbde9e93c01
    Former-commit-id: fec5b7397a1c3fc77379074ccf6ce1a14b6aa7fe
    Former-commit-id: ce58df8ac4b16581e57dd8b0d208ab1ee5948352
    Former-commit-id: 3873d12dd2323ba471a191a90f2a66c931273dbc [formerly 7a4d4915db6b284c2a7295850b40b5809d2b3b8c]
    Former-commit-id: d30e69243f641cbe061b6de4e77af613d70b2212

---
+++ scripts/testing/CheckResults.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+# Script to check results produced by TAMM.
+
+import sys
+import os
+import json
+import math
+import subprocess
+
+testcase = str(sys.argv[1])
+num_procs = int(sys.argv[2])
+
+def check_result_file(res_file,result_string, num_lines):
+    get_results = []
+    with open(res_file, "r") as outf:
+        for line in outf:
+            if line.strip().startswith(result_string):
+                if num_lines[0] == "=":
+                    tvalue = line.split("=")[1]
+                    get_results.append(tvalue.strip())
+                else:  # Assume number for now
+                    nl = num_lines[0]
+                    lc = 0
+                    while lc != nl:
+                        next_line = next(outf)
+                        next_line = next_line.strip()
+                        if next_line.startswith(str(lc+1) + " "):
+                            residuum = ' '.join(next_line.split())
+                            residuum = residuum.split(" ")[1]
+                            get_results.append(residuum.strip())
+                            lc += 1
+                    assert len(get_results) == nl
+    return get_results
+
+
+
+tamm_root = os.path.dirname(os.path.abspath(__file__)) + "/../.."
+
+with open(tamm_root+"/CI_testing/integration/test.json") as json_data_file:
+    data = json.load(json_data_file)
+
+
+num_tests = len(data)
+num_failed = 0
+input_folder = tamm_root+"/CI_testing/integration/"
+
+#for testcase, patterns in data.iteritems():
+patterns = data[testcase]
+print "Running Test: " + testcase + ".nw"
+nwinput = input_folder + "/inputs/" + testcase + ".nw"
+tamm_output = testcase + ".tammout"
+
+run_tamm_code = "mpirun -n " + str(num_procs) + " " + os.environ['NWCHEM_TOP'] + "/bin/LINUX64/nwchem " + nwinput
+print run_tamm_code
+tof = open(tamm_output,"w")
+pid = subprocess.Popen(run_tamm_code, stdout=tof, stderr=tof, shell=True)
+pid.communicate()
+tof.close()
+
+#print run_tamm_code + " 2>&1 | tee " + tamm_output
+#os.system(run_tamm_code + " 2>&1 | tee " + tamm_output)
+
+orig_result = input_folder + "correct_outputs/" + testcase + ".output"
+
+for result_string,num_lines in patterns.iteritems():
+    res1 = check_result_file(orig_result,result_string,num_lines)
+    res2 = check_result_file(tamm_output,result_string,num_lines)
+
+    if num_lines[0] == "=":
+        value1 = float(res1[0])
+        value2 = float(res2[0])
+        if math.fabs(value1 - value2) > 1.0e-10:
+            print result_string + " does not match"
+            print "Expected value = " + res1[0] + ", Found = " + res2[0]
+            num_failed +=1
+        continue
+
+    nl = num_lines[0]
+    for v in range(0,nl):
+        value1 = float(res1[v])
+        value2 = float(res2[v])
+        if math.fabs(value1 - value2) > 1.0e-10:
+            print result_string + " Residuum does not match"
+            print "At iteration " + str(v+1) + ": Expected = " + res1[v] + ", Found = " + res2[v]
+            num_failed += 1
+
+if num_failed > 0: sys.exit(1)

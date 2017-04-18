ce4594c593f22445360a42e7b8e630143132bfeb
Ajay Panyala <ajay.panyala@gmail.com>
2017-04-17 21:26:12 -0700

    Cleanup cmakelists - Add patch,unpatch for nwchem cmake build
    Former-commit-id: daa5a242d2abd6b2d30697db371732d2a39e5bc7
    Former-commit-id: 32d55cc2015ec5de06f97c45ca46ce2fff967398
    Former-commit-id: 053a835a81a84e6c6d0556f36c0daa881f6727ad
    Former-commit-id: 446d5fb499009c2323b93f328251aaba0590963d [formerly e2272a77fe01514562d3c929b2be146283da600f]
    Former-commit-id: 76cbf47d6a46153dc51a13754fbac0b11149b1c6

---
+++ cmake/toolchains/intel-openmpi-mkl.cmake
@@ -0,0 +1,28 @@
+
+set(CMAKE_BUILD_TYPE Release)
+
+# Compilers (assuming the compilers are in the PATH)
+set(CMAKE_C_COMPILER icc)
+set(CMAKE_CXX_COMPILER icpc)
+set(CMAKE_Fortran_COMPILER ifort)
+# set(MPI_C_COMPILER mpicc)
+# set(MPI_CXX_COMPILER mpicxx)
+
+set(TAMM_PROC_COUNT 2)
+
+# BLAS
+set(TAMM_BLAS_INC /opt/intel/mkl/include/)
+set(TAMM_BLAS_LIB /opt/intel/mkl/lib/intel64)
+
+# BLAS. Only 8-byte BLAS is supported for now. 
+# Set BLAS_LIBRARIES only when using nwchem internal blas - All other cases these come from ga_config
+#set(BLAS_LIBRARIES "-mkl=parallel -lmkl_blacs_openmpi_ilp64 -lmkl_core -lmkl_intel_thread -lpthread -lm -ldl" CACHE STRING "BLAS linker flags")
+
+set(ANTLR_CPPRUNTIME /opt/libraries/ANTLR4/antlr4-cpp-runtime)
+
+# General NWCHEM Options
+set(NWCHEM_TOP /home/panyala/git/nwchem-cmake/nwchem-devel)
+set(GA_CONFIG ${NWCHEM_TOP}/buildIntel/nwc-install/external/ga/bin)
+set(NWCHEM_BUILD_DIR ${NWCHEM_TOP}/buildIntel/)
+option(NWCHEM_CMAKE_BUILD "Using NWCHEM CMAKE build" ON)
+

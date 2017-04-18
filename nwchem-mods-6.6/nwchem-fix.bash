55e15b30624280ebc3b48cba33549a59cde1963c
Ajay Panyala <ajay.panyala@gmail.com>
2017-04-17 21:54:28 -0700

    adjust patch,unpatch for nwchem 6.6 mods

--- nwchem-mods-6.6/nwchem-fix.bash
+++ nwchem-mods-6.6/nwchem-fix.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 if [ $# -gt 0 ]; then
     cp template.patch gnumake.patch
-    sed -i -- "s:(LIB_TEMP):$1:g" ./gnumake.patch
-    sed -i -- "s:(LIB_ANTLR):$2:g" ./gnumake.patch
-    patch -s -N -r - $NWCHEM_TOP/src/GNUmakefile ./gnumake.patch
+    sed -i -- "s:(LIB_TEMP):$2:g" ./gnumake.patch
+    sed -i -- "s:(LIB_ANTLR):$3:g" ./gnumake.patch
+    patch -s -N -r - $1/src/GNUmakefile ./gnumake.patch
     rm gnumake.patch
 else
     echo "Usage $0 INSTALL_LIB_PATH"
 fi
 
-if [ -z "$3" ]; then
-    rsync -rav --progress ./src $NWCHEM_TOP 
+if [ -z "$4" ]; then
+    rsync -rav --progress ./src $1
 fi
-#cp *.F $NWCHEM_TOP/src/tce/
+#cp *.F $1/src/tce/
 

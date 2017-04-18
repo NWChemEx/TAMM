d3499038d613eb8b2e01abc28ba59ecb4448a776
Ajay Panyala <ajay.panyala@gmail.com>
2017-04-18 12:43:38 -0700

    update jenkins file
    Former-commit-id: 36831387987aee730fb91f96fa3bb47e0ba97e0e
    Former-commit-id: 65a125ba36a28cc2e64d01438f5b0033696d012f
    Former-commit-id: 732db248b10ee86ab0c658eb91571d3353c6e2bc
    Former-commit-id: 370c98e68bf43600c25c37171995c8c781ba93a1 [formerly 222f98263eaf03bbad95f2ccccd3b00a78882d8a]
    Former-commit-id: 9e64cd7840917d21fea03e92d9a9316c6436c888

--- cmake/toolchains/jenkins-gcc-openmpi-netlib.cmake
+++ cmake/toolchains/jenkins-gcc-openmpi-netlib.cmake
@@ -10,17 +10,18 @@ set(CMAKE_Fortran_COMPILER gfortran)
 set(TAMM_PROC_COUNT 8)
 
 # BLAS
-set(TAMM_BLAS_INC /opt/intel/mkl/include/)
-set(TAMM_BLAS_LIB /opt/intel/mkl/lib/intel64)
+#set(TAMM_BLAS_INC /opt/intel/mkl/include/)
+set(TAMM_BLAS_LIB $ENV{WORKSPACE}/nwchem-devel/lib/LINUX64)
+
 
 # BLAS. Only 8-byte BLAS is supported for now. 
 # Set BLAS_LIBRARIES only when using nwchem internal blas - All other cases these come from ga_config
 set(BLAS_LIBRARIES "-lnwcblas" CACHE STRING "BLAS linker flags")
 
-set(ANTLR_CPPRUNTIME /opt/libraries/ANTLR4/antlr4-cpp-runtime)
+#set(ANTLR_CPPRUNTIME /opt/libraries/ANTLR4/antlr4-cpp-runtime)
 
 # General NWCHEM Options
-set(NWCHEM_TOP /home/jenkins/nwchem-cmake/nwchem-devel)
-set(GA_CONFIG ${NWCHEM_TOP}/build/nwc-install/external/ga/bin)
-set(NWCHEM_BUILD_DIR ${NWCHEM_TOP}/build/)
-option(NWCHEM_CMAKE_BUILD "Using NWCHEM CMAKE build" ON)
+set(NWCHEM_TOP $ENV{WORKSPACE}/nwchem-devel)
+set(GA_CONFIG ${NWCHEM_TOP}/src/tools/install/bin)
+set(NWCHEM_BUILD_DIR ${NWCHEM_TOP}/src/)
+option(NWCHEM_CMAKE_BUILD "Using NWCHEM CMAKE build" OFF)

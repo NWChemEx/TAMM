1ab32244aa513747a27e194101103d0322fde641
Sriram Krishnamoorthy <sriram@pnnl.gov>
2017-05-24 19:02:10 -0700

    code a little more generic in terms of its type. not just doubles. index_sort and index_sortacc still do not support non-double implementations

--- tammx/index_sort.cc
+++ tammx/index_sort.cc
@@ -8,14 +8,18 @@
 // Operated by Battelle for the U.S. Department of Energy
 //
 //------------------------------------------------------------------------------
+
+#ifndef TAMM_TENSOR_TAMMX_H_
+#define TAMM_TENSOR_TAMMX_H_
+
 #include "tammx/index_sort.h"
 #include <algorithm>
 #include <cassert>
 #include <iostream>
 
-namespace tamm {
+namespace tammx {
 
-static void copy(const double *sbuf, double *dbuf, size_t sz, double alpha) {
+void copy(const double *sbuf, double *dbuf, size_t sz, double alpha) {
   for (size_t i = 0; i < sz; i++) {
     dbuf[i] = alpha * sbuf[i];
   }
@@ -28,31 +32,6 @@ void index_sort_3(const double *sbuf, double *dbuf, size_t sz1, size_t sz2,
 void index_sort_4(const double *sbuf, double *dbuf, size_t sz1, size_t sz2,
                   size_t sz3, size_t sz4, int p1, int p2, int p3, int p4,
                   double alpha);
-
-void index_sort(const double *sbuf, double *dbuf, int ndim, const size_t *sizes,
-                const int *perm, double alpha) {
-  assert(sbuf);
-  assert(dbuf);
-  assert(ndim >= 0);
-  assert(ndim == 0 || sizes);
-  assert(ndim == 0 || perm);
-
-  if (ndim == 0) {
-    *dbuf = *sbuf * alpha;
-  } else if (ndim == 1) {
-    copy(sbuf, dbuf, sizes[0], alpha);
-  } else if (ndim == 2) {
-    index_sort_2(sbuf, dbuf, sizes[0], sizes[1], perm[0], perm[1], alpha);
-  } else if (ndim == 3) {
-    index_sort_3(sbuf, dbuf, sizes[0], sizes[1], sizes[2], perm[0], perm[1],
-                 perm[2], alpha);
-  } else if (ndim == 4) {
-    index_sort_4(sbuf, dbuf, sizes[0], sizes[1], sizes[2], sizes[3], perm[0],
-                 perm[1], perm[2], perm[3], alpha);
-  } else {
-    assert(0);
-  }
-}
 
 void index_sort_2(const double *sbuf, double *dbuf, size_t sz1, size_t sz2,
                   int p1, int p2, double alpha) {
@@ -134,7 +113,7 @@ void index_sort_4(const double *sbuf, double *dbuf, size_t sz1, size_t sz2,
  * accumulate variants
  */
 
-static void copy_add(const double *sbuf, double *dbuf, size_t sz,
+void copy_add(const double *sbuf, double *dbuf, size_t sz,
                      double alpha) {
   for (size_t i = 0; i < sz; i++) {
     dbuf[i] += alpha * sbuf[i];
@@ -239,4 +218,6 @@ void index_sortacc_4(const double *sbuf, double *dbuf, size_t sz1, size_t sz2,
     }
   }
 }
-} /*namespace tamm*/
+} /*namespace tammx*/
+
+#endif // TAMM_TENSOR_TAMMX_H_

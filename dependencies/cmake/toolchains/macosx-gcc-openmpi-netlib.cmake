f6741f4ba7d384b74464d545aa5fe1a8a9060e06
Ajay Panyala <ajay.panyala@gmail.com>
2017-05-26 10:09:15 -0700

    cmake build for tamm dependencies
    Former-commit-id: 8fc5657215627d3988342a02913407d8d3649988
    Former-commit-id: 1cde0580d6b4f5c7ced53edb8460c1d682d12e75
    Former-commit-id: 732ac67820f74a6de7c6a52619ce1f411c68dfa5
    Former-commit-id: 790177f58dad0ce1326f04a8fd5b12be0226c096 [formerly 4dd4030ad855251ff5c91ada8e62eb32de7180ae]
    Former-commit-id: 34722beb1452c94ead6bfe0bdd37914bc2512dbe

---
+++ dependencies/cmake/toolchains/macosx-gcc-openmpi-netlib.cmake
@@ -0,0 +1,28 @@
+
+set(CMAKE_BUILD_TYPE Release)
+
+# Compilers (assuming the compilers are in the PATH)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+set(CMAKE_Fortran_COMPILER gfortran)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+#Location where the dependencies need to be installed.
+#Default location: cmake_build_folder/tamm-deps
+#set(CMAKE_INSTALL_PREFIX /opt/tamm-deps)
+
+# MPI 
+set(MPI_INCLUDE_PATH /Users/panyala/software/libraries/openmpi/include)
+set(MPI_LIBRARY_PATH /Users/panyala/software/libraries/openmpi/lib)
+set(MPI_LIBRARIES "-lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi")
+
+#Number of cores to be used for the build
+set(TAMM_PROC_COUNT 2)
+
+# The following option needs to be set only if BOOST is installed in a non-standard path
+set(BOOST_INSTALL_PATH /Users/panyala/software/libraries/BOOST)
+
+# GA Options
+#set(ARMCI_NETWORK OPENIB)
+set(USE_OFFLOAD "OFFLOAD" ON) 
+

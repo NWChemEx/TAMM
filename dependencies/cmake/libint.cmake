7bb5aabaaed25a365a106e39b1087f402dae2e8b
Ajay Panyala <ajay.panyala@gmail.com>
2017-05-26 14:27:37 -0700

    undo changes to libint build

--- dependencies/cmake/libint.cmake
+++ dependencies/cmake/libint.cmake
@@ -20,19 +20,20 @@ if(BUILD)
         PREFIX LIBINT
         GIT_REPOSITORY https://github.com/evaleev/libint.git
         SOURCE_DIR ${PROJECT_BINARY_DIR}/external/libint
-        BINARY_DIR ${CMAKE_BINARY_DIR}/external/libint-build
-        CONFIGURE_COMMAND ./autogen.sh \
-        COMMAND  ./configure --prefix=${CMAKE_INSTALL_PREFIX}/libint2
+        #PATCH_COMMAND ${PROJECT_BINARY_DIR}/external/libint/autogen.sh
+        #BINARY_DIR ${CMAKE_BINARY_DIR}/external/libint-build
+        CONFIGURE_COMMAND  ./autogen.sh COMMAND ./configure
+                --prefix=${CMAKE_INSTALL_PREFIX}/libint2
 			    CXX=${CMAKE_CXX_COMPILER}
-                            CC=${CMAKE_C_COMPILER}
-                            CXXFLAGS=${LIBINT_FLAGS}
-                            CPPFLAGS=-I${BOOST_INSTALL_PATH}/include
-                            LDFLAGS=-L${BOOST_INSTALL_PATH}/lib
-                            --enable-eri=0 
-                            --enable-eri3=0
-                            --enable-eri2=0
-                            --with-max-am=4
-                            --disable-t1g12-support
+                CC=${CMAKE_C_COMPILER}
+                CXXFLAGS=${LIBINT_FLAGS}
+                CPPFLAGS=-I${BOOST_INSTALL_PATH}/include
+                LDFLAGS=-L${BOOST_INSTALL_PATH}/lib
+                --enable-eri=0
+                --enable-eri3=0
+                --enable-eri2=0
+                --with-max-am=4
+                --disable-t1g12-support
         BUILD_COMMAND make -j${TAMM_PROC_COUNT}
         INSTALL_COMMAND make install
         BUILD_IN_SOURCE 1

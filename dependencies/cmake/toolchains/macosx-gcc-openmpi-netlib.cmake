4fd98126c25d585113ec028436661e16d05e344d
Ajay Panyala <ajay.panyala@gmail.com>
2017-06-14 21:26:58 -0700

    Clean cmake build - disable building old tamm code

--- dependencies/cmake/toolchains/macosx-gcc-openmpi-netlib.cmake
+++ dependencies/cmake/toolchains/macosx-gcc-openmpi-netlib.cmake
@@ -25,12 +25,13 @@ set(MPI_INCLUDE_PATH /Users/panyala/software/libraries/openmpi/include)
 set(MPI_LIBRARY_PATH /Users/panyala/software/libraries/openmpi/lib)
 set(MPI_LIBRARIES "-lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi")
 
-
-
 #set(TALSH_INSTALL_PATH /home/gawa722/Cascade/nwchem/TAL_SH)
-# General NWCHEM Options
-#Using NWCHEM CMAKE build
-set(NWCHEM_TOP /home/panyala/git/nwchem-cmake/nwchem-devel)
-set(GA_CONFIG ${NWCHEM_TOP}/build/nwchem-install/external/ga/bin)
-set(NWCHEM_BUILD_DIR ${NWCHEM_TOP}/build/)
-option(NWCHEM_CMAKE_BUILD "Using NWCHEM CMAKE build" ON)
+
+# Optionally set BLAS, LAPACK & SCALAPACK. Support only 8-byte integers for now.
+# If not set, will build BLAS+LAPACK automatically.
+#set(BLAS_INCLUDE_PATH /opt/intel/mkl/include/)
+##set(BLAS_LIBRARY_PATH /opt/intel/mkl/lib/intel64)
+#
+#set(BLAS_LIBRARIES "-mkl -lmkl_lapack95_ilp64 -lmkl_blas95_ilp64 -lmkl_core -lmkl_intel_thread -lpthread -lm -ldl" CACHE STRING "BLAS linker flags")
+#set(LAPACK_LIBRARIES "${BLAS_LIBRARIES}" CACHE STRING "LAPACK linker flags")
+#set(SCALAPACK_LIBRARIES "-mkl -lmkl_scalapack_ilp64 -lmkl_blacs_openmpi_ilp64 -lmkl_intel_thread -lpthread -lm -ldl" CACHE STRING "SCALAPACK linker flags")

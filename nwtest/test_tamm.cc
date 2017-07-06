b0d3f5fac5ce854e0cd8fc26581b16473d0f4633
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-07-05 17:16:17 -0700

    Add new tamm test code driver
    Former-commit-id: c5d38257f77dc0c55fcf8376dacdb1ff208e8fc8
    Former-commit-id: 5fc126b87b98307f1fe9ae5d6ba362a22fe0f2d3
    Former-commit-id: 53fb28c179c4ed3ed6be1f382c3aff4b7f919272
    Former-commit-id: 4ca42bec49d0ad2ab1e72474e4e93a2c5d6bb0cd [formerly d99e3e93570e1ef7d0fe10096cd7fb20a700b104]
    Former-commit-id: 10e89caa0ca3ba2a74d8e4fdfc1b0bf342782a6a

---
+++ nwtest/test_tamm.cc
@@ -0,0 +1,43 @@
+//------------------------------------------------------------------------------
+// Copyright (C) 2016, Pacific Northwest National Laboratory
+// This software is subject to copyright protection under the laws of the
+// United States and other countries
+//
+// All rights in this computer software are reserved by the
+// Pacific Northwest National Laboratory (PNNL)
+// Operated by Battelle for the U.S. Department of Energy
+//
+//------------------------------------------------------------------------------
+#include <iostream>
+#include "tensor/corf.h"
+#include "tensor/equations.h"
+#include "tensor/input.h"
+#include "tensor/schedulers.h"
+#include "tensor/t_assign.h"
+#include "tensor/t_mult.h"
+#include "tensor/tensor.h"
+#include "tensor/tensors_and_ops.h"
+#include "tensor/variables.h"
+
+extern "C" {
+void init_fortran_vars_(Integer *noa1, Integer *nob1, Integer *nva1,
+                        Integer *nvb1, bool intorb1, bool restricted1,
+                        Integer *spins, Integer *syms, Integer *ranges);
+}
+
+int main() {
+    Integer noa1 = 2;
+    Integer nob1 = 2;
+    Integer nva1 = 2;
+    Integer nvb1 = 2;
+    bool intorb1 = false;
+    bool restricted1 = false;
+    Integer spins[noa1+nob1+nva1+nvb1] = {0};
+    Integer syms[noa1+nob1+nva1+nvb1] = {0};
+    Integer ranges[noa1+nob1+nva1+nvb1] = {0};
+
+    init_fortran_vars_(&noa1, &nob1, &nva1, &nvb1, intorb1, restricted1,
+                       &spins[0], &syms[0], &ranges[0]);
+
+    return 0;
+}

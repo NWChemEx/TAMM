1611f8d9fa62bf80e81d3a7a51a8f443f83b5a4f
Sriram Krishnamoorthy <sriram@pnnl.gov>
2017-09-11 12:15:11 -0700

    improved printing of ga

--- tammx/ops.h
+++ tammx/ops.h
@@ -1903,6 +1903,9 @@ template<typename T, typename LabeledTensorType>
 inline void
 AddOp<T, LabeledTensorType>::execute() {
   using T1 = typename LabeledTensorType::element_type;
+
+  std::cout<<"ADD_OP. C"<<lhs_.label_<<" += "<<alpha_<<" * A"<<rhs_.label_<<"\n";
+  
   tensor_ga_print(*lhs_.tensor_);
   tensor_ga_print(*rhs_.tensor_);
   if(exec_mode_ == ExecutionMode::fortran) {
@@ -2118,6 +2121,10 @@ template<typename T, typename LabeledTensorType>
 inline void
 MultOp<T, LabeledTensorType>::execute() {
   using T1 = typename LabeledTensorType::element_type;
+  std::cout<<"MULT_OP. C"<<lhs_.label_<<" += "<<alpha_
+           <<" * A"<<rhs1_.label_
+           <<" * B"<<rhs2_.label_
+           <<"\n";
 
   tensor_ga_print(*lhs_.tensor_);
   tensor_ga_print(*rhs1_.tensor_);

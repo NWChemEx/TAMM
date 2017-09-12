1f276072b18eecaca95b42b684a5b8f903d175f8
Sriram Krishnamoorthy <sriram@pnnl.gov>
2017-09-11 21:05:01 -0700

    clean up some todo comments. they have been cataloged elsewhere
    Former-commit-id: 3cacced5ff67674382fe4bb6dea8a6bca86c1928
    Former-commit-id: f0c85107c7313385ef466c880a973f12e21e5013
    Former-commit-id: 1c21b40016e7562381049c00ad9edbb43d953ada
    Former-commit-id: 90dca20a159e7639d164f63630ef6d09a518b5a9 [formerly 9b7fc54662191f6832574dc5d53f0156810856ab]
    Former-commit-id: a46d64ea3e50a874485f94f57cdc1bc13341de08

--- tammx/tammx.h
+++ tammx/tammx.h
@@ -12,6 +12,7 @@
 #include <map>
 #include <iostream>
 #include <type_traits>
+
 #include "tammx/boundvec.h"
 #include "tammx/types.h"
 #include "tammx/index_sort.h"
@@ -22,30 +23,6 @@
 #include "tammx/product_iterator.h"
 #include "tammx/triangle_loop.h"
 #include "tammx/copy_symmetrizer.h"
-
-/**
- * @todo Check pass-by-value, reference, or pointer, especially for
- * Block and Tensor
- *
- * @todo Parallelize parallel_work
- *
- * @todo Implement TCE::init() and TCE::finalize()
- *
- * @todo should TCE be a "singleton" or an object? Multiple distinct
- * CC calculations in one run?
- *
- * @todo Make everything process-group aware
- *
- * @todo BoundVec should properly destroy objects
- *
- * @todo A general expression template formulation
- *
- * @todo Move/copy semantics for Tensor and Block
- *
- * @todo Scoped allocation for Tensor & Block
- *
- */
-
 #include "tammx/block.h"
 #include "tammx/labeled-block.h"
 #include "tammx/labeled-tensor.h"

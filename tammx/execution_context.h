51bd5f82dcbada6a92db88e5d9280f5b745a046f
Sriram Krishnamoorthy <sriram@pnnl.gov>
2017-09-11 23:04:31 -0700

    move some code to separate header file
    Former-commit-id: ff001d7e1653573f5e1d19e8ac1b6b0a1940b2ff
    Former-commit-id: d520ecf131fae9ba7a6b1a71932656b2f0e50a54
    Former-commit-id: 1c9c2e8e59334ef8869a504bb2fcc7a7f6609993
    Former-commit-id: bd1a92791801c03d3a88f5c859125b2c94333eb0 [formerly bc8ac64df6b7883e808b58b39ca3a32f33b502d5]
    Former-commit-id: 41ba3b5b02eccde5478e6bb416bdf5e6744c8757

---
+++ tammx/execution_context.h
@@ -0,0 +1,82 @@
+#ifndef TAMMX_EXECUTION_CONTEXT_H_
+#define TAMMX_EXECUTION_CONTEXT_H_
+
+#include "tammx/types.h"
+#include "tammx/proc-group.h"
+#include "tammx/memory_manager.h"
+#include "tammx/distribution.h"
+#include "tammx/ops.h"
+
+namespace tammx {
+class ExecutionContext {
+ public:
+  ExecutionContext(ProcGroup pg, Distribution* default_distribution,
+                   MemoryManager* default_memory_manager,
+                   Irrep default_irrep, bool default_spin_restricted)
+      : pg_{pg},
+        default_distribution_{default_distribution},
+        default_memory_manager_{default_memory_manager},
+        default_irrep_{default_irrep},
+        default_spin_restricted_{default_spin_restricted} {}
+
+  Scheduler scheduler() {
+    return Scheduler{pg_,
+          default_distribution_,
+          default_memory_manager_,
+          default_irrep_,
+          default_spin_restricted_};
+  }
+
+  void allocate() {
+    //no-op
+  }
+
+  template<typename T, typename ...Args>
+  void allocate(Tensor<T>& tensor, Args& ... tensor_list) {
+    tensor.alloc(pg_, default_distribution_, default_memory_manager_);
+    allocate(tensor_list...);
+  }
+
+
+  static void deallocate() {
+    //no-op
+  }
+
+  template<typename T, typename ...Args>
+  static void deallocate(Tensor<T>& tensor, Args& ... tensor_list) {
+    tensor.dealloc();
+    deallocate(tensor_list...);
+  }
+
+  ProcGroup pg() const {
+    return pg_;
+  }
+
+  Distribution* distribution() const {
+    return default_distribution_;
+  }
+
+  MemoryManager* memory_manager() const {
+    return default_memory_manager_;
+  }
+
+  Irrep irrep() const {
+    return  default_irrep_;
+  }
+
+  bool is_spin_restricted() const {
+    return default_spin_restricted_;
+  }
+
+ private:
+  ProcGroup pg_;
+  Distribution* default_distribution_;
+  MemoryManager* default_memory_manager_;
+  Irrep default_irrep_;
+  bool default_spin_restricted_;
+}; // class ExecutionContext
+
+} // namespace tammx
+
+#endif // TAMMX_EXECUTION_CONTEXT_H_
+

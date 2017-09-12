40c0c970c5c035bfc5eff5d37abcf5086c85e4a6
Sriram Krishnamoorthy <sriram@pnnl.gov>
2017-09-11 22:54:32 -0700

    move some code to separate header file

--- tammx/util.h
+++ tammx/util.h
@@ -14,6 +14,7 @@
 #include "tammx/tce.h"
 #include "tammx/perm.h"
 #include "tammx/tensor_dims.h"
+#include "tammx/tensor_labels.h"
 
 /**
  * @todo Check types are convertible to necessary type rather than is_same
@@ -237,31 +238,6 @@ factorial(int n) {
   return ret;
 }
 
-
-namespace tensor_labels {
-
-struct OLabel : public IndexLabel {
-  OLabel(int n)
-      : IndexLabel{n, RangeType{DimType::o}} {}
-};
-
-struct VLabel : public IndexLabel {
-  VLabel(int n)
-      : IndexLabel{n, RangeType{DimType::v}} {}
-};
-
-struct NLabel : public IndexLabel {
-  NLabel(int n)
-      : IndexLabel{n, RangeType{DimType::n}} {}
-};
-
-const OLabel h1{1}, h2{2}, h3{3}, h4{4}, h5{5}, h6{6}, h7{7}, h8{8}, h9{9}, h10{10}, h11{11};
-const VLabel p1{1}, p2{2}, p3{3}, p4{4}, p5{5}, p6{6}, p7{7}, p8{8}, p9{9}, p10{10}, p11{11};
-
-const OLabel i{0}, j{1};
-const VLabel a{0}, b{1};
-
-} // namespace tensor_labels
 
 template<typename Itr>
 class NestedIterator {

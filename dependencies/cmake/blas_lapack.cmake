480e78f50c0643dd20dc55e63edb358f7291c71c
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-09-19 17:06:41 -0700

    Add blas+lapack to dependencies build

--- dependencies/cmake/blas_lapack.cmake
+++ dependencies/cmake/blas_lapack.cmake
@@ -1,14 +1,14 @@
 
 if(EXISTS ${CMAKE_INSTALL_PREFIX}/blas_lapack/lib/libblas.a)
-    set(BLAS_INCLUDE_PATH ${CMAKE_INSTALL_PREFIX}/blas_lapack/include)
-    set(BLAS_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/blas_lapack/lib)
+#    set(BLAS_INCLUDE_PATH ${CMAKE_INSTALL_PREFIX}/blas_lapack/include)
+#    set(BLAS_LIBRARY_PATH ${CMAKE_INSTALL_PREFIX}/blas_lapack/lib)
     add_custom_target(BLAS_LAPACK ALL)
 else()
     message("Building BLAS+LAPACK")
     include(ExternalProject)
     ExternalProject_Add(BLAS_LAPACK
         PREFIX BLAS_LAPACK
-        URL http://www.netlib.org/lapack/lapack-3.7.0.tgz
+        URL http://www.netlib.org/lapack/lapack-3.7.1.tgz
         SOURCE_DIR ${PROJECT_BINARY_DIR}/external/blas_lapack
         UPDATE_COMMAND mkdir -p "${PROJECT_BINARY_DIR}/external/blas_lapack/build"
         BINARY_DIR ${PROJECT_BINARY_DIR}/external/blas_lapack/build

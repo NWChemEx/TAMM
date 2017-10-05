84d2f34d0c86deecef14bf5ccf43edf6764bac15
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-10-05 13:36:38 -0700

    Updates to sample toolchain files

--- dependencies/cmake/toolchains/intel-impi-mkl.cmake
+++ dependencies/cmake/toolchains/intel-impi-mkl.cmake
@@ -2,21 +2,19 @@
 set(CMAKE_BUILD_TYPE Release)
 
 # Compilers (assuming the compilers are in the PATH)
-set(CMAKE_C_COMPILER gcc)
-set(CMAKE_CXX_COMPILER g++)
-set(CMAKE_Fortran_COMPILER gfortran)
+set(CMAKE_C_COMPILER icc)
+set(CMAKE_CXX_COMPILER icpc)
+set(CMAKE_Fortran_COMPILER ifort)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-set(BLAS_INCLUDE_PATH /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/Accelerate.framework/Frameworks/vecLib.framework/Headers/)
 
 # General NWCHEM Options
 # Set the following when using the NWCHEM CMAKE build
-set(NWCHEM_TOP /Users/panyala/software/libraries/nwchem-cmake/nwchem-devel)
+set(NWCHEM_TOP /opt/nwchem-cmake/nwchem-devel)
 set(GA_CONFIG ${NWCHEM_TOP}/build/nwchem-install/external/ga/bin)
 set(NWCHEM_BUILD_DIR ${NWCHEM_TOP}/build/)
 option(NWCHEM_CMAKE_BUILD "Using NWCHEM CMAKE build" ON)
 
-# Set the following when using the old Makefile-based NWCHEM build
+# Set the following when using the regular Makefile-based NWCHEM build
 # set(NWCHEM_TOP /opt/libraries/nwchem-devel)
 # set(NWCHEM_BUILD_TARGET LINUX64)
 # set(GA_CONFIG ${NWCHEM_TOP}/src/tools/install/bin)
@@ -24,16 +22,22 @@ option(NWCHEM_CMAKE_BUILD "Using NWCHEM CMAKE build" ON)
 
 
 #------------------ OPTIONAL ---------------------
-#Location where the dependencies need to be installed.
-#Default location: cmake_build_folder/tamm-deps
-#set(CMAKE_INSTALL_PREFIX /opt/tamm-deps)
 
 #Number of cores to be used for the build
 set(TAMM_PROC_COUNT 2)
 
-set(EIGEN3_INSTALL_PATH /Users/panyala/PNNL/code/tamm/dependencies/b1/tamm-deps/eigen3)
-set(LIBINT_INSTALL_PATH /Users/panyala/PNNL/code/tamm/dependencies/b1/tamm-deps/libint2)
-#set(ANTLR_CPPRUNTIME_PATH /opt/libraries/ANTLR4/antlr4-cpp-runtime)
+#Location where the dependencies need to be installed.
+#Default location: cmake_build_folder/tamm-deps
+#set(CMAKE_INSTALL_PREFIX /opt/tamm-deps)
+
+#set(LIBINT_INSTALL_PATH /opt/libint2) 
+#set(ANTLR_CPPRUNTIME_PATH /opt/ANTLR/CppRuntime) 
+#set(EIGEN3_INSTALL_PATH /opt/eigen3) 
+#set(BLAS_INCLUDE_PATH /opt/blas_lapack/include) 
+#set(BLAS_LIBRARY_PATH /opt/blas_lapack/lib) 
+#set(GTEST_INSTALL_PATH /opt/googletest) 
+
+#set(TALSH_INSTALL_PATH /opt/TAL_SH)
 
 
 #-------------------- NOT NEEDED FOR NOW --------------------------
@@ -48,17 +52,12 @@ set(LIBINT_INSTALL_PATH /Users/panyala/PNNL/code/tamm/dependencies/b1/tamm-deps/
 #set(USE_OFFLOAD "OFFLOAD" ON)
 
 # MPI 
-#set(MPI_INCLUDE_PATH /Users/panyala/software/libraries/openmpi/include)
-#set(MPI_LIBRARY_PATH /Users/panyala/software/libraries/openmpi/lib)
+#set(MPI_INCLUDE_PATH /opt/openmpi/include)
+#set(MPI_LIBRARY_PATH /opt/openmpi/lib)
 #set(MPI_LIBRARIES "-lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi")
 
-#set(TALSH_INSTALL_PATH /home/gawa722/Cascade/nwchem/TAL_SH)
-
-# Optionally set BLAS, LAPACK & SCALAPACK. Support only 8-byte integers for now.
-# If not set, will build BLAS+LAPACK automatically.
-#set(BLAS_INCLUDE_PATH /opt/intel/mkl/include/)
-##set(BLAS_LIBRARY_PATH /opt/intel/mkl/lib/intel64)
-#
 #set(BLAS_LIBRARIES "-mkl -lmkl_lapack95_ilp64 -lmkl_blas95_ilp64 -lmkl_core -lmkl_intel_thread -lpthread -lm -ldl" CACHE STRING "BLAS linker flags")
 #set(LAPACK_LIBRARIES "${BLAS_LIBRARIES}" CACHE STRING "LAPACK linker flags")
 #set(SCALAPACK_LIBRARIES "-mkl -lmkl_scalapack_ilp64 -lmkl_blacs_openmpi_ilp64 -lmkl_intel_thread -lpthread -lm -ldl" CACHE STRING "SCALAPACK linker flags")
+
+

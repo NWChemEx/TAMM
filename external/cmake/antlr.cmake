65838bbd6a364486c899adf1f6f5a60055fa2f2c
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-10-07 17:29:22 -0700

    remove antlr sources
    Former-commit-id: 40de7a02f7cde7942ea4c1ab1b58a12d5043df23
    Former-commit-id: 266c623c691171eb616aaf1f62c438b2139aee18
    Former-commit-id: 72f770f663745b5125d40faaa869ac516c6e6a92
    Former-commit-id: 5560a15f7af2f1a69d808b183feafd1b2a5bb20a [formerly 923c3d86f6e169b45998b1c6b6e3d9e0787b3646]
    Former-commit-id: b858cf10349148b1bac055a0467dde1d3870acd9

--- external/cmake/antlr.cmake
+++ external/cmake/antlr.cmake
@@ -16,29 +16,29 @@ if(EXISTS ${CMAKE_INSTALL_PREFIX}/ANTLR/CppRuntime/libantlr4-runtime.a)
 else()
 
     message(STATUS "Building ANTLR4 CPP Runtime library at: ${ANTLR_CPPRUNTIME_PATH}")
-    SET(ANTLR_SOURCES ${PROJECT_BINARY_DIR}/external/ANTLR/)
-    execute_process(COMMAND mkdir -p "${ANTLR_SOURCES}")
-    file(COPY ${PROJECT_SOURCE_DIR}/ANTLR4/ DESTINATION ${ANTLR_SOURCES})
+
 
     # Build the ANTLR C Runtime library.
     include(ExternalProject)
     ExternalProject_Add(ANTLR
         PREFIX ANTLR
-        SOURCE_DIR ${ANTLR_SOURCES}/antlr4-cpp-runtime
-        #INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/ANTLR/CppRuntime
+        URL http://www.antlr.org/download/antlr4-cpp-runtime-4.7-source.zip
+        SOURCE_DIR ${PROJECT_BINARY_DIR}/external/ANTLR
+        UPDATE_COMMAND mkdir -p "${PROJECT_BINARY_DIR}/external/ANTLR/build"
+        BINARY_DIR ${PROJECT_BINARY_DIR}/external/ANTLR/build
+        PATCH_COMMAND patch < ${PROJECT_SOURCE_DIR}/cmake/antlr_cmakelists.patch
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}  -DWITH_DEMO=OFF
         -DCMAKE_INSTALL_PREFIX=${ANTLR_CPPRUNTIME_PATH}
-        BINARY_DIR ${ANTLR_SOURCES}/antlr4-cpp-runtime/build
         BUILD_COMMAND make -j${TAMM_PROC_COUNT}
         INSTALL_COMMAND make install
     )
 
 endif()
 
-file(COPY ${PROJECT_SOURCE_DIR}/ANTLR4/antlr-4.7-complete.jar DESTINATION ${CMAKE_INSTALL_PREFIX}/ANTLR/)
-# Set location of the ANTLR binary.
-set(AntlrBinary ${CMAKE_INSTALL_PREFIX}/ANTLR/antlr-4.7-complete.jar)
+# file(COPY ${PROJECT_SOURCE_DIR}/ANTLR4/antlr-4.7-complete.jar DESTINATION ${CMAKE_INSTALL_PREFIX}/ANTLR/)
+# # Set location of the ANTLR binary.
+# set(AntlrBinary ${CMAKE_INSTALL_PREFIX}/ANTLR/antlr-4.7-complete.jar)
 
 endif()
 

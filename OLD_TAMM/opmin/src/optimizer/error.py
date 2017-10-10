a5a2650d4b406cc32d755c2ac5b2d61038128fd5
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-10-10 15:54:48 -0700

    Remove OLD_TAMM subrepo

--- OLD_TAMM/opmin/src/optimizer/error.py
+++
@@ -1,8 +0,0 @@
-class OptimizerError(Exception):
-
-    def __init__(self, message):
-        Exception.__init__(self)
-        self.message = message
-
-    def __str__(self):
-        return 'optimizer error: ' + self.message

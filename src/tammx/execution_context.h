4968ce3d3fcafa88ecdf69f5f09497fe32d58a1f
Sriram Krishnamoorthy <sriram@pnnl.gov>
2017-10-14 21:55:31 -0700

    more documentation

--- src/tammx/execution_context.h
+++ src/tammx/execution_context.h
@@ -14,6 +14,8 @@ namespace tammx {
  * @brief Wrapper class to hold information during execution.
  *
  * This class holds the choice of default memory manager, distribution, irrep, etc.
+ *
+ * @todo Should spin_restricted be wrapper by this class? Or should it always default to false?
  */
 class ExecutionContext {
  public:
@@ -49,6 +51,13 @@ class ExecutionContext {
     //no-op
   }
 
+  /**
+   * Allocate a list of tensor with default parameters (irrep, etc.)
+   * @tparam T Type of element in tensor
+   * @tparam Args Type of list of tensors to be allocated
+   * @param tensor First tensor in the list
+   * @param tensor_list Remaining tensors in the list
+   */
   template<typename T, typename ...Args>
   void allocate(Tensor<T>& tensor, Args& ... tensor_list) {
     tensor.alloc(default_distribution_, default_memory_manager_);
@@ -59,6 +68,13 @@ class ExecutionContext {
     //no-op
   }
 
+  /**
+   * Allocate a list of tensor with default parameters (irrep, etc.) using local memory manager
+   * @tparam T Type of element in tensor
+   * @tparam Args Type of list of tensors to be allocated
+   * @param tensor First tensor in the list
+   * @param tensor_list Remaining tensors in the list
+   */
   template<typename T, typename ...Args>
   void allocate_local(Tensor<T>& tensor, Args& ... tensor_list) {
     tensor.alloc(default_distribution_, memory_manager_local_);
@@ -70,28 +86,55 @@ class ExecutionContext {
     //no-op
   }
 
+  /**
+   * Deallocate a list of tensors
+   * @tparam T Type of element in tensor
+   * @tparam Args Type of list of tensors to be allocated
+   * @param tensor First tensor in the list
+   * @param tensor_list Remaining tensors in the list
+   */
   template<typename T, typename ...Args>
   static void deallocate(Tensor<T>& tensor, Args& ... tensor_list) {
     tensor.dealloc();
     deallocate(tensor_list...);
   }
 
+  /**
+   * Process group for this execution context
+   * @return Underlying process group
+   */
   ProcGroup pg() const {
     return pg_;
   }
 
+  /**
+   * Get the default distribution
+   * @return Default distribution
+   */
   Distribution* distribution() const {
     return default_distribution_;
   }
 
+  /**
+   * Get the default memory manager
+   * @return Default memory manager
+   */
   MemoryManager* memory_manager() const {
     return default_memory_manager_;
   }
 
+  /**
+   * Get the default irrep
+   * @return Default irrep
+   */
   Irrep irrep() const {
     return  default_irrep_;
   }
 
+  /**
+   * Check the default value of spin_restricted
+   * @return Default for spin_restricted
+   */
   bool is_spin_restricted() const {
     return default_spin_restricted_;
   }

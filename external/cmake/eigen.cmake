e767dde9f4ac9714d25ad22738f8ed2d41aff831
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-11-06 06:59:29 -0800

    Some improvements to cmake build
    Former-commit-id: 1f58a42ee659d13b26b3dccff517a2f3496aed0c
    Former-commit-id: 8cf855bb8947b9371f4eb4b935b7d3c36b5369a0
    Former-commit-id: bd26817477a83eac0ef5adb19614d1bd3f7aa128
    Former-commit-id: 1bea1c937c6eeb6868bea209a4b904273326c260 [formerly 488ffdfaa0ce8e5ba613261b2103cdf3e311a994]
    Former-commit-id: eb95549a97ad845d1e1262be52a3b1f6b85ed5b2

--- external/cmake/eigen.cmake
+++ external/cmake/eigen.cmake
@@ -8,6 +8,8 @@ else()
 
 set (EIGEN3_INSTALL_PATH ${CMAKE_INSTALL_PREFIX}/eigen3)
 
+
+
 if(EXISTS ${CMAKE_INSTALL_PREFIX}/eigen3/include/eigen3/Eigen/Core)
 
     ADD_CUSTOM_TARGET(EIGEN3 ALL)
@@ -16,7 +18,23 @@ else()
 
     message("Building Eigen")
     include(ExternalProject)
-    ExternalProject_Add(EIGEN3
+
+    if(CMAKE_CXX_COMPILER_ID MATCHES "PGI")
+    
+        ExternalProject_Add(EIGEN3
+            PREFIX EIGEN3
+            URL http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz
+            SOURCE_DIR ${PROJECT_BINARY_DIR}/external/eigen
+            UPDATE_COMMAND mkdir -p "${PROJECT_BINARY_DIR}/external/eigen/build"
+            BINARY_DIR ${PROJECT_BINARY_DIR}/external/eigen/build
+            CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_COMPILER=g++
+            -DCMAKE_C_COMPILER=gcc -DCMAKE_Fortran_COMPILER=gfortran
+                -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/eigen3
+            #BUILD_COMMAND make -j${NWX_PROC_COUNT} blas
+            INSTALL_COMMAND make install)
+
+    else()
+        ExternalProject_Add(EIGEN3
         PREFIX EIGEN3
         URL http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz
         SOURCE_DIR ${PROJECT_BINARY_DIR}/external/eigen
@@ -26,8 +44,8 @@ else()
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER}
             -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/eigen3
         #BUILD_COMMAND make -j${NWX_PROC_COUNT} blas
-        INSTALL_COMMAND make install
-    )
+        INSTALL_COMMAND make install)
+    endif()
 endif()
 
 endif()

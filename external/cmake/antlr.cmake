cd985292767d67a048633df23740949855c5bf1e
Ajay Panyala <ajay.panyala@pnnl.gov>
2017-11-17 15:22:08 -0800

    Some Cleanup in build

--- external/cmake/antlr.cmake
+++ external/cmake/antlr.cmake
@@ -9,7 +9,7 @@ else()
 # Set location of the ANTLR C runtime library.
 set (ANTLR_CPPRUNTIME_PATH ${CMAKE_INSTALL_PREFIX}/ANTLR/CppRuntime)
 
-if(EXISTS ${CMAKE_INSTALL_PREFIX}/ANTLR/CppRuntime/libantlr4-runtime.a)
+if(EXISTS ${CMAKE_INSTALL_PREFIX}/ANTLR/CppRuntime/lib/libantlr4-runtime.a)
 
     add_custom_target(ANTLR ALL)
 
@@ -21,7 +21,7 @@ else()
     # Build the ANTLR C Runtime library.
     include(ExternalProject)
 
-    if(CMAKE_CXX_COMPILER_ID MATCHES "PGI")
+    if(${CMAKE_CXX_COMPILER_ID} MATCHES "PGI")
     
         ExternalProject_Add(ANTLR
             PREFIX ANTLR
@@ -33,7 +33,6 @@ else()
             CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_COMPILER=g++
             -DCMAKE_C_COMPILER=gcc  -DWITH_DEMO=OFF
             -DCMAKE_INSTALL_PREFIX=${ANTLR_CPPRUNTIME_PATH}
-            BUILD_COMMAND make -j${NWX_PROC_COUNT}
             INSTALL_COMMAND make install
         )
 
@@ -49,12 +48,8 @@ else()
         CMAKE_ARGS -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}  -DWITH_DEMO=OFF -DWITH_LIBCXX=OFF
         -DCMAKE_INSTALL_PREFIX=${ANTLR_CPPRUNTIME_PATH}
-        BUILD_COMMAND make -j${NWX_PROC_COUNT}
         INSTALL_COMMAND make install)
-
-
     endif()
-
 endif()
 
 # file(COPY ${PROJECT_SOURCE_DIR}/ANTLR4/antlr-4.7-complete.jar DESTINATION ${CMAKE_INSTALL_PREFIX}/ANTLR/)

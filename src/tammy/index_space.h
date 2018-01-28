2ee23b35d1c44d02a72b4472a1b23a52766b3603
Sriram Krishnamoorthy <sriram@pnnl.gov>
2018-01-27 18:04:29 -0800

    adding missing files

---
+++ src/tammy/index_space.h
@@ -0,0 +1,301 @@
+// Copyright 2016 Pacific Northwest National Laboratory
+
+#ifndef TAMMY_INDEX_SPACE_H_
+#define TAMMY_INDEX_SPACE_H_
+
+#include <vector>
+
+#include "boundvec.h"
+#include "types.h"
+#include "errors.h"
+
+namespace tammy {
+
+class IndexRange;
+class IndexLabel;
+
+class IndexSpace {
+ public:
+  using Iterator = std::vector<BlockIndex>::const_iterator;  
+  
+  virtual bool has_spatial() const {
+    return false;
+  }
+  virtual Irrep spatial(BlockIndex) const {
+    return Irrep{0};
+  }
+  virtual bool has_spin() const {
+    return false;
+  }
+  virtual Spin spin(BlockIndex) const {
+    return Spin{1};
+  }
+  virtual Size size(BlockIndex) const = 0;
+  virtual Offset offset(BlockIndex) const = 0;
+  virtual Iterator begin(RangeValue rv,
+                         const BlockDimVec& indep_indices={}) const = 0;
+  virtual Iterator end(RangeValue rv,
+                       const BlockDimVec& indep_indices={}) const = 0;
+
+  virtual IndexRange ER() const = 0;
+  virtual IndexRange NR() const = 0;
+
+  IndexLabel N(Label label) const;
+  IndexLabel E(Label label) const;
+
+  template<typename... LabelArgs>
+  auto N(Label label, LabelArgs... labels) const {
+    return std::make_tuple(N(label), N(labels)...);
+  }
+
+  template<typename... LabelArgs>
+  auto E(Label label, LabelArgs... labels) const {
+    return std::make_tuple(E(label), E(labels)...);
+  }
+
+  bool is_identical_to(const IndexSpace& is) const {
+    return this == &is;
+  }
+
+  //strict definition of compatbility
+  bool is_compatible_with(const IndexSpace& is) const {
+    return is_identical_to(is);
+  }
+};  // IndexSpace
+
+inline bool
+operator == (const IndexSpace& lhs, const IndexSpace& rhs) {
+  return lhs.is_identical_to(rhs);
+}
+
+inline bool
+operator < (const IndexSpace& lhs, const IndexSpace& rhs) {
+  return &lhs < &rhs;
+}
+
+inline bool
+operator != (const IndexSpace& lhs, const IndexSpace& rhs) {
+  return !(lhs == rhs);
+}
+
+inline bool
+operator > (const IndexSpace& lhs, const IndexSpace& rhs) {
+  return !(lhs < rhs) && (lhs != rhs);
+}
+
+inline bool
+operator <= (const IndexSpace& lhs, const IndexSpace& rhs) {
+  return (lhs < rhs) || (lhs == rhs);
+}
+
+inline bool
+operator >= (const IndexSpace& lhs, const IndexSpace& rhs) {
+  return (lhs > rhs) || (lhs == rhs);
+}
+
+///////////////////////////////////////////////////////////
+
+
+class IndexRange {
+ public:
+  IndexRange()
+      : is_{nullptr},
+        rv_{0} {}
+  
+  IndexRange(const IndexSpace& is,
+             RangeValue rv)
+      : is_{&is},
+        rv_{rv} {}
+
+  IndexRange(const IndexRange& ir) = default;
+  IndexRange& operator = (const IndexRange& ir) = default;
+
+  const IndexSpace& is() const {
+    return *is_;
+  }
+
+  RangeValue rv() const {
+    return rv_;
+  }
+
+  IndexSpace::Iterator begin(const BlockDimVec& indep_indices = {}) const {
+    return is().begin(rv_, indep_indices);
+  }
+
+  IndexSpace::Iterator end(const BlockDimVec& indep_indices = {}) const {
+    return is().end(rv_, indep_indices);
+  }
+
+ private:
+  const IndexSpace* is_; //non-owning pointer (using pointer to get default constructor)
+  RangeValue rv_;
+};  // IndexRange
+
+inline bool
+operator == (const IndexRange& lhs, const IndexRange& rhs) {
+  return lhs.is() == rhs.is() && 
+      lhs.rv() == rhs.rv();
+}
+
+inline bool
+operator < (const IndexRange& lhs, const IndexRange& rhs) {
+  return (lhs.is() < rhs.is()) ||
+      ((lhs.is() == rhs.is()) && (lhs.rv() < rhs.rv()));
+}
+
+inline bool
+operator != (const IndexRange& lhs, const IndexRange& rhs) {
+  return !(lhs == rhs);
+}
+
+inline bool
+operator > (const IndexRange& lhs, const IndexRange& rhs) {
+  return !(lhs < rhs) && (lhs != rhs);
+}
+
+inline bool
+operator <= (const IndexRange& lhs, const IndexRange& rhs) {
+  return (lhs < rhs) || (lhs == rhs);
+}
+
+inline bool
+operator >= (const IndexRange& lhs, const IndexRange& rhs) {
+  return (lhs > rhs) || (lhs == rhs);
+}
+
+///////////////////////////////////////////////////////////
+
+
+class DependentIndexLabel;
+
+class IndexLabel {
+ public:
+  IndexLabel(IndexRange ir,
+             Label label)
+      : ir_{ir},
+        label_{label} {}
+
+  IndexLabel() = default;
+  IndexLabel(const IndexLabel&) = default;
+  IndexLabel& operator = (const IndexLabel&) = default;
+  
+  const IndexRange& ir() const {
+    return ir_;
+  }
+  
+  Label label() const {
+    return label_;
+  }
+
+  DependentIndexLabel operator() (IndexLabel il1) const;
+  DependentIndexLabel operator() () const;
+  DependentIndexLabel operator() (IndexLabel il1, IndexLabel il2) const;
+
+ protected:
+  IndexRange ir_;
+  Label label_;
+};  // class IndexLabel
+
+inline bool
+operator == (const IndexLabel& lhs, const IndexLabel& rhs) {
+  return lhs.ir() == rhs.ir() &&
+      lhs.label() == rhs.label();
+}
+
+inline bool
+operator < (const IndexLabel& lhs, const IndexLabel& rhs) {
+  return (lhs.ir() < rhs.ir()) ||
+      ((lhs.ir() == rhs.ir()) && (lhs.label() < rhs.label()));
+}
+
+inline bool
+operator != (const IndexLabel& lhs, const IndexLabel& rhs) {
+  return !(lhs == rhs);
+}
+
+inline bool
+operator > (const IndexLabel& lhs, const IndexLabel& rhs) {
+  return !(lhs < rhs) && (lhs != rhs);
+}
+
+inline bool
+operator <= (const IndexLabel& lhs, const IndexLabel& rhs) {
+  return (lhs < rhs) || (lhs == rhs);
+}
+
+inline bool
+operator >= (const IndexLabel& lhs, const IndexLabel& rhs) {
+  return (lhs > rhs) || (lhs == rhs);
+}
+
+
+///////////////////////////////////////////////////////////
+
+
+class DependentIndexLabel {
+ public:
+  DependentIndexLabel(IndexRange& ir,
+                      Label label,
+                      const TensorVec<IndexLabel>& indep_labels)
+      : il_{ir, label},
+        indep_labels_{indep_labels} {}
+
+  DependentIndexLabel(const IndexLabel &il,
+                      const TensorVec<IndexLabel>& indep_labels)
+      : il_{il},
+        indep_labels_{indep_labels} {}
+
+  DependentIndexLabel(const DependentIndexLabel&) = default;
+  DependentIndexLabel& operator = (const DependentIndexLabel&) = default;
+
+  const IndexRange& ir() const {
+    return il_.ir();
+  }
+
+  Label label() const {
+    return il_.label();
+  }
+
+  const TensorVec<IndexLabel>& indep_labels() const {
+    return indep_labels_;
+  }
+
+ protected:
+  IndexLabel il_;
+  TensorVec<IndexLabel> indep_labels_;
+};  // class DependentIndexLabel
+
+///////////////////////////////////////////////////////////
+
+inline IndexLabel
+IndexSpace::N(Label label) const {
+  return IndexLabel{NR(), label};
+}
+
+inline IndexLabel
+IndexSpace::E(Label label) const {
+  return IndexLabel{ER(), label};
+}
+
+///////////////////////////////////////////////////////////
+
+
+inline DependentIndexLabel
+IndexLabel::operator() (IndexLabel il1) const {
+  return {*this, {il1}};
+}
+
+inline DependentIndexLabel
+IndexLabel::operator() () const {
+  return {*this, {}};
+}
+
+inline DependentIndexLabel
+IndexLabel::operator() (IndexLabel il1, IndexLabel il2) const {
+  return {*this, {il1, il2}};
+}
+
+
+}  // namespace tammy
+
+#endif  // TAMMY_INDEX_SPACE_H_
---
+++ src/tammy/mso.h
@@ -0,0 +1,230 @@
+// Copyright 2016 Pacific Northwest National Laboratory
+
+#ifndef TAMMY_MSO_H_
+#define TAMMY_MSO_H_
+
+#include <vector>
+#include <numeric>
+
+#include "index_space.h"
+#include "types.h"
+#include "errors.h"
+
+namespace tammy {
+
+class MSO : public IndexSpace {
+ public:
+  MSO() = default; //@todo delete after debugging
+  MSO(BlockCount noa, BlockCount nob, BlockCount nva, BlockCount nvb,
+     const std::vector<Irrep>& spatials,
+     const std::vector<Spin>& spins,
+      const std::vector<Size,BlockIndex>& sizes)
+      : noa_{noa},
+        nob_{nob},
+        nva_{nva},
+        nvb_{nvb},
+        spatials_{spatials},
+        spins_{spins},
+        sizes_(sizes) {
+          // EXPECTS(noa_ >=0 && nva_ >=0);
+          // EXPECTS(nob_ >=0 && nvb_ >= 0);
+          BlockCount nov_ab = noa_ + nob_ + nva_ + nvb_;
+          EXPECTS(spatials_.size() == nov_ab);
+          EXPECTS(spins_.size() == nov_ab);
+          EXPECTS(sizes_.size() == nov_ab);
+          
+          offsets_.push_back(Offset{0});
+          std::partial_sum(sizes_.begin(), sizes_.end(), std::back_inserter(offsets_));
+          for(auto s: spins_) {
+            EXPECTS(s==Spin{1} || s==Spin{2});
+          }
+          for(BlockIndex i{0}; i<nov_ab; i++) {
+            block_indices_.push_back(i);
+          }
+        }
+           
+  bool has_spatial() const override {
+    return true;
+  }
+  Irrep spatial(BlockIndex bid) const override {
+    return spatials_[bid];
+  }
+  bool has_spin() const override {
+    return true;
+  }
+  Spin spin(BlockIndex bid) const override {
+    return spins_[bid];
+  }
+  Size size(BlockIndex bid) const override {
+    return sizes_[bid];
+  }
+  Offset offset(BlockIndex bid) const override {
+    return offsets_[bid];
+  }
+
+  Iterator begin(RangeValue rv,
+                 const BlockDimVec& bdv={}) const override {    
+    EXPECTS(bdv.size() == 0);
+    auto ret = block_indices_.begin();
+    switch(rv) {
+      case range_n:
+        ret += 0;
+        break;
+      case range_o:
+        ret += 0;
+        break;
+      case range_oa:
+        ret += 0;
+        break;
+      case range_ob:
+        ret += noa_;
+        break;
+      case range_v:
+        ret += noa_ + nob_;
+        break;
+      case range_va:
+        ret += noa_ + nob_;
+        break;
+      case range_vb:
+        ret += noa_ + nob_ + nva_;
+        break;
+      case range_e:
+        ret += noa_ + nob_ + nva_ + nvb_;
+        break;
+      default:
+        assert(0);
+    }
+    return ret;
+  }
+  
+  Iterator end(RangeValue rv,
+               const BlockDimVec& bdv={}) const override {
+    EXPECTS(bdv.size() == 0);
+    auto ret = block_indices_.begin();
+    switch(rv) {
+      case range_n:
+        ret += noa_ + nob_ + nva_ + nvb_;
+        break;
+      case range_o:
+        ret += noa_ + nob_;
+        break;
+      case range_oa:
+        ret += noa_;
+        break;
+      case range_ob:
+        ret += noa_ + nob_;
+        break;
+      case range_v:
+        ret += noa_ + nob_ + nva_ + nvb_;
+        break;
+      case range_va:
+        ret += noa_ + nob_ + nva_;
+        break;
+      case range_vb:
+        ret += noa_ + nob_ + nva_ + nvb_;
+        break;
+      case range_e:
+        ret += noa_ + nob_ + nva_ + nvb_;
+        break;
+      default:
+        assert(0);
+    }
+    return ret;
+  }    
+
+  IndexRange NR() const override {
+    return {*this, range_n};
+  }
+  IndexRange ER() const override {
+    return {*this, range_e};
+  }
+
+  IndexRange OR() const {
+    return {*this, range_o};
+  }
+  
+  IndexRange VR() const {
+    return {*this, range_v};
+  }
+  IndexRange OaR() const {
+    return {*this, range_oa};
+  }
+  IndexRange VaR() const {
+    return {*this, range_va};
+  }
+  IndexRange ObR() const {
+    return {*this, range_ob};
+  }
+  IndexRange VbR() const {
+    return {*this, range_vb};
+  }
+
+  IndexLabel O(Label label) const {
+    return {OR(),label};
+  }
+  IndexLabel V(Label label) const {
+    return {VR(),label};
+  }    
+  IndexLabel Oa(Label label) const {
+    return {OaR(),label};
+  }
+  IndexLabel Va(Label label) const {
+    return {VaR(),label};
+  }
+  IndexLabel Ob(Label label) const {
+    return {ObR(),label};
+  }
+  IndexLabel Vb(Label label) const {
+    return {VbR(),label};
+  }
+
+  template<typename... LabelArgs>
+  auto O(Label label, LabelArgs... labels) const {
+    return std::make_tuple(O(label), O(labels)...);
+  }
+  template<typename... LabelArgs>
+  auto V(Label label, LabelArgs... labels) const {
+    return std::make_tuple(V(label), V(labels)...);
+  }    
+  template<typename... LabelArgs> auto
+  Oa(Label label, LabelArgs... labels) const {
+    return std::make_tuple(Oa(label), Oa(labels)...);
+  }
+  template<typename... LabelArgs> auto
+  Va(Label label, LabelArgs... labels) const {
+    return std::make_tuple(Va(label), Va(labels)...);
+  }
+  template<typename... LabelArgs> auto
+  Ob(Label label, LabelArgs... labels) const {
+    return std::make_tuple(Ob(label), Ob(labels)...);
+  }
+  template<typename... LabelArgs> auto
+  Vb(Label label, LabelArgs... labels) const {
+    return std::make_tuple(Vb(label), Vb(labels)...);
+  }
+
+ protected:
+  BlockCount noa_;
+  BlockCount nob_;
+  BlockCount nva_;
+  BlockCount nvb_;
+  std::vector<Irrep> spatials_;
+  std::vector<Spin> spins_;
+  // std::vector<Size> sizes_;
+  StrongNumIndexedVector<Size,BlockIndex> sizes_;
+  std::vector<Offset> offsets_;
+  std::vector<BlockIndex> block_indices_;
+
+  static const RangeValue range_e   = 0x00;
+  static const RangeValue range_oa  = 0x01;
+  static const RangeValue range_ob  = 0x02;
+  static const RangeValue range_o   = 0x03;
+  static const RangeValue range_va  = 0x10;
+  static const RangeValue range_vb  = 0x20;
+  static const RangeValue range_v   = 0x30;
+  static const RangeValue range_n   = 0x33;
+};  // MSO
+
+}  // namespace tammy
+
+#endif  // TAMMY_MSO_H_

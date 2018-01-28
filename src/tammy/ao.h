4e5f38f0c81075154f1a120c5827c1f3874f2ecd
Sriram Krishnamoorthy <sriram@pnnl.gov>
2018-01-28 00:40:43 -0800

    AO indices

---
+++ src/tammy/ao.h
@@ -0,0 +1,132 @@
+// Copyright 2016 Pacific Northwest National Laboratory
+
+#ifndef TAMMY_AO_H_
+#define TAMMY_AO_H_
+
+#include <vector>
+#include <numeric>
+
+#include "index_space.h"
+#include "types.h"
+#include "errors.h"
+
+namespace tammy {
+
+class AO : public IndexSpace {
+ public:
+  AO() = default; //@todo delete after debugging
+  AO(BlockCount no, BlockCount nv, 
+     const std::vector<Size>& sizes)
+      : no_{no},
+        nv_{nv},
+        sizes_{sizes} {
+          // EXPECTS(noa_ >=0 && nva_ >=0);
+          // EXPECTS(nob_ >=0 && nvb_ >= 0);
+          BlockCount nov = no_ + nv_;
+          EXPECTS(sizes_.size() == nov);          
+          offsets_.emplace_back(0);
+          std::partial_sum(sizes_.begin(), sizes_.end(), std::back_inserter(offsets_));
+          for(BlockIndex i{0}; i<nov; i++) {
+            block_indices_.push_back({i});
+          }
+        }
+
+  Iterator begin(RangeValue rv,
+                 const BlockDimVec& bdv={}) const override {
+    EXPECTS(bdv.size() == 0);
+    auto ret = block_indices_.begin();
+    BlockCount off;
+    switch(rv) {
+      case range_n:
+        off = BlockCount{0};
+        break;
+      case range_o:
+        off = BlockCount{0};
+        break;
+      case range_v:
+        off = no_;
+        break;
+      case range_e:
+        off = no_ + nv_;
+        break;
+      default:
+        UNREACHABLE();
+    }
+    return ret + strongnum_cast<size_t>(off.value());
+  }
+
+  Iterator end(RangeValue rv,
+               const BlockDimVec& bdv={}) const override {
+    EXPECTS(bdv.size() == 0);
+    auto ret = block_indices_.begin();
+    BlockCount off;
+    switch(rv) {
+      case range_n:
+        off = no_ + nv_;
+        break;
+      case range_o:
+        off = no_;
+        break;
+      case range_v:
+        off = no_ + nv_;
+        break;
+      case range_e:
+        off = no_ + nv_;
+        break;
+      default:
+        UNREACHABLE();
+    }
+    return ret + strongnum_cast<size_t>(off.value());
+  }    
+
+  IndexRange NR() const override {
+    return {*this, range_n};
+  }
+  
+  IndexRange ER() const override {
+    return {*this, range_e};
+  }
+
+  IndexRange OR() const {
+    return {*this, range_o};
+  }
+  
+  IndexRange VR() const {
+    return {*this, range_v};
+  }
+
+  IndexLabel O(Label label) const {
+    return {OR(), label};
+  }
+  
+  IndexLabel V(Label label) const {
+    return {VR(), label};
+  }
+
+  template<typename... LabelArgs>
+  auto O(Label label, LabelArgs... labels) const {
+    return std::make_tuple(O(label), O(labels)...);
+  }
+
+  template<typename... LabelArgs>
+  auto V(Label label, LabelArgs... labels) const {
+    return std::make_tuple(V(label), V(labels)...);
+  }
+
+ protected:
+  BlockCount no_;
+  BlockCount nv_;  
+  std::vector<BlockIndex> block_indices_;
+  std::vector<Size> sizes_;
+  std::vector<Offset> offsets_;
+
+  static const RangeValue range_e = 0x0;
+  static const RangeValue range_o = 0x1;
+  static const RangeValue range_v = 0x2;
+  static const RangeValue range_n = 0x3;
+};  // AO
+
+
+}  // namespace tammy
+
+#endif  // TAMMY_AO_H_

872f527d23f321583c02e9a1a5ded23ae89c4419
ryanmrichard <ryanmrichard1@gmail.com>
2018-02-14 17:36:18 -0600

    Removes NWChemExBase directory from repo
    Former-commit-id: aa58d436a02eb75bedb819834ae8f59663c3bcc8
    Former-commit-id: a11b4e6aa48fdf3b8f78de418ae3b6f79fd03db9
    Former-commit-id: 8cb7640e3b68c4f6590feb88d6d166065f59a961
    Former-commit-id: ef8abed8c19c29ba0c86c6e517c49456ee608ae0 [formerly dcf0b239bcfd86b953d1068fe732c578162d26b8]
    Former-commit-id: c89f0d771daa0ffb88181047ba9f8eeb35f96cd9

--- NWChemExBase/NWChemExBase_Test/BuildEigen3_files/BuildEigen3.cpp
+++
@@ -1,9 +0,0 @@
-#include "BuildEigen3/BuildEigen3.hpp"
-#include <Eigen/Core>
-
-void BuildEigen3::run_test()
-{
-    //Even if this gets optimized out the compiler will have proved it can find
-    //Eigen
-    Eigen::MatrixXd m(2,2);
-}

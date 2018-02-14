30786c01c9b869fe1c23eac25503aaa149724d74
Erdal Mutlu <ermutlu@ku.edu.tr>
2018-02-13 16:24:23 -0800

    Initial Loop Iterator construction
    Former-commit-id: e29b3002cdb8355170c593294038fadddc9c6906
    Former-commit-id: 0be6fc9e07583feb4594eab73314bc4e299b316a
    Former-commit-id: 01b8a92281083f9898044c0cae63ec85a4b8cc30
    Former-commit-id: 0ddf6ea3a1ec55b682440919edb864e3575814ea [formerly 3bfa3b8d66b5ac72b0490453dce68014d694d671]
    Former-commit-id: c8c4aced279b219ae63f605cab72bfbf5ecd17dc

--- src/tammy/test.cc
+++ src/tammy/test.cc
@@ -14,6 +14,7 @@
 #include "memory_manager.h"
 #include "memory_manager_local.h"
 #include "distribution.h"
+#include "perm_symmetry.h"
 
 // //#include "memory_manager_ga.h"
 // #include "proc_group.h"
@@ -52,11 +53,15 @@ int main()
   
   // do we want , or just +. Are these indices neither upper nor lower
   auto T1 = Tensor<double>::create<TensorImpl<double>>(i, j); 
+  auto T2 = Tensor<double>::create<TensorImpl<double>>(i | j | E);
+  auto T3 = Tensor<double>::create<TensorImpl<double>>(E | k | j); 
+  auto T4 = Tensor<double>::create<TensorImpl<double>>(j + l | E |E); 
+  
 
   //dependent index labels should only be constructed for dependent index spaces
-  auto T2 = Tensor<double>::create<TensorImpl<double>>(i1(k) + j | k | E);
-  auto T3 = Tensor<double>::create<TensorImpl<double>>(E | i + j1(l) | l);
-  auto T4 = Tensor<double>::create<TensorImpl<double>>(i1(k) + j1(l) | k | l);
+  // auto T2 = Tensor<double>::create<TensorImpl<double>>(i1(k) + j | k | E);
+  // auto T3 = Tensor<double>::create<TensorImpl<double>>(E | i + j1(l) | l);
+  // auto T4 = Tensor<double>::create<TensorImpl<double>>(i1(k) + j1(l) | k | l);
 
   // Valid Tensor Creation
   auto V_T1 = Tensor<double>::create<TensorImpl<double>>(i1(j), j); // disabled for now
@@ -76,26 +81,30 @@ int main()
   auto I_T5 = Tensor<double>::create<TensorImpl<double>>(i + i | E | E); // duplicated index labels
   auto I_T6 = Tensor<double>::create<TensorImpl<double>>(i(k) + i | E | E); // duplicated index labels
 
+#endif
+
+
+
   // Failing operations
   // General Case: tensor with same index label
-  T1(i,j) = T2(k,k);
-  T1(i,j) += T2(j,j);
-  T1(i,j) = T2(j,i) * T3(k,k);
-  T1(i,i) += T2(i,j) * T3(k,l);
+  // T1(i,j) = T2(k,k);
+  // T1(i,j) += T2(j,j);
+  // T1(i,j) = T2(j,i) * T3(k,k);
+  // T1(i,i) += T2(i,j) * T3(k,l);
 
-  // General Case: length of the index label vector does not match the rank in the tensor
-  T1(i,j) += T2(j,i,k);
-  T1(i,j) = 3 * T3(i,j,k);
-  T1(i,j) += T2(j,i) * T3(i,j,l);
 
-  // Addition Case: each index label should occur exactly once in LHS and RHS
-  T1(i,j) += T2(j,i,j);
+  // // General Case: length of the index label vector does not match the rank in the tensor
+  // T1(i,j) += T2(j,i,k);
+  // T1(i,j) += T2(j,i) * T3(i,j,l);
 
-  // Multiplication Case: every outer index label appears in exactly one RHS tensor
-  T1(i,j) = T2(j,i) * T4(i,k);
+  // // Addition Case: each index label should occur exactly once in LHS and RHS
+  // T1(i,j) += T2(j,i,j);
 
-  // Multiplication Case: every inner index label appears exactly once in both RHS tensors
-  T1(i,j) += T2(j,i) * T3(k,j);
+  // // Multiplication Case: every outer index label appears in exactly one RHS tensor
+  // T1(i,j) = T2(j,i) * T4(i,k);
+
+  // // Multiplication Case: every inner index label appears exactly once in both RHS tensors
+  // T1(i,j) += T2(j,i) * T3(k,j);
 
   // Passing operations
   T1(i,j) = 0;
@@ -106,28 +115,28 @@ int main()
   T1(i,j) += 3 * T2(j,i);
   T1(i,j) = T2(i,k) * T3(k,j);
   T1(i,j) += T2(i,k) * T3(k,j);
-  T1(i,j) = 3 * T2(i,l) * T3(j,l);
-  T1(i,j) += 3 * T2(i,l) * T3(j,l);
+  T1(i,j) = 3 * T2(i,l) * T4(j,l);
+  T1(i,j) += 3 * T2(i,l) * T4(j,l);
 
-  Scheduler sch{ProcGroup{}, nullptr, nullptr};
+  // Scheduler sch{ProcGroup{}, nullptr, nullptr};
 
-  sch
-      .tensors(T1,T2,T3)
-      .live_in(T2, T3)
-      .live_out(T1)
-      (T1(i,j)  = 0,
-       T1(i,j) += .52,
-       T1(i,j)  =     T2(j,i),
-       T1(i,j) +=     T2(j,i),
-       T1(i,j)  = 3 * T2(j,i),
-       T1(i,j) += 3 * T2(j,i),
-       T1(i,j)  =     T2(j,i) * T3(k,j),
-       T1(i,j) +=     T2(j,i) * T3(k,j),
-       T1(i,j)  = 3 * T2(j,i) * T3(j,l),
-       T1(i,j) += 3 * T2(j,i) * T3(j,l)
-       )
-      .execute();
-#endif
+  // sch
+  //     .tensors(T1,T2,T3)
+  //     .live_in(T2, T3)
+  //     .live_out(T1)
+  //     (  T1(i,j) = 0,
+  //         T1(i,j) += .52,
+  //         T1(i,j) = T2(j,i),
+  //         T1(i,j) += T2(j,i),
+  //         T1(i,j) = 3 * T2(j,i),
+  //         T1(i,j) += 3 * T2(j,i),
+  //         T1(i,j) = T2(i,k) * T3(k,j),
+  //         T1(i,j) += T2(i,k) * T3(k,j),
+  //         T1(i,j) = 3 * T2(i,l) * T4(j,l),
+  //         T1(i,j) += 3 * T2(i,l) * T4(j,l)
+  //      )
+  //     .execute();
+// #endif
   
   return 0;
 }

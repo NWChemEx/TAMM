6e200c4bb696122ace90821c869b928d29d62f64
Erdal Mutlu <ermutlu@ku.edu.tr>
2018-02-22 12:54:26 -0800

    clang-format updates and applied

--- src/tammy/ao.h
+++ src/tammy/ao.h
@@ -3,147 +3,108 @@
 #ifndef TAMMY_AO_H_
 #define TAMMY_AO_H_
 
-#include <vector>
 #include <numeric>
+#include <vector>
 
-#include "index_space.h"
-#include "types.h"
 #include "errors.h"
+#include "index_space.h"
 #include "strong_num_indexed_vector.h"
+#include "types.h"
 
 namespace tammy {
 
 class AO : public IndexSpace {
- public:
-  AO() = default; //@todo delete after debugging
-  AO(BlockCount no, BlockCount nv, 
-     const std::vector<Size>& sizes)
-      : no_{no},
-        nv_{nv},
-        sizes_{sizes} {
-          // EXPECTS(noa_ >=0 && nva_ >=0);
-          // EXPECTS(nob_ >=0 && nvb_ >= 0);
-          BlockCount nov = no_ + nv_;
-          EXPECTS(sizes_.size() == nov);          
-          offsets_.emplace_back(0);
-          std::partial_sum(sizes_.begin(), sizes_.end(), std::back_inserter(offsets_));
-          for(BlockIndex i{0}; i<nov; i++) {
-            block_indices_.push_back({i});
-          }
+    public:
+    AO() = default; //@todo delete after debugging
+    AO(BlockCount no, BlockCount nv, const std::vector<Size>& sizes) :
+      no_{no},
+      nv_{nv},
+      sizes_{sizes} {
+        // EXPECTS(noa_ >=0 && nva_ >=0);
+        // EXPECTS(nob_ >=0 && nvb_ >= 0);
+        BlockCount nov = no_ + nv_;
+        EXPECTS(sizes_.size() == nov);
+        offsets_.emplace_back(0);
+        std::partial_sum(sizes_.begin(), sizes_.end(),
+                         std::back_inserter(offsets_));
+        for(BlockIndex i{0}; i < nov; i++) { block_indices_.push_back({i}); }
+    }
+    Size size(BlockIndex bid) const override { return sizes_[bid]; }
+    Offset offset(BlockIndex bid) const override { return offsets_[bid]; }
+    int num_indep_indices() const override { return 0; }
+
+    Iterator begin(
+      RangeValue rv,
+      const TensorVec<IndexSpace::Iterator>& bdv = {}) const override {
+        EXPECTS(bdv.size() == 0);
+        auto ret = block_indices_.begin();
+        BlockCount off;
+        switch(rv) {
+            case range_n: off = BlockCount{0}; break;
+            case range_o: off = BlockCount{0}; break;
+            case range_v: off = no_; break;
+            case range_e: off = no_ + nv_; break;
+            default: UNREACHABLE();
         }
-  Size size(BlockIndex bid) const override {
-    return sizes_[bid];
-  }
-  Offset offset(BlockIndex bid) const override {
-    return offsets_[bid];
-  }
-  int num_indep_indices() const override {
-    return 0;
-  }
-
-  Iterator begin(RangeValue rv,
-                 const TensorVec<IndexSpace::Iterator>& bdv={}) const override {
-    EXPECTS(bdv.size() == 0);
-    auto ret = block_indices_.begin();
-    BlockCount off;
-    switch(rv) {
-      case range_n:
-        off = BlockCount{0};
-        break;
-      case range_o:
-        off = BlockCount{0};
-        break;
-      case range_v:
-        off = no_;
-        break;
-      case range_e:
-        off = no_ + nv_;
-        break;
-      default:
-        UNREACHABLE();
+        using size_type = decltype(block_indices_)::size_type;
+        return ret + off.template value<size_type>();
     }
-    using size_type = decltype(block_indices_)::size_type;
-    return ret + off.template value<size_type>();
-  }
 
-  Iterator end(RangeValue rv,
-               const TensorVec<IndexSpace::Iterator>& bdv={}) const override {
-    EXPECTS(bdv.size() == 0);
-    auto ret = block_indices_.begin();
-    BlockCount off;
-    switch(rv) {
-      case range_n:
-        off = no_ + nv_;
-        break;
-      case range_o:
-        off = no_;
-        break;
-      case range_v:
-        off = no_ + nv_;
-        break;
-      case range_e:
-        off = no_ + nv_;
-        break;
-      default:
-        UNREACHABLE();
+    Iterator end(RangeValue rv, const TensorVec<IndexSpace::Iterator>& bdv = {})
+      const override {
+        EXPECTS(bdv.size() == 0);
+        auto ret = block_indices_.begin();
+        BlockCount off;
+        switch(rv) {
+            case range_n: off = no_ + nv_; break;
+            case range_o: off = no_; break;
+            case range_v: off = no_ + nv_; break;
+            case range_e: off = no_ + nv_; break;
+            default: UNREACHABLE();
+        }
+        using size_type = decltype(block_indices_)::size_type;
+        return ret + off.template value<size_type>();
     }
-    using size_type = decltype(block_indices_)::size_type;
-    return ret + off.template value<size_type>();
-  }    
 
-  bool is_superset_of(RangeValue rv1,
-                      RangeValue rv2) const override {
-    return (rv1 & rv2) == rv2;
-  }
+    bool is_superset_of(RangeValue rv1, RangeValue rv2) const override {
+        return (rv1 & rv2) == rv2;
+    }
 
-  IndexRange NR() const override {
-    return {*this, range_n};
-  }
-  
-  IndexRange ER() const override {
-    return {*this, range_e};
-  }
+    IndexRange NR() const override { return {*this, range_n}; }
 
-  IndexRange OR() const {
-    return {*this, range_o};
-  }
-  
-  IndexRange VR() const {
-    return {*this, range_v};
-  }
+    IndexRange ER() const override { return {*this, range_e}; }
 
-  IndexLabel O(Label label) const {
-    return {OR(), label};
-  }
-  
-  IndexLabel V(Label label) const {
-    return {VR(), label};
-  }
+    IndexRange OR() const { return {*this, range_o}; }
 
-  template<typename... LabelArgs>
-  auto O(LabelArgs... labels) const {
-    return OR().labels(labels...);
-  }
+    IndexRange VR() const { return {*this, range_v}; }
 
-  template<typename... LabelArgs>
-  auto V(LabelArgs... labels) const {
-    return VR().labels(labels...);
-  }
+    IndexLabel O(Label label) const { return {OR(), label}; }
 
- protected:
-  BlockCount no_;
-  BlockCount nv_;  
-  std::vector<BlockIndex> block_indices_;
-  StrongNumIndexedVector<Size,BlockIndex> sizes_;
-  StrongNumIndexedVector<Offset,BlockIndex> offsets_;
+    IndexLabel V(Label label) const { return {VR(), label}; }
 
-  static const RangeValue range_e = 0x0;
-  static const RangeValue range_o = 0x1;
-  static const RangeValue range_v = 0x2;
-  static const RangeValue range_n = 0x3;
-};  // AO
+    template<typename... LabelArgs>
+    auto O(LabelArgs... labels) const {
+        return OR().labels(labels...);
+    }
 
+    template<typename... LabelArgs>
+    auto V(LabelArgs... labels) const {
+        return VR().labels(labels...);
+    }
 
-}  // namespace tammy
+    protected:
+    BlockCount no_;
+    BlockCount nv_;
+    std::vector<BlockIndex> block_indices_;
+    StrongNumIndexedVector<Size, BlockIndex> sizes_;
+    StrongNumIndexedVector<Offset, BlockIndex> offsets_;
 
-#endif  // TAMMY_AO_H_
+    static const RangeValue range_e = 0x0;
+    static const RangeValue range_o = 0x1;
+    static const RangeValue range_v = 0x2;
+    static const RangeValue range_n = 0x3;
+}; // AO
+
+} // namespace tammy
+
+#endif // TAMMY_AO_H_
--- src/tammy/types.h
+++ src/tammy/types.h
@@ -3,11 +3,11 @@
 #ifndef TAMMY_TYPES_H_
 #define TAMMY_TYPES_H_
 
-#include <iosfwd>
-#include <complex>
-#include "strong_num.h"
 #include "boundvec.h"
 #include "errors.h"
+#include "strong_num.h"
+#include <complex>
+#include <iosfwd>
 
 #include <mpi.h>
 
@@ -16,7 +16,7 @@ namespace tammy {
 struct IrrepSpace;
 using Irrep = StrongNum<IrrepSpace, uint32_t>;
 struct SpinSpace;
-using Spin = StrongNum<SpinSpace, uint32_t>;
+using Spin       = StrongNum<SpinSpace, uint32_t>;
 using TensorRank = uint32_t;
 struct OffsetSpace;
 using Offset = StrongNum<OffsetSpace, uint64_t>;
@@ -27,77 +27,61 @@ using Proc = StrongNum<ProcSpace, int64_t>;
 struct SignSpace;
 using Sign = StrongNum<SignSpace, int32_t>;
 
-//these are typedefs for usability
-using Size = Offset;
+// these are typedefs for usability
+using Size       = Offset;
 using BlockCount = BlockIndex;
-using Label = int;
+using Label      = int;
 
-enum class AllocationStatus {
-  invalid,
-  created,
-  attached
-};
+enum class AllocationStatus { invalid, created, attached };
 
 enum class ElementType {
-  invalid,
-  single_precision,
-  double_precision,
-  single_complex,
-  double_complex
+    invalid,
+    single_precision,
+    double_precision,
+    single_complex,
+    double_complex
 };
 
 template<typename T>
-constexpr ElementType
-tensor_element_type() {
-  return ElementType::invalid;
+constexpr ElementType tensor_element_type() {
+    return ElementType::invalid;
 }
 
 template<>
-constexpr ElementType
-tensor_element_type<double>() {
-  return ElementType::double_precision;
+constexpr ElementType tensor_element_type<double>() {
+    return ElementType::double_precision;
 }
 
 template<>
-constexpr ElementType
-tensor_element_type<float>() {
-  return ElementType::single_precision;
+constexpr ElementType tensor_element_type<float>() {
+    return ElementType::single_precision;
 }
 
 template<>
-constexpr ElementType
-tensor_element_type<std::complex<float>>() {
-  return ElementType::single_complex;
+constexpr ElementType tensor_element_type<std::complex<float>>() {
+    return ElementType::single_complex;
 }
 
 template<>
-constexpr ElementType
-tensor_element_type<std::complex<double>>() {
-  return ElementType::double_complex;
+constexpr ElementType tensor_element_type<std::complex<double>>() {
+    return ElementType::double_complex;
 }
 
-static inline constexpr size_t
-element_size(ElementType eltype) {
-  size_t ret = 0;
-  switch(eltype) {
-    case ElementType::single_precision:
-      ret = sizeof(float);
-      break;
-    case ElementType::double_precision:
-      ret = sizeof(double);
-      break;
-    case ElementType::single_complex:
-      ret = sizeof(std::complex<float>);
-      break;
-    case ElementType::double_complex:
-      ret = sizeof(std::complex<double>);
-      break;
-    default:
-      UNREACHABLE();
-  }
-  return ret;
+static inline constexpr size_t element_size(ElementType eltype) {
+    size_t ret = 0;
+    switch(eltype) {
+        case ElementType::single_precision: ret = sizeof(float); break;
+        case ElementType::double_precision: ret = sizeof(double); break;
+        case ElementType::single_complex:
+            ret = sizeof(std::complex<float>);
+            break;
+        case ElementType::double_complex:
+            ret = sizeof(std::complex<double>);
+            break;
+        default: UNREACHABLE();
+    }
+    return ret;
 }
-
 
 // enum class DimType {
 //   o  = 0b0011,
@@ -159,7 +143,7 @@ template<typename T>
 using TensorVec = BoundVec<T, maxrank>;
 
 using BlockDimVec = TensorVec<BlockIndex>;
-//using DimTypeVec = TensorVec<DimType>;
+// using DimTypeVec = TensorVec<DimType>;
 using PermVec = TensorVec<uint32_t>;
 
 ///////////
@@ -168,17 +152,16 @@ using PermVec = TensorVec<uint32_t>;
 
 using RangeValue = int64_t;
 
-//enum class IndexSpaceType { mo, mso, ao, aso, aux };
-enum class SpinMask { ignore, upper, lower};
-enum class IndexPosition { upper, lower, neither};
+// enum class IndexSpaceType { mo, mso, ao, aso, aux };
+enum class SpinMask { ignore, upper, lower };
+enum class IndexPosition { upper, lower, neither };
 
 //////////////////
 
 namespace SpinType {
 const Spin alpha{1};
 const Spin beta{2};
-}; //namespace SpinType
-
+}; // namespace SpinType
 
 // /**
 //  * @brief Signature of a tensor addition operation in NWChem TCE
@@ -193,7 +176,6 @@ const Spin beta{2};
 //                      MPI_Fint *tb, MPI_Fint *offsetb,
 //                      MPI_Fint *tc, MPI_Fint *offsetc);
 
-} //namespace tammy
+} // namespace tammy
 
-
-#endif  // TAMMY_TYPES_H_
+#endif // TAMMY_TYPES_H_

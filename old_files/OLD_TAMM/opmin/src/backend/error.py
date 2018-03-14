13c53295bb7a52cac78469a3a82b6c06dd6652d8
Ajay Panyala <ajay.panyala@gmail.com>
2018-03-14 15:25:41 -0700

    Remove OLD_TAMM subrepo

--- old_files/OLD_TAMM/opmin/src/backend/error.py
+++
@@ -1,8 +0,0 @@
-class BackEndError(Exception):
-
-    def __init__(self, message):
-        Exception.__init__(self)
-        self.message = message
-
-    def __str__(self):
-        return 'back-end error: ' + self.message

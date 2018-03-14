87524ec52b2a5de213791551c4d11edeb75d58bd
Ajay Panyala <ajay.panyala@gmail.com>
2018-03-14 15:25:41 -0700

    Remove OLD_TAMM subrepo
    Former-commit-id: 13c53295bb7a52cac78469a3a82b6c06dd6652d8
    Former-commit-id: d325c2395f46fc5a3aaffabe29eb13905ad6d4f8
    Former-commit-id: f69039c69c1a54313c8d611c3951d38a81a8f393
    Former-commit-id: 9d6e79d82fb905484c0a0230d2f3d17c44b8860e [formerly 992005a5f3e0f99b2efa9827b1e1d0b84ed095c7]
    Former-commit-id: 4809482f08370d368bd6f675c46efd4d6476528b

--- old_files/OLD_TAMM/opmin/src/ast/error.py
+++
@@ -1,8 +0,0 @@
-class ASTError(Exception):
-
-    def __init__(self, message):
-        Exception.__init__(self)
-        self.message = message
-
-    def __str__(self):
-        return 'abstract syntax tree error: ' + self.message

d6a030206be312931794696785e2bd7db5e78bfb
Ajay Panyala <ajay.panyala@gmail.com>
2018-03-14 17:33:48 -0700

    Move tammx tests out of nwtest

---
+++ tests/tammx/test_eigen.h
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------------
+// Copyright (C) 2016, Pacific Northwest National Laboratory
+// This software is subject to copyright protection under the laws of the
+// United States and other countries
+//
+// All rights in this computer software are reserved by the
+// Pacific Northwest National Laboratory (PNNL)
+// Operated by Battelle for the U.S. Department of Energy
+//
+//------------------------------------------------------------------------------
+//#include <iostream>
+//#include "gtest/gtest.h"
+
+//#include "macdecls.h"
+//
+//#include "tammx/tammx.h"
+//
+//#include <mpi.h>
+//#include <ga.h>
+//#include <macdecls.h>
+
+// namespace {
+// tammx::ExecutionContext* g_ec;
+// }
+
+// class TestEnvironment : public testing::Environment {
+//  public:
+//   explicit TestEnvironment(tammx::ExecutionContext* ec) {
+//     g_ec = ec;
+//   }
+// };
+
+#include "tammx/tammx.h"
+
+// namespace {
+//     tammx::ExecutionContext* g_ec;
+//     }
+    
+//     class TestEnvironment : public testing::Environment {
+//      public:
+//       explicit TestEnvironment(tammx::ExecutionContext* ec) {
+//         g_ec = ec;
+//       }
+//     };
+
+using namespace tammx::tensor_labels;
+
+

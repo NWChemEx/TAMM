74d2d866f9b1b7eeab53d90a68f4fcd82e4e4d6f
Sriram Krishnamoorthy <sriram@pnnl.gov>
2018-04-13 10:56:04 -0700

    more routines for CCSD

--- src/tammy/test_ccsd.cc
+++ src/tammy/test_ccsd.cc
@@ -109,82 +109,128 @@ template<typename T>
 void ccsd_t2(const TiledIndexSpace& MO, Tensor<T>& f1, Tensor<T>& i0,
              Tensor<T>& t1, Tensor<T>& t2, Tensor<T>& v2) {
 
-    const TiledIndexSpace& O = MO("occ");
-    const TiledIndexSpace& V = MO("virt");
+    const TiledIndexSpace &O = MO("occ");
+    const TiledIndexSpace &V = MO("virt");
 
-    Tensor<T> t2_2_1{O,V,O,O};
-    Tensor<T> t2_2_2_1{O,O,O,O} ;
-    Tensor<T> t2_2_2_2_1{O,O,O,V};
-    Tensor<T> t2_2_4_1{O,V};
-    Tensor<T> t2_2_5_1{O,O,O,V};
-    Tensor<T> t2_4_1{O,O};
-    Tensor<T> t2_4_2_1{O,V};
-    Tensor<T> t2_5_1{V,V};
-    Tensor<T> t2_6_1{O,O,O,O};
-    Tensor<T> t2_6_2_1{O,O,O,V};
-    Tensor<T> t2_7_1{O,V,O,V};
-    Tensor<T> vt1t1_1{O,V,O,O};
+    Tensor<T> t2_2_1{O, V, O, O};
+    Tensor<T> t2_2_2_1{O, O, O, O};
+    Tensor<T> t2_2_2_2_1{O, O, O, V};
+    Tensor<T> t2_2_4_1{O, V};
+    Tensor<T> t2_2_5_1{O, O, O, V};
+    Tensor<T> t2_4_1{O, O};
+    Tensor<T> t2_4_2_1{O, V};
+    Tensor<T> t2_5_1{V, V};
+    Tensor<T> t2_6_1{O, O, O, O};
+    Tensor<T> t2_6_2_1{O, O, O, V};
+    Tensor<T> t2_7_1{O, V, O, V};
+    Tensor<T> vt1t1_1{O, V, O, O};
 
-    TiledIndexLabel p1,p2,p3,p4,p5,p6,p7,p8,p9;
-    TiledIndexLabel h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11;
-    
-    std::tie(p1,p2,p3,p4,p5,p6,p7,p8,p9) = MO.range_labels<9>("virt");
-    std::tie(h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11) = MO.range_labels<11>("occ");
+    TiledIndexLabel p1, p2, p3, p4, p5, p6, p7, p8, p9;
+    TiledIndexLabel h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11;
+
+    std::tie(p1, p2, p3, p4, p5, p6, p7, p8, p9) = MO.range_labels<9>("virt");
+    std::tie(h1, h2, h3, h4, h5, h6, h7, h8, h9, h10, h11) = MO.range_labels<11>("occ");
 
 
     // Scheduler::allocate(t2_2_1, t2_2_2_1, t2_2_2_2_1, t2_2_4_1, t2_2_5_1, t2_4_1, t2_4_2_1,
     //         t2_5_1, t2_6_1, t2_6_2_1, t2_7_1, vt1t1_1);
 
-    i0(p3,p4,h1,h2)             =        v2(p3,p4,h1,h2);
-    t2_2_1(h10,p3,h1,h2)        =        v2(h10,p3,h1,h2);
-    t2_2_2_1(h10,h11,h1,h2)     = -1  *  v2(h10,h11,h1,h2);
-    t2_2_2_2_1(h10,h11,h1,p5)   =        v2(h10,h11,h1,p5);
-    t2_2_2_2_1(h10,h11,h1,p5)  += -0.5 * t1(p6,h1) * v2(h10,h11,p5,p6);
-    t2_2_2_1(h10,h11,h1,h2)    +=        t1(p5,h1) * t2_2_2_2_1(h10,h11,h2,p5);
-    t2_2_2_1(h10,h11,h1,h2)    += -0.5 * t2(p7,p8,h1,h2) * v2(h10,h11,p7,p8);
-    t2_2_1(h10,p3,h1,h2)       += 0.5  * t1(p3,h11) * t2_2_2_1(h10,h11,h1,h2);
-    t2_2_4_1(h10,p5)            =        f1(h10,p5);
-    t2_2_4_1(h10,p5)           += -1   * t1(p6,h7) * v2(h7,h10,p5,p6);
-    t2_2_1(h10,p3,h1,h2)       += -1   * t2(p3,p5,h1,h2) * t2_2_4_1(h10,p5);
-    t2_2_5_1(h7,h10,h1,p9)      =        v2(h7,h10,h1,p9);
-    t2_2_5_1(h7,h10,h1,p9)     +=        t1(p5,h1) * v2(h7,h10,p5,p9);
-    t2_2_1(h10,p3,h1,h2)       +=        t2(p3,p9,h1,h7) * t2_2_5_1(h7,h10,h2,p9);
-    t2(p1,p2,h3,h4)            += 0.5  * t1(p1,h3) * t1(p2,h4);
-    t2_2_1(h10,p3,h1,h2)       += 0.5  * t2(p5,p6,h1,h2) * v2(h10,p3,p5,p6);
-    t2(p1,p2,h3,h4)            += -0.5 * t1(p1,h3) * t1(p2,h4);
-    i0(p3,p4,h1,h2)            += -1   * t1(p3,h10) * t2_2_1(h10,p4,h1,h2);
-    i0(p3,p4,h1,h2)            += -1   * t1(p5,h1) * v2(p3,p4,h2,p5);
-    t2_4_1(h9,h1)               =        f1(h9,h1);
-    t2_4_2_1(h9,p8)             =        f1(h9,p8);
-    t2_4_2_1(h9,p8)            +=        t1(p6,h7) * v2(h7,h9,p6,p8);
-    t2_4_1(h9,h1)              +=        t1(p8,h1) * t2_4_2_1(h9,p8);
-    t2_4_1(h9,h1)              += -1   * t1(p6,h7) * v2(h7,h9,h1,p6);
-    t2_4_1(h9,h1)              += -0.5 * t2(p6,p7,h1,h8) * v2(h8,h9,p6,p7);
-    i0(p3,p4,h1,h2)            += -1   * t2(p3,p4,h1,h9) * t2_4_1(h9,h2);
-    t2_5_1(p3,p5)               =        f1(p3,p5);
-    t2_5_1(p3,p5)              += -1   * t1(p6,h7) * v2(h7,p3,p5,p6);
-    t2_5_1(p3,p5)              += -0.5 * t2(p3,p6,h7,h8) * v2(h7,h8,p5,p6);
-    i0(p3,p4,h1,h2)            += 1    * t2(p3,p5,h1,h2) * t2_5_1(p4,p5);
-    t2_6_1(h9,h11,h1,h2)        = -1   * v2(h9,h11,h1,h2);
-    t2_6_2_1(h9,h11,h1,p8)      =        v2(h9,h11,h1,p8);
-    t2_6_2_1(h9,h11,h1,p8)     += 0.5  * t1(p6,h1) * v2(h9,h11,p6,p8);
-    t2_6_1(h9,h11,h1,h2)       +=        t1(p8,h1) * t2_6_2_1(h9,h11,h2,p8);
-    t2_6_1(h9,h11,h1,h2)       += -0.5 * t2(p5,p6,h1,h2) * v2(h9,h11,p5,p6);
-    i0(p3,p4,h1,h2)            += -0.5 * t2(p3,p4,h9,h11) * t2_6_1(h9,h11,h1,h2);
-    t2_7_1(h6,p3,h1,p5)         =        v2(h6,p3,h1,p5);
-    t2_7_1(h6,p3,h1,p5)        += -1   * t1(p7,h1) * v2(h6,p3,p5,p7);
-    t2_7_1(h6,p3,h1,p5)        += -0.5 * t2(p3,p7,h1,h8) * v2(h6,h8,p5,p7);
-    i0(p3,p4,h1,h2)            += -1   * t2(p3,p5,h1,h6) * t2_7_1(h6,p4,h2,p5);
-    vt1t1_1(h5,p3,h1,h2)        = 0;
-    vt1t1_1(h5,p3,h1,h2)       += -2   * t1(p6,h1) * v2(h5,p3,h2,p6);
-    i0(p3,p4,h1,h2)            += -0.5 * t1(p3,h5) * vt1t1_1(h5,p4,h1,h2);
-    t2(p1,p2,h3,h4)            += 0.5  * t1(p1,h3) * t1(p2,h4);
-    i0(p3,p4,h1,h2)            += 0.5  * t2(p5,p6,h1,h2) * v2(p3,p4,p5,p6);
-    t2(p1,p2,h3,h4)            += -0.5 * t1(p1,h3) * t1(p2,h4);
+    i0(p3, p4, h1, h2) = v2(p3, p4, h1, h2);
+    t2_2_1(h10, p3, h1, h2) = v2(h10, p3, h1, h2);
+    t2_2_2_1(h10, h11, h1, h2) = -1 * v2(h10, h11, h1, h2);
+    t2_2_2_2_1(h10, h11, h1, p5) = v2(h10, h11, h1, p5);
+    t2_2_2_2_1(h10, h11, h1, p5) += -0.5 * t1(p6, h1) * v2(h10, h11, p5, p6);
+    t2_2_2_1(h10, h11, h1, h2) += t1(p5, h1) * t2_2_2_2_1(h10, h11, h2, p5);
+    t2_2_2_1(h10, h11, h1, h2) += -0.5 * t2(p7, p8, h1, h2) * v2(h10, h11, p7, p8);
+    t2_2_1(h10, p3, h1, h2) += 0.5 * t1(p3, h11) * t2_2_2_1(h10, h11, h1, h2);
+    t2_2_4_1(h10, p5) = f1(h10, p5);
+    t2_2_4_1(h10, p5) += -1 * t1(p6, h7) * v2(h7, h10, p5, p6);
+    t2_2_1(h10, p3, h1, h2) += -1 * t2(p3, p5, h1, h2) * t2_2_4_1(h10, p5);
+    t2_2_5_1(h7, h10, h1, p9) = v2(h7, h10, h1, p9);
+    t2_2_5_1(h7, h10, h1, p9) += t1(p5, h1) * v2(h7, h10, p5, p9);
+    t2_2_1(h10, p3, h1, h2) += t2(p3, p9, h1, h7) * t2_2_5_1(h7, h10, h2, p9);
+    t2(p1, p2, h3, h4) += 0.5 * t1(p1, h3) * t1(p2, h4);
+    t2_2_1(h10, p3, h1, h2) += 0.5 * t2(p5, p6, h1, h2) * v2(h10, p3, p5, p6);
+    t2(p1, p2, h3, h4) += -0.5 * t1(p1, h3) * t1(p2, h4);
+    i0(p3, p4, h1, h2) += -1 * t1(p3, h10) * t2_2_1(h10, p4, h1, h2);
+    i0(p3, p4, h1, h2) += -1 * t1(p5, h1) * v2(p3, p4, h2, p5);
+    t2_4_1(h9, h1) = f1(h9, h1);
+    t2_4_2_1(h9, p8) = f1(h9, p8);
+    t2_4_2_1(h9, p8) += t1(p6, h7) * v2(h7, h9, p6, p8);
+    t2_4_1(h9, h1) += t1(p8, h1) * t2_4_2_1(h9, p8);
+    t2_4_1(h9, h1) += -1 * t1(p6, h7) * v2(h7, h9, h1, p6);
+    t2_4_1(h9, h1) += -0.5 * t2(p6, p7, h1, h8) * v2(h8, h9, p6, p7);
+    i0(p3, p4, h1, h2) += -1 * t2(p3, p4, h1, h9) * t2_4_1(h9, h2);
+    t2_5_1(p3, p5) = f1(p3, p5);
+    t2_5_1(p3, p5) += -1 * t1(p6, h7) * v2(h7, p3, p5, p6);
+    t2_5_1(p3, p5) += -0.5 * t2(p3, p6, h7, h8) * v2(h7, h8, p5, p6);
+    i0(p3, p4, h1, h2) += 1 * t2(p3, p5, h1, h2) * t2_5_1(p4, p5);
+    t2_6_1(h9, h11, h1, h2) = -1 * v2(h9, h11, h1, h2);
+    t2_6_2_1(h9, h11, h1, p8) = v2(h9, h11, h1, p8);
+    t2_6_2_1(h9, h11, h1, p8) += 0.5 * t1(p6, h1) * v2(h9, h11, p6, p8);
+    t2_6_1(h9, h11, h1, h2) += t1(p8, h1) * t2_6_2_1(h9, h11, h2, p8);
+    t2_6_1(h9, h11, h1, h2) += -0.5 * t2(p5, p6, h1, h2) * v2(h9, h11, p5, p6);
+    i0(p3, p4, h1, h2) += -0.5 * t2(p3, p4, h9, h11) * t2_6_1(h9, h11, h1, h2);
+    t2_7_1(h6, p3, h1, p5) = v2(h6, p3, h1, p5);
+    t2_7_1(h6, p3, h1, p5) += -1 * t1(p7, h1) * v2(h6, p3, p5, p7);
+    t2_7_1(h6, p3, h1, p5) += -0.5 * t2(p3, p7, h1, h8) * v2(h6, h8, p5, p7);
+    i0(p3, p4, h1, h2) += -1 * t2(p3, p5, h1, h6) * t2_7_1(h6, p4, h2, p5);
+    vt1t1_1(h5, p3, h1, h2) = 0;
+    vt1t1_1(h5, p3, h1, h2) += -2 * t1(p6, h1) * v2(h5, p3, h2, p6);
+    i0(p3, p4, h1, h2) += -0.5 * t1(p3, h5) * vt1t1_1(h5, p4, h1, h2);
+    t2(p1, p2, h3, h4) += 0.5 * t1(p1, h3) * t1(p2, h4);
+    i0(p3, p4, h1, h2) += 0.5 * t2(p5, p6, h1, h2) * v2(p3, p4, p5, p6);
+    t2(p1, p2, h3, h4) += -0.5 * t1(p1, h3) * t1(p2, h4);
 
     // Scheduler::deallocate(t2_2_1, t2_2_2_1, t2_2_2_2_1, t2_2_4_1, t2_2_5_1, t2_4_1, t2_4_2_1,
     //          t2_5_1, t2_6_1, t2_6_2_1, t2_7_1, vt1t1_1);
 
+}
+class Scheduler;
+template<typename T>
+void jacobi(Scheduler& sch, Tensor<T>& d_r, Tensor<T>& d_t, T shift, bool transpose,
+            const T* const p_evl_sorted) {
+    //@todo implement
+}
+
+/**
+ *
+ * @tparam T
+ * @param MO
+ * @param p_evl_sorted
+ * @return pair of residual and energy
+ */
+template<typename T>
+std::pair<double,double> rest(ExecutionContext& ec,
+                              const TiledIndexSpace& MO,
+                              const Tensor<T>& d_r1,
+                              const Tensor<T>& d_r2,
+                              const Tensor<T>& de,
+                              const T* const p_evl_sorted) {
+    Scheduler sch{ec.scheduler()};
+    Tensor<T> d_r1_residual{}, d_r2_residual{};
+    sch
+      .allocate(d_r1_residual, d_r2_residual)
+      (d_r1_residual() = d_r1()  * d_r1())
+      (d_r2_residual() = d_r2()  * d_r2())
+      ( [&](Scheduler& sch) {
+        double r1, r2;
+        d_r1_residual.get({}, &r1, sizeof(double));
+        d_r2_residual.get({}, &r2, sizeof(double));
+        residual = std::max(0.5*std::sqrt(r1),
+                            0.5*std::sqrt(r2));
+        de.get({}, &energy, sizeof(double));
+      })
+      ( [&](Scheduler& sch) {
+        jacobi(sch, d_r1, d_t1, -1.0 * zshiftl, false, p_evl_sorted);
+      })
+      ( [&](Scheduler& sch) {
+        jacobi(sch, d_r2, d_t2, -2.0 * zshiftl, false, p_evl_sorted);
+      })
+      .deallocate(d_r1_residual, d_r2_residual)
+        .execute();
+      ;
+    return {residual, energy};
 }
 
 int main() {
@@ -208,11 +254,24 @@ int main() {
     Tensor<T> t2{V, V, O, O};
     Tensor<T> f1{N, N};
     Tensor<T> v2{N, N, N, N};
- 
+
+    TiledIndexSpace UnitTiledMO{MO_IS, 1};
+    Tensor<T> EVL{N};
+    //@todo Set EVL to have local distribution (one copy in each MPI rank)
+    Tensor<T>::allocate(EVL);
+
+    TiledIndexLabel n1;
+
+    std::tie(n1) = UnitTiledMO.range_labels<1>("all");
+    EVL(n1) = f1(n1, n1);
+    double *p_evl_sorted = EVL.access({0});
+    
+    double energy, residual;
     while(true) {
         ccsd_e(MO, de, t1, t2, f1, v2);
         ccsd_t1(MO, i1, t1, t2, f1, v2);
         ccsd_t2(MO, i2, t1, t2, f1, v2);
+        std::tie(residual, energy) = rest(ec, MO, i1, i2, de, p_evl_sorted);
     }
     return 0;
 }

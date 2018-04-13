1a33ecfee1743a62fd8d6737ba5660104a955e1c
Sriram Krishnamoorthy <sriram@pnnl.gov>
2018-04-13 10:10:44 -0700

    ccsd example
    Former-commit-id: 5e9593ebd67a6fc0aa02ea082da8815ce201acf4
    Former-commit-id: 0ad3d74291094fecc335fdcc1320cb86d9d4dcee
    Former-commit-id: a73c4a6af8c04a18ec01c4f03a72d5788817e3fb
    Former-commit-id: b5a922fb89db91c6f9752e777c9498d497f56eed [formerly 5b286785b47671f67b240a60d46d4cc9852dcc54]
    Former-commit-id: 639b1baff78b419e958b51aef1284b63f8fe6769

--- src/tammy/test_ccsd.cc
+++ src/tammy/test_ccsd.cc
@@ -1,6 +1,7 @@
 #include "tensor_sketch.h"
 
-using namespace tammy;
+using tammy::TiledIndexSpace;
+using tammy::Tensor;
 
 template<typename T>
 void ccsd_e(const TiledIndexSpace& MO, Tensor<T>& de, const Tensor<T>& t1,
@@ -22,23 +23,25 @@ void ccsd_e(const TiledIndexSpace& MO, Tensor<T>& de, const Tensor<T>& t1,
     de() += 0.25 * t2(p1, p2, h3, h4) * v2(h3, h4, p1, p2);
 }
 
-template<typename T>
-void driver_e() {
-    // Construction of tiled index space MO from skretch
-    IndexSpace MO_IS{range(0, 200),
-                     {{"occ", {range(0, 100)}}, {"virt", {range(100, 200)}}}};
-    TiledIndexSpace MO{MO_IS, 10};
-
-    const TiledIndexSpace& O = MO("occ");
-    const TiledIndexSpace& V = MO("virt");
-    const TiledIndexSpace& N = MO("all");
-    Tensor<T> de{};
-    Tensor<T> t1{V, O};
-    Tensor<T> t2{V, V, O, O};
-    Tensor<T> f1{N, N};
-    Tensor<T> v2{N, N, N, N};
-    ccsd_e(MO, de, t1, t2, f1, v2);
-}
+//template<typename T>
+//void driver_e(const TiledIndexSpace& MO) {
+//    // Construction of tiled index space MO from skretch
+////    IndexSpace MO_IS{range(0, 200),
+////                     {{"occ", {range(0, 100)}}, {"virt", {range(100, 200)}}}};
+////    TiledIndexSpace MO{MO_IS, 10};
+//
+//    const TiledIndexSpace& O = MO("occ");
+//    const TiledIndexSpace& V = MO("virt");
+//    const TiledIndexSpace& N = MO("all");
+//    Tensor<T> de{};
+//    Tensor<T> t1{V, O};
+//    Tensor<T> t2{V, V, O, O};
+//    Tensor<T> f1{N, N};
+//    Tensor<T> v2{N, N, N, N};
+//    Tensor<T>::allocate(de, t1, t2, f1, v2);
+//    ccsd_e(MO, de, t1, t2, f1, v2);
+//    Tensor<T>::deallocate(de, t1, t2, f1, v2);
+//}
 
 template<typename T>
 void ccsd_t1(const TiledIndexSpace& MO, Tensor<T>& i0, const Tensor<T>& t1,
@@ -57,6 +60,7 @@ void ccsd_t1(const TiledIndexSpace& MO, Tensor<T>& i0, const Tensor<T>& t1,
     std::tie(p2, p3, p4, p5, p6, p7) = MO.range_labels<6>("virt");
     std::tie(h1, h4, h5, h6, h7, h8) = MO.range_labels<6>("occ");
 
+    Tensor<T>::allocate(t1_2_1, t1_2_2_1, t1_3_1, t1_5_1, t1_6_1);
     i0(p2, h1)       = f1(p2, h1);
     t1_2_1(h7, h1)   = f1(h7, h1);
     t1_2_2_1(h7, p3) = f1(h7, p3);
@@ -76,29 +80,33 @@ void ccsd_t1(const TiledIndexSpace& MO, Tensor<T>& i0, const Tensor<T>& t1,
     t1_6_1(h4, h5, h1, p3) += -1 * t1(p6, h1) * v2(h4, h5, p3, p6);
     i0(p2, h1) += -0.5 * t2(p2, p3, h4, h5) * t1_6_1(h4, h5, h1, p3);
     i0(p2, h1) += -0.5 * t2(p3, p4, h1, h5) * v2(h5, p2, p3, p4);
+    Tensor<T>::deallocate(t1_2_1, t1_2_2_1, t1_3_1, t1_5_1, t1_6_1);
 }
 
-template<typename T>
-void driver_t1() {
-    // Construction of tiled index space MO from skretch
-    IndexSpace MO_IS{range(0, 200),
-                     {{"occ", {range(0, 100)}}, {"virt", {range(100, 200)}}}};
-    TiledIndexSpace MO{MO_IS, 10};
-
-    const TiledIndexSpace& O = MO("occ");
-    const TiledIndexSpace& V = MO("virt");
-    const TiledIndexSpace& N = MO("all");
-
-    Tensor<T> i0{};
-    Tensor<T> t1{V, O};
-    Tensor<T> t2{V, V, O, O};
-    Tensor<T> f1{N, N};
-    Tensor<T> v2{N, N, N, N};
-    ccsd_t1(MO, i0, t1, t2, f1, v2);
-}
+//template<typename T>
+//void driver_t1(TiledIn) {
+//    // Construction of tiled index space MO from skretch
+////    IndexSpace MO_IS{range(0, 200),
+////                     {{"occ", {range(0, 100)}}, {"virt", {range(100, 200)}}}};
+////    TiledIndexSpace MO{MO_IS, 10};
+//
+//    const TiledIndexSpace& O = MO("occ");
+//    const TiledIndexSpace& V = MO("virt");
+//    const TiledIndexSpace& N = MO("all");
+//
+//    Tensor<T> i0{};
+//    Tensor<T> t1{V, O};
+//    Tensor<T> t2{V, V, O, O};
+//    Tensor<T> f1{N, N};
+//    Tensor<T> v2{N, N, N, N};
+//    ccsd_t1(MO, i0, t1, t2, f1, v2);
+//}
 
 int main() {
-    driver_e<double>();
-    driver_t1<double>();
+    while(true) {
+        ccsd_e(MO, de, t1, t2, f1, v2);
+        ccsd_t1(MO, i1, t1, t2, f1, v2);
+        ccsd_t2(MO, i2, t1, t2, f1, v2);
+    }
     return 0;
 }

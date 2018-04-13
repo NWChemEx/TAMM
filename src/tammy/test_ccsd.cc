85375d8503d013119dd6269ffe059ba099ca4f35
Erdal Mutlu <ermutlu@ku.edu.tr>
2018-04-13 10:47:19 -0700

    added ccsd_t2
    Former-commit-id: 404c4e730109ef8ae6cdb961d7b8ac317a2a8ddf
    Former-commit-id: 0558a80d1763f7f023fba116b1a961ff01c08abb
    Former-commit-id: f829a46b527cbe35929834785147c2ba4641f2d7
    Former-commit-id: 4453d9b6d060790973b401017a642402b0310f1f [formerly 6b342420198b3241ad5a3ada957dfbca2a8f0bd0]
    Former-commit-id: c41ede89132b9bdf6098c11b6c518cac2c96947e

--- src/tammy/test_ccsd.cc
+++ src/tammy/test_ccsd.cc
@@ -1,7 +1,10 @@
 #include "tensor_sketch.h"
 
+using tammy::IndexSpace;
 using tammy::TiledIndexSpace;
+using tammy::TiledIndexLabel;
 using tammy::Tensor;
+using tammy::range;
 
 template<typename T>
 void ccsd_e(const TiledIndexSpace& MO, Tensor<T>& de, const Tensor<T>& t1,
@@ -60,12 +63,12 @@ void ccsd_t1(const TiledIndexSpace& MO, Tensor<T>& i0, const Tensor<T>& t1,
     std::tie(p2, p3, p4, p5, p6, p7) = MO.range_labels<6>("virt");
     std::tie(h1, h4, h5, h6, h7, h8) = MO.range_labels<6>("occ");
 
-    Tensor<T>::allocate(t1_2_1, t1_2_2_1, t1_3_1, t1_5_1, t1_6_1);
+    // Scheduler::allocate(t1_2_1, t1_2_2_1, t1_3_1, t1_5_1, t1_6_1);
     i0(p2, h1)       = f1(p2, h1);
     t1_2_1(h7, h1)   = f1(h7, h1);
     t1_2_2_1(h7, p3) = f1(h7, p3);
     t1_2_2_1(h7, p3) += -1 * t1(p5, h6) * v2(h6, h7, p3, p5);
-    t1_2_1(h7, h1) += t1(p3, h1) * t1_2_2_1(h7, p3);
+    t1_2_1(h7, h1)  += t1(p3, h1) * t1_2_2_1(h7, p3);
     t1_2_1(h7, h1) += -1 * t1(p4, h5) * v2(h5, h7, h1, p4);
     t1_2_1(h7, h1) += -0.5 * t2(p3, p4, h1, h5) * v2(h5, h7, p3, p4);
     i0(p2, h1) += -1 * t1(p2, h7) * t1_2_1(h7, h1);
@@ -80,7 +83,7 @@ void ccsd_t1(const TiledIndexSpace& MO, Tensor<T>& i0, const Tensor<T>& t1,
     t1_6_1(h4, h5, h1, p3) += -1 * t1(p6, h1) * v2(h4, h5, p3, p6);
     i0(p2, h1) += -0.5 * t2(p2, p3, h4, h5) * t1_6_1(h4, h5, h1, p3);
     i0(p2, h1) += -0.5 * t2(p3, p4, h1, h5) * v2(h5, p2, p3, p4);
-    Tensor<T>::deallocate(t1_2_1, t1_2_2_1, t1_3_1, t1_5_1, t1_6_1);
+    // Scheduler::deallocate(t1_2_1, t1_2_2_1, t1_3_1, t1_5_1, t1_6_1);
 }
 
 //template<typename T>
@@ -102,7 +105,110 @@ void ccsd_t1(const TiledIndexSpace& MO, Tensor<T>& i0, const Tensor<T>& t1,
 //    ccsd_t1(MO, i0, t1, t2, f1, v2);
 //}
 
+template<typename T>
+void ccsd_t2(const TiledIndexSpace& MO, Tensor<T>& f1, Tensor<T>& i0,
+             Tensor<T>& t1, Tensor<T>& t2, Tensor<T>& v2) {
+
+    const TiledIndexSpace& O = MO("occ");
+    const TiledIndexSpace& V = MO("virt");
+
+    Tensor<T> t2_2_1{O,V,O,O};
+    Tensor<T> t2_2_2_1{O,O,O,O} ;
+    Tensor<T> t2_2_2_2_1{O,O,O,V};
+    Tensor<T> t2_2_4_1{O,V};
+    Tensor<T> t2_2_5_1{O,O,O,V};
+    Tensor<T> t2_4_1{O,O};
+    Tensor<T> t2_4_2_1{O,V};
+    Tensor<T> t2_5_1{V,V};
+    Tensor<T> t2_6_1{O,O,O,O};
+    Tensor<T> t2_6_2_1{O,O,O,V};
+    Tensor<T> t2_7_1{O,V,O,V};
+    Tensor<T> vt1t1_1{O,V,O,O};
+
+    TiledIndexLabel p1,p2,p3,p4,p5,p6,p7,p8,p9;
+    TiledIndexLabel h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11;
+    
+    std::tie(p1,p2,p3,p4,p5,p6,p7,p8,p9) = MO.range_labels<9>("virt");
+    std::tie(h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11) = MO.range_labels<11>("occ");
+
+
+    // Scheduler::allocate(t2_2_1, t2_2_2_1, t2_2_2_2_1, t2_2_4_1, t2_2_5_1, t2_4_1, t2_4_2_1,
+    //         t2_5_1, t2_6_1, t2_6_2_1, t2_7_1, vt1t1_1);
+
+    i0(p3,p4,h1,h2)             =        v2(p3,p4,h1,h2);
+    t2_2_1(h10,p3,h1,h2)        =        v2(h10,p3,h1,h2);
+    t2_2_2_1(h10,h11,h1,h2)     = -1  *  v2(h10,h11,h1,h2);
+    t2_2_2_2_1(h10,h11,h1,p5)   =        v2(h10,h11,h1,p5);
+    t2_2_2_2_1(h10,h11,h1,p5)  += -0.5 * t1(p6,h1) * v2(h10,h11,p5,p6);
+    t2_2_2_1(h10,h11,h1,h2)    +=        t1(p5,h1) * t2_2_2_2_1(h10,h11,h2,p5);
+    t2_2_2_1(h10,h11,h1,h2)    += -0.5 * t2(p7,p8,h1,h2) * v2(h10,h11,p7,p8);
+    t2_2_1(h10,p3,h1,h2)       += 0.5  * t1(p3,h11) * t2_2_2_1(h10,h11,h1,h2);
+    t2_2_4_1(h10,p5)            =        f1(h10,p5);
+    t2_2_4_1(h10,p5)           += -1   * t1(p6,h7) * v2(h7,h10,p5,p6);
+    t2_2_1(h10,p3,h1,h2)       += -1   * t2(p3,p5,h1,h2) * t2_2_4_1(h10,p5);
+    t2_2_5_1(h7,h10,h1,p9)      =        v2(h7,h10,h1,p9);
+    t2_2_5_1(h7,h10,h1,p9)     +=        t1(p5,h1) * v2(h7,h10,p5,p9);
+    t2_2_1(h10,p3,h1,h2)       +=        t2(p3,p9,h1,h7) * t2_2_5_1(h7,h10,h2,p9);
+    t2(p1,p2,h3,h4)            += 0.5  * t1(p1,h3) * t1(p2,h4);
+    t2_2_1(h10,p3,h1,h2)       += 0.5  * t2(p5,p6,h1,h2) * v2(h10,p3,p5,p6);
+    t2(p1,p2,h3,h4)            += -0.5 * t1(p1,h3) * t1(p2,h4);
+    i0(p3,p4,h1,h2)            += -1   * t1(p3,h10) * t2_2_1(h10,p4,h1,h2);
+    i0(p3,p4,h1,h2)            += -1   * t1(p5,h1) * v2(p3,p4,h2,p5);
+    t2_4_1(h9,h1)               =        f1(h9,h1);
+    t2_4_2_1(h9,p8)             =        f1(h9,p8);
+    t2_4_2_1(h9,p8)            +=        t1(p6,h7) * v2(h7,h9,p6,p8);
+    t2_4_1(h9,h1)              +=        t1(p8,h1) * t2_4_2_1(h9,p8);
+    t2_4_1(h9,h1)              += -1   * t1(p6,h7) * v2(h7,h9,h1,p6);
+    t2_4_1(h9,h1)              += -0.5 * t2(p6,p7,h1,h8) * v2(h8,h9,p6,p7);
+    i0(p3,p4,h1,h2)            += -1   * t2(p3,p4,h1,h9) * t2_4_1(h9,h2);
+    t2_5_1(p3,p5)               =        f1(p3,p5);
+    t2_5_1(p3,p5)              += -1   * t1(p6,h7) * v2(h7,p3,p5,p6);
+    t2_5_1(p3,p5)              += -0.5 * t2(p3,p6,h7,h8) * v2(h7,h8,p5,p6);
+    i0(p3,p4,h1,h2)            += 1    * t2(p3,p5,h1,h2) * t2_5_1(p4,p5);
+    t2_6_1(h9,h11,h1,h2)        = -1   * v2(h9,h11,h1,h2);
+    t2_6_2_1(h9,h11,h1,p8)      =        v2(h9,h11,h1,p8);
+    t2_6_2_1(h9,h11,h1,p8)     += 0.5  * t1(p6,h1) * v2(h9,h11,p6,p8);
+    t2_6_1(h9,h11,h1,h2)       +=        t1(p8,h1) * t2_6_2_1(h9,h11,h2,p8);
+    t2_6_1(h9,h11,h1,h2)       += -0.5 * t2(p5,p6,h1,h2) * v2(h9,h11,p5,p6);
+    i0(p3,p4,h1,h2)            += -0.5 * t2(p3,p4,h9,h11) * t2_6_1(h9,h11,h1,h2);
+    t2_7_1(h6,p3,h1,p5)         =        v2(h6,p3,h1,p5);
+    t2_7_1(h6,p3,h1,p5)        += -1   * t1(p7,h1) * v2(h6,p3,p5,p7);
+    t2_7_1(h6,p3,h1,p5)        += -0.5 * t2(p3,p7,h1,h8) * v2(h6,h8,p5,p7);
+    i0(p3,p4,h1,h2)            += -1   * t2(p3,p5,h1,h6) * t2_7_1(h6,p4,h2,p5);
+    vt1t1_1(h5,p3,h1,h2)        = 0;
+    vt1t1_1(h5,p3,h1,h2)       += -2   * t1(p6,h1) * v2(h5,p3,h2,p6);
+    i0(p3,p4,h1,h2)            += -0.5 * t1(p3,h5) * vt1t1_1(h5,p4,h1,h2);
+    t2(p1,p2,h3,h4)            += 0.5  * t1(p1,h3) * t1(p2,h4);
+    i0(p3,p4,h1,h2)            += 0.5  * t2(p5,p6,h1,h2) * v2(p3,p4,p5,p6);
+    t2(p1,p2,h3,h4)            += -0.5 * t1(p1,h3) * t1(p2,h4);
+
+    // Scheduler::deallocate(t2_2_1, t2_2_2_1, t2_2_2_2_1, t2_2_4_1, t2_2_5_1, t2_4_1, t2_4_2_1,
+    //          t2_5_1, t2_6_1, t2_6_2_1, t2_7_1, vt1t1_1);
+
+}
+
 int main() {
+
+    // Construction of tiled index space MO from skretch
+    IndexSpace MO_IS{range(0, 200),
+                    {{"occ", {range(0, 100)}}, {"virt", {range(100, 200)}}}};
+    TiledIndexSpace MO{MO_IS, 10};
+
+    const TiledIndexSpace& O = MO("occ");
+    const TiledIndexSpace& V = MO("virt");
+    const TiledIndexSpace& N = MO("all");
+
+    using T = double;
+
+    Tensor<T> de{};
+    Tensor<T> i1{};
+    Tensor<T> i2{};
+
+    Tensor<T> t1{V, O};
+    Tensor<T> t2{V, V, O, O};
+    Tensor<T> f1{N, N};
+    Tensor<T> v2{N, N, N, N};
+ 
     while(true) {
         ccsd_e(MO, de, t1, t2, f1, v2);
         ccsd_t1(MO, i1, t1, t2, f1, v2);

139f0cfb5516a074bec66c2f6f93ae5cd812fd7b
Ajay Panyala <ajay.panyala@gmail.com>
2018-04-13 14:05:26 -0700

    Index Spaces documentation (#20)
    * Add Index Spaces notes to dox
    
    * updated docs for the comments
    
    * render latex in doxygen docs
    
    * Added new sketch to the repo
    
    * Added Spatial attribute
    
    * Updated IndexSpace with new design
    
    * Updated Attributes and added TiledIndexSpaces
    
    * Added validation checks for Attributes and updated the documentation
    
    * some todo notes
    
    * edits
    
    * test ccsd
    
    * Added CCSD_T1
    
    * Updates for the initial comments
    
    * Minor fix on example codes
    
    * Updates on index spaces
    
    * minor fix
    
    * minor update
    
    * Compiling tensor and operations
    
    * ccsd example
    
    * dummy EC, scheduler
    
    * added ccsd_t2
    
    * more routines for CCSD
    
    * main
    
    * added index_space()
    
    * mods
    
    * more on driver
    
    * edits
    
    * accessors and allocate functions
    
    * WIP
    
    * WIP
    
    * edits
    
    * added get method
    
    * compiles
    
    * compiles
    
    * remove mpi stuff
    
    * use scheduler in t1,t2
    
    * minor
    
    
    Former-commit-id: 612fcab61a373193f21dd93808a8357c3cab6510
    Former-commit-id: 7905590aa38b856f88b26935dd044ad1748dcdd0
    Former-commit-id: d5370d8a2a6e1baa6042e3001bae622292b9e5bf
    Former-commit-id: f80032cfb6dffce36ebb0481cf1cc43cd0e31990 [formerly 8a7a3d1fe058e00772f39456adbfc0de6fabc033]
    Former-commit-id: 8a78929695a7780320d3c3632903bd0a4852313a

--- src/tammy/test_index_space.cc
+++ src/tammy/test_index_space.cc
@@ -3,33 +3,37 @@
 // #include <iostream>
 
 #include "index_space_sketch.h"
-#include <typeinfo>
 
 using namespace tammy;
 
-void printIndices(IndexSpace is) {
+void printIndices(const IndexSpace& is) {
     std::cout << "Indices: ";
     for(const auto& point : is) { std::cout << point << " "; }
-    std::cout << std::endl << std::endl;
+    std::cout << std::endl;
 }
 
 int main() {
     // Construct an index space spanning from 0 to N-1 => [0,N)
-    IndexSpace is1{range(Index{10})}; // indicies = {0,1,2,3,4,5,6,7,8,9}
+    IndexSpace is1{range(10)}; // indicies = {0,1,2,3,4,5,6,7,8,9}
     std::cout << "Full is1 \t";
     printIndices(is1);
 
     // By specifying the indicies it represents -
-    // IndexSpace(std::initializer_list<Index> list)
-    IndexSpace is2{
-      {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-      {{"occ", {range(0, 5)}},
-       {"virt", {range(5, 10)}},
-       {"alpha", {range(0, 3), range(5, 8)}},
-       {"beta",
-        {range(3, 5), range(8, 10)}}}}; // indicies = {0,1,2,3,4,5,6,7,8,9}
+    // IndexSpace with subspace and attributes
+    // indicies = {0,1,2,3,4,5,6,7,8,9}
+    IndexSpace is2{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+                   {{"occ", {range(0, 5)}},
+                    {"virt", {range(5, 10)}},
+                    {"alpha", {range(0, 3), range(5, 8)}},
+                    {"beta", {range(3, 5), range(8, 10)}}},
+                   {{Spin{1}, {range(2, 5), range(7, 10)}},
+                    {Spin{2}, {range(0, 2), range(5, 7)}}}};
+
     std::cout << "Full is2 \t";
     printIndices(is2);
+
+    std::cout << "all_is2 \t";
+    printIndices(is2("all"));
 
     std::cout << "occ_is2 \t";
     printIndices(is2("occ"));
@@ -48,18 +52,18 @@ int main() {
     printIndices(is3);
 
     // By giving a range - IndexSpace(Range r)
-    IndexSpace is4{
-      range(Index{0}, Index{10})}; // indicies = {0,1,2,3,4,5,6,7,8,9}
+    IndexSpace is4{range(0, 10)}; // indicies = {0,1,2,3,4,5,6,7,8,9}
     std::cout << "Full is4 \t";
     printIndices(is4);
 
-    IndexSpace is5{range(Index{5}, Index{10})}; // indicies = {5,6,7,8,9}
+    IndexSpace is5{range(5, 10)}; // indicies = {5,6,7,8,9}
     std::cout << "Full is5 \t";
     printIndices(is5);
 
     // Constructing index spaces from other index spaces
     // IndexSpace(IndexSpace& is1, IndexSpace& is2)
-    IndexSpace is7{{is3, is5}}; // indicies = {0,1,2,3,4,5,6,7,8,9}
+    IndexSpace is7{
+      std::vector<IndexSpace>{is3, is5}}; // indicies = {0,1,2,3,4,5,6,7,8,9}
     std::cout << "Aggregated is7 \t";
     printIndices(is7);
     // printIndices(is7("occ"));
@@ -67,10 +71,9 @@ int main() {
     IndexSpace is8{
       {is5, is2("occ")},
       {"occ", "virt"},
-      {{"occ_alpha", {range(0, 3)}},
-       {"occ_beta", {range(3, 5)}},
-       {"virt_alpha", {range(5, 8)}},
-       {"virt_beta", {range(8, 10)}}}}; // indicies = {5,6,7,8,9,0,1,2,3,4}
+      {{"alpha", {range(0, 3), range(5, 8)}},
+       {"beta",
+        {range(3, 5), range(8, 10)}}}}; // indicies = {5,6,7,8,9,0,1,2,3,4}
 
     std::cout << "Aggregated is8 \t";
     printIndices(is8);
@@ -81,40 +84,44 @@ int main() {
     std::cout << "virt_is8 \t";
     printIndices(is8("virt"));
 
-    std::cout << "occ_alpha_is8 \t";
-    printIndices(is8("occ_alpha"));
+    std::cout << "alpha_is8 \t";
+    printIndices(is8("alpha"));
 
-    std::cout << "occ_beta_is8 \t";
-    printIndices(is8("occ_beta"));
+    std::cout << "beta_is8 \t";
+    printIndices(is8("beta"));
 
-    std::cout << "virt_alpha_is8 \t";
-    printIndices(is8("virt_alpha"));
+    IndexSpace temp_is{range(10, 20),
+                       {{"occ", {range(0, 5)}},
+                        {"virt", {range(5, 10)}},
+                        {"alpha", {range(0, 3), range(5, 8)}},
+                        {"beta", {range(3, 5), range(8, 10)}}},
+                       {{Spin{1}, {range(2, 5), range(7, 10)}},
+                        {Spin{2}, {range(0, 2), range(5, 7)}}}};
 
-    std::cout << "virt_beta_is8 \t";
-    printIndices(is8("virt_beta"));
+    IndexSpace is18{{temp_is, is2},
+                    {"occ", "virt"},
+                    {{"local", {range(2, 5)}}},
+                    {{"alpha", {"occ:alpha", "virt:alpha"}},
+                     {"beta", {"occ:beta", "virt:beta"}}}};
 
-#if 0
-  IndexSpace is18{ {is5, is2("occ")},
-                  {"occ", "virt"},
-                  {
-                    {"alpha_some",  {range(0,5)}},
-                    {"beta_some",  {range(5,10)}},
-                  },  
-                  {
-                    {"alpha", {"occ::alpha", "virt::alpha"}},
-                    {"beta",  {"occ::beta", "virt::beta"}}
-                  }
-                };
-#endif
+    std::cout << "Aggregated is18 \t";
+    printIndices(is18);
+    std::cout << "local_is18 \t";
+    printIndices(is18("local"));
+    std::cout << "alpha_is18 \t";
+    printIndices(is18("alpha"));
+    std::cout << "beta_is18 \t";
+    printIndices(is18("beta"));
 
     // Disjoint aggregation
     IndexSpace is9{{is3, is5}}; // indicies = {0,1,2,3,4,5,6,7,8,9}
     std::cout << "Aggregated is9 \t";
     printIndices(is9);
-    // Non-disjoint aggregation
-    IndexSpace is10{{is3, is3}}; // indicies = {0,1,2,3,4,0,1,2,3,4}
-    std::cout << "Aggregated is10 \t";
-    printIndices(is10);
+
+    // Non-disjoint aggregation - This will give an error as it is not allowed
+    // IndexSpace is10{{is3, is3}}; // indicies = {0,1,2,3,4,0,1,2,3,4}
+    // std::cout << "Aggregated is10 \t";
+    // printIndices(is10);
 
     // Sub-space by permuting the indicies of another index space
     // IndexSpace(IndexSpace& ref, range r)
@@ -135,6 +142,16 @@ int main() {
     printIndices(is12("occ"));
     std::cout << "virt_is12 \t";
     printIndices(is12("virt"));
+
+    TiledIndexSpace tis{is18, 10};
+
+    TiledIndexSpace tis2{tis, "occ"};
+
+    TiledIndexLabel i, j, k;
+    std::tie(i, j, k) = tis.range_labels<3>("occ", 4);
+    std::cout << "label i " << i.get_label() << std::endl;
+    std::cout << "label j " << j.get_label() << std::endl;
+    std::cout << "label k " << k.get_label() << std::endl;
 
     // Get the index value from an index space
     // By using point method - Point IndexSpace::point(Index i)

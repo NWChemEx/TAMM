85188e7d19eeb3687f77f3833b381fff91805ba0
Marcin Zalewski <marcin.zalewski@gmail.com>
2018-07-23 23:07:26 -0700

    Initial version of configurable execution for operations based on tammx_v3.
    Much of the functionality is copied from tammx_v3.  One of the immediately
    important points is that while execution *can* be configured, currently
    it is hardcoded to GA atomic counter.
    
    Three of the tests are failing, but they seem to be failing before this
    commit as well.
    
    
    Former-commit-id: 6e64cd57bdb1e435a7e3f048491d0f27e3e28f8b
    Former-commit-id: 39adecb192bd54db081aee470a0e72e1e68b6252
    Former-commit-id: 3fdcfccbfd725c90cf5f6511f919c7c52028e839
    Former-commit-id: af058ca90d39c1de50239959a4bc1dc5c804fd6e [formerly e1c4f7f8589affecbeb88d24f01e8e62c76136bb]
    Former-commit-id: 5a0a4f07fc92911bb2e034424e29c50a526a3d09

---
+++ src/tamm/work.hpp
@@ -0,0 +1,96 @@
+// Copyright 2018 Pacific Northwest National Laboratory
+
+#ifndef TAMM_WORK_HPP_
+#define TAMM_WORK_HPP_
+
+#include "tamm/atomic_counter.h"
+
+namespace tamm {
+
+/**
+ * Execution policy
+ */
+enum class ExecutionPolicy {
+  sequential_replicated, //<Sequential execution on all ranks
+  // sequential_single,     //<Sequential execution on one rank
+  parallel               //<Parallel distributed execution
+};
+
+/**
+ * @brief Parallel execution using GA atomic counters
+ * @tparam Itr Type of iterator
+ * @tparam Fn Function type to be applied on each iterated element
+ * @param first Begin task iterator
+ * @param last End task iterator
+ Loser* @param fn Function to be applied on each iterator element
+ */
+template<typename Itr, typename Fn>
+void
+parallel_work_ga(ProcGroup& ec_pg, Itr first, Itr last, Fn fn) {
+  AtomicCounter *ac = new AtomicCounterGA(ec_pg, 1);
+  ac->allocate(0);
+  int64_t next = ac->fetch_add(0, 1);
+  for(int64_t count=0; first != last; ++first, ++count) {
+    if(next == count) {
+      fn(*first);
+      next = ac->fetch_add(0, 1);
+    }
+  }
+  ec_pg.barrier();
+  ac->deallocate();
+  delete ac;
+}
+
+/**
+ * @brief Parallel execution
+ *
+ * @copydetails parallel_work_ga()
+ */
+template<typename Itr, typename Fn>
+void
+parallel_work(ProcGroup& ec_pg, Itr first, Itr last, Fn fn) {
+  parallel_work_ga(ec_pg, first, last, fn);
+  // Select other types of parallel work in some way
+}
+
+/**
+ * Sequential replicated execution of tasks
+ * @copydetails parallel_work()
+ */
+template<typename Itr, typename Fn>
+void
+seq_work(ProcGroup& ec_pg, Itr first, Itr last, Fn fn) {
+  //std::cerr<<pg.rank()<<" " <<__FILE__<<" "<<__LINE__<<" "<<__FUNCTION__<<"\n";
+  //std::for_each(first, last, fn);
+  for(; first != last; ++first) {
+    //std::cerr<<pg.rank()<<" " <<__FILE__<<" "<<__LINE__<<" "<<__FUNCTION__<<"\n";    
+    fn(*first);
+    //std::cerr<<pg.rank()<<" " <<__FILE__<<" "<<__LINE__<<" "<<__FUNCTION__<<"\n";    
+  }  
+  //std::cerr<<pg.rank()<<" " <<__FILE__<<" "<<__LINE__<<" "<<__FUNCTION__<<"\n";
+}
+
+/**
+ * @brief Execute iterator using the given execution policy
+ * @copydetails parallel_work()
+ * @param exec_policy Execution policy
+ */
+template<typename Itr, typename Fn>
+void
+do_work(ProcGroup& ec_pg, Itr first, Itr last, Fn fn, const ExecutionPolicy exec_policy = ExecutionPolicy::parallel) {
+  if(exec_policy == ExecutionPolicy::sequential_replicated) {
+    seq_work(ec_pg, first, last, fn);
+  } else {
+    parallel_work(ec_pg, first, last, fn);
+  }
+}
+
+template<typename Iterable, typename Fn>
+void
+do_work(ProcGroup& ec_pg, Iterable& iterable, Fn fn, const ExecutionPolicy exec_policy = ExecutionPolicy::parallel) {
+  do_work(ec_pg, iterable.begin(), iterable.end(), fn, exec_policy);
+}
+
+} // namespace tamm
+
+#endif  // TAMM_WORK_HPP_

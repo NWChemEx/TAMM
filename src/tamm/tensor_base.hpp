a64beb2181cb07ba5e441fee619f9dacfe24ca32
Ajay Panyala <ajay.panyala@gmail.com>
2018-07-30 13:37:21 -0700

    Undo prev changes
    Former-commit-id: aacb635df57301a6695ef86ddb8d723015264317
    Former-commit-id: 0769236c9ce88f761e11f7e1d9fdb6409eb15f3d
    Former-commit-id: 569126ff98ed2def1e8673bce88ae04ca0e2a117
    Former-commit-id: c2686f37364c49f47c60ca194136974eed927cbe [formerly 5b2f1a3c3e8adda91a3df0e9ed4171d22c9387a4]
    Former-commit-id: 99df3b05a8fa7baf4474541a27698f2c82d1126b

--- src/tamm/tensor_base.hpp
+++ src/tamm/tensor_base.hpp
@@ -81,11 +81,6 @@ public:
         tlabels_.insert(tlabels_.begin(), block_indices_[0].label(-1 - block_indices_.size()));
     }
 
-    template<class... Ts>
-    TensorBase(const TiledIndexLabel& til, Ts... rest) : TensorBase{rest...} {
-        block_indices_.insert(block_indices_.begin(), til.tiled_index_space());
-        tlabels_.insert(tlabels_.begin(), til);
-    }
 
     /**
      * @brief Construct a new TensorBase object from a single TiledIndexSpace

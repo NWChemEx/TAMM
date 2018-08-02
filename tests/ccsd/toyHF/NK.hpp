8098f8f8f54af58c395e85a1d49ffc55cdc5c4c7
Ajay Panyala <ajay.panyala@gmail.com>
2018-08-02 13:30:42 -0700

    NK compiles
    Former-commit-id: 0d09c11536add35481a1e9966383410bfc4a82b2
    Former-commit-id: 924a61b2fa1bb407004ef25acb1ac8fa1e7d9fdf
    Former-commit-id: 965e15339a00b42e2b7964ad70657a9432023884
    Former-commit-id: fc7c46a5539fe67a2255e4b2aeb1aa51dad7450a [formerly 77538d0e53c2dd16de64753fb439ca696265c555]
    Former-commit-id: 4a139e7f22444e996f1c6ef8b308b82bd5ae7ab2

--- tests/ccsd/toyHF/NK.hpp
+++ tests/ccsd/toyHF/NK.hpp
@@ -5,7 +5,17 @@ using namespace tamm;
 static const int NUM_NK_ITER = 6;
 
 template<typename T>
-void fn(const TiledIndexSpace& TIS) {
+void fn() {
+
+    ProcGroup pg{GA_MPI_Comm()};
+    auto mgr = MemoryManagerGA::create_coll(pg);
+    Distribution_NW distribution;
+    ExecutionContext *ec = new ExecutionContext{pg,&distribution,mgr};
+
+    IndexSpace IS{range(0, 10),
+                  {{"occ", {range(0, 5)}}, {"virt", {range(5, 10)}}}};
+    TiledIndexSpace TIS{IS, 1};
+
     // 1: k = 0;
     size_t k = 0;
 
@@ -19,18 +29,24 @@ void fn(const TiledIndexSpace& TIS) {
     Tensor<T> F1{n, n}, V2{n, n, n, n};
     Tensor<T> T1{a, i}, T2{a, b, i, j};
     Tensor<T> R1{a, i}, R2{a, b, i, j};
+
+    Tensor<T>::allocate(ec,F1,T1,V2,T2);
+
     Tensor<T> X1[NUM_NK_ITER], X2[NUM_NK_ITER];
 
     for(size_t i = 0; i < NUM_NK_ITER; i++) {
         X1[i] = Tensor<T>{a, i};
         X2[i] = Tensor<T>{a, b, i, j};
+        Tensor<T>::allocate(ec,X1[i],X2[i]);
     }
 
-    Tensor<T> res1{}, res2{};
+    Tensor<T> res{}, res1{}, res2{}, res1_tmp{};
     R1 = ccsd_t1(T1, T2, F1, V2);
     R2 = ccsd_t2(T1, T2, F1, V2);
 
-    Scheduler{}
+    Tensor<T>::allocate(ec,res,res1,res2,res1_tmp);
+
+    Scheduler{ec}
         (res1() = 0)
         (res2() = 0)
         (res1() += R1() * R1())
@@ -39,21 +55,26 @@ void fn(const TiledIndexSpace& TIS) {
         (res() += res2())
       .execute();
 
+    double res_local;
     //@todo apply sqrt() to res()
     res.get({}, &res_local);
     res_local = sqrt(res_local);
 
 
-    double res_local;
+    
+    Scheduler sch{ec};
     // res.get({}, &res_local);
     while(res_local > tol) {
         // 3: = ||R(T(k))||; -- ignore
 
         // 4: V (:; 1)   R(T(k))= ;
 
-        Scheduler{}(R1() /= res())(R2() /= res())(X1[0]() = R1())(X2[0]() =
-                                                                    R2())
-          .execute();
+        sch
+            (R1() /= res())
+            (R2() /= res())
+            (X1[0]() = R1())
+            (X2[0]() = R2())
+            .execute();
 
         const double h = 1.0e-2;
         // 5: for j = 1, 2,...maxgmiter do
@@ -67,13 +88,16 @@ void fn(const TiledIndexSpace& TIS) {
             R1_tmp = ccsd_t1(T1_tmp, T2_tmp, F1, V2);
             R2_tmp = ccsd_t2(T1_tmp, T2_tmp, F1, V2);
             // 7: W   (W 􀀀 R(T(k)))=h;
-            Scheduler{}(R1_tmp() -= R1())(R2_tmp() -= R2())(R1_tmp() /=
-                                                            h)(R2_tmp() /= h)
-              .execute();
+            sch
+                (R1_tmp() -= R1())
+                (R2_tmp() -= R2())
+                (R1_tmp() /= h)
+                (R2_tmp() /= h)
+                .execute();
             // 8: H(:; 1 : j)   V (:; 1 : j)TW;
             // 9: W   W 􀀀 V (:; 1 : j)h;
-            Tensor Htmp[NUM_NK_ITER][NUM_NK_ITER]; //all scalars
-            Scheduler sch;
+            Tensor<T> Htmp[NUM_NK_ITER][NUM_NK_ITER]; //all scalars
+            
             for(size_t j1=0; j1 < j; j1++) {
                 sch
                 (Htmp[j1][j] = X1[j1]() * R1_tmp())
@@ -83,14 +107,14 @@ void fn(const TiledIndexSpace& TIS) {
             }
             sch.execute();
             // 10: H(j + 1; j) = ||W||;
-            Scheduler{}
+            sch
             (res1_tmp() = R1_tmp() * R1_tmp())
             (res1_tmp() += R2_tmp() * R2_tmp())
             .execute();
             //@todo do res1_tmp() = sqrt(res1_tmp)
 
             // 11: V (:; j + 1) = W=H(j + 1; j);
-            Scheduler{}
+            sch
                 (Htmp[j+1][j]() = res1_tmp())
                 (X1[j+1]() = R1_tmp() / res1_tmp())
                 (X2[j+1]() = R2_tmp() / res1_tmp())
@@ -107,5 +131,12 @@ void fn(const TiledIndexSpace& TIS) {
         //16: Evaluate R(T(k)); 
         
         //17: end while
+    } //while
+
+    Tensor<T>::deallocate(F1,T1,V2,T2);
+    Tensor<T>::allocate(res,res1,res2,res1_tmp);
+
+    for(size_t i = 0; i < NUM_NK_ITER; i++) {
+        Tensor<T>::deallocate(X1[i],X2[i]);
     }
 }

ee2dba701cfa4f33651150e94e15012a9e25d6c3
Ajay Panyala <ajay.panyala@gmail.com>
2018-09-07 12:24:07 -0700

    HF driver as test
    Former-commit-id: 7f2db91b822b1440271dcc69e518aedf874e04c0
    Former-commit-id: dc767a2806c958075b18125e30769e0dbf02681f
    Former-commit-id: 0dffcb42cfaf9444632c7a4ddd91720f33998bed
    Former-commit-id: 0b86d71b2e0e03abda5d1d1e2549bec9c7dcd132 [formerly be6933de050fa297e08e8f33f5a93877e1d18853]
    Former-commit-id: 7fad079edfb173baaab73881c564fc1c033f76dc

--- tests/HF/Test_HartreeFock.cpp
+++ tests/HF/Test_HartreeFock.cpp
@@ -1,146 +1,66 @@
-#define CATCH_CONFIG_MAIN
-#include <catch/catch.hpp>
+#define CATCH_CONFIG_RUNNER
 
-#include <tamm/tamm.hpp>
+#include "HF/hartree_fock.hpp"
+#include "catch/catch.hpp"
+#include "tamm/tamm.hpp"
+#include "macdecls.h"
+#include "ga-mpi.h"
+
 
 using namespace tamm;
 
-using Atom = std::tuple<int, double, double, double>; // libint2::Atom
-using Contraction =
-  std::tuple<int, bool, std::vector<double>>; // libint2::Shell::Contraction
-using Shell =
-  std::tuple<std::vector<double>, std::vector<Contraction>,
-             std::array<double, 3>, std::vector<double>>; // libint2::Shell
 
-auto one_body_overlap_integral_lambda = [](auto& val) {};
-auto one_body_kinetic_integral_lambda = [](auto& val) {};
-auto one_body_nuclear_integral_lambda = [](auto& val) {};
-
-template<typename T>
-void compute_soad(const std::vector<Atom>& atoms, Tensor<T>& D) {}
-template<typename T>
-void compute_2body_fock(const std::vector<Shell>& shells, const Tensor<T>& D,
-                        Tensor<T>& F) {}
-template<typename T>
-auto eigen_solve(const Tensor<T>& F, const Tensor<T>& S) {
-    return std::make_tuple(Tensor<T>{}, double{});
-}
-
-template<typename T>
-void norm(const Tensor<T>& temp, const Tensor<T>& rmsd) {}
-
-template<typename T>
-double get_scalar(const Tensor<T>& tensor) {
-    return double{};
-}
-
-template<typename TensorType>
-void hartree_fock(ExecutionContext& ec, const TiledIndexSpace& AO,
-                  Tensor<TensorType>& C, Tensor<TensorType>& F) {
-    const std::vector<Atom> atoms;
-    const std::vector<Shell> shells;
-
-    Scheduler sch{ec};
-
-    const TiledIndexSpace& N = AO("all");
-    const TiledIndexSpace& O = AO("occ");
-
-    TiledIndexLabel i, j, k;
-    TiledIndexLabel io, jo, ko;
-
-    std::tie(i, j, k)    = AO.labels<3>("all");
-    std::tie(io, jo, ko) = AO.labels<3>("occ");
-
-    Tensor<TensorType> S{N, N, one_body_overlap_integral_lambda};
-    Tensor<TensorType> T{N, N, one_body_kinetic_integral_lambda};
-    Tensor<TensorType> V{N, N, one_body_nuclear_integral_lambda};
-
-    Tensor<TensorType> H{N, N};
-
-    sch.allocate(H, T, V)(H(i, j) = T(i, j))(H(i, j) += V(i, j)).execute();
-
-    Tensor<TensorType> D{N, N};
-
-    compute_soad(atoms, D);
-
-    const int maxiter{100};
-    const double conv{1e-12};
-    int iter{0};
-
-    Tensor<TensorType> ehf, ediff, rmsd;
-    Tensor<TensorType> eps{N, N};
-
-    do {
-        ++iter;
-        // Save a copy of energy and the density
-        Tensor<TensorType> ehf_last;
-        Tensor<TensorType> D_last{N, N};
-
-        sch
-          .allocate(ehf_last,
-                    D_last)(ehf_last() = ehf())(D_last(i, j) = D(i, j))
-          .execute();
-
-        // build a new Fock matrix
-        sch(F(i, j) = H(i, j)).execute();
-
-        compute_2body_fock(shells, D, F); // accumulate into F
-
-        // solve F C = e S C
-        // Eigen::GeneralizedSelfAdjointEigenSolver<Matrix> gen_eig_solver(F,
-        // S);  eps = gen_eig_solver(F,S).eigenvalues();  C =
-        // gen_eig_solver(F,S).eigenvectors();
-        double eps;
-        // @to-do un-comment once the eigen_solve is ready
-        // std::tie(C, eps) = eigen_solve(F, S);
-
-        // compute density, D = C(occ) . C(occ)T
-        // C_occ(ao,bo) = C(ao,bo); //C.leftCols(ndocc);
-        // C_occ_transpose(ao,bo) = C_occ(bo,ao);
-
-        sch(D(io, jo) = C(io, ko) * C(ko, jo)).execute();
-
-        Tensor<TensorType> tmp1{N, N}, tmp2{N, N};
-        // compute HF energy
-        // ehf() += D(i, j) * (H(i, j) + F(i, j));
-
-        sch
-          .allocate(tmp1, tmp2)(ehf() = 0.0)(tmp1(i, j) = H(i, j))(
-            tmp1(i, j) += F(i, j))(ehf() = D(i, j) * tmp1(i, j))
-          // compute difference with last iteration
-          (ediff() = ehf())(ediff() += -1.0 * ehf_last())(tmp2(i, j) = D(i, j))(
-            tmp2(i, j) += -1.0 * D_last(i, j))
-          .execute();
-
-        norm(tmp2, rmsd); // rmsd() = tmp2(a,b).norm();
-
-        sch(rmsd() = tmp2(i, j) * tmp2(i, j)).execute();
-        // e.g.:Tensor<TensorType> rmsd_local{AllocationModel::replicated};
-        // e.g.:rmsd_local(a) = rmsd(a);
-        // e.g.: rmsd(a) +=  rmsd_local(a);
-        // TODO: only put rmsd_local in process 0 to rmsd
-    } while(
-      ((fabs(get_scalar(ediff) > conv) || (fabs(get_scalar(rmsd)) > conv)) &&
-       (iter < maxiter)));
-}
+std::string filename;
 
 TEST_CASE("HartreeFock testcase") {
-    // Construction of tiled index space AO from scratch
-    IndexSpace AO_IS{range(0, 200),
-                     {{"occ", {range(0, 100)}}, {"virt", {range(100, 200)}}}};
+    using T = double;
 
-    TiledIndexSpace AO{AO_IS, 10};
+    Matrix C;
+    Matrix F;
+    // Tensor4D V2;
+    // TAMM_SIZE ov_alpha{0};
+    // TAMM_SIZE freeze_core    = 0;
+    // TAMM_SIZE freeze_virtual = 0;
 
-    const TiledIndexSpace& N = AO("all");
-    const TiledIndexSpace& O = AO("occ");
+    // double hf_energy{0.0};
+    // libint2::BasisSet shells;
+    // TAMM_SIZE nao{0};
 
-    Tensor<double> C{O, O};
-    Tensor<double> F{N, N};
+    auto hf_t1 = std::chrono::high_resolution_clock::now();
+    // std::tie(ov_alpha, nao, hf_energy, shells) = hartree_fock(filename, C, F);
+    CHECK_NOTHROW(hartree_fock(filename, C, F));
+    auto hf_t2 = std::chrono::high_resolution_clock::now();
 
-    //@todo construct C
-    //@todo construct F
+    double hf_time =
+      std::chrono::duration_cast<std::chrono::duration<double>>((hf_t2 - hf_t1)).count();
+    std::cout << "\nTime taken for Hartree-Fock: " << hf_time << " secs\n";
+}
 
-    ExecutionContext ec;
+int main( int argc, char* argv[] )
+{
+    if(argc<2){
+        std::cout << "Please provide an input file!\n";
+        return 1;
+    }
 
-    CHECK_NOTHROW(hartree_fock<double>(ec, AO, C, F));
+    filename = std::string(argv[1]);
+    std::ifstream testinput(filename); 
+    if(!testinput){
+        std::cout << "Input file provided [" << filename << "] does not exist!\n";
+        return 1;
+    }
+
+    MPI_Init(&argc,&argv);
+    GA_Initialize();
+    MA_init(MT_DBL, 8000000, 20000000);
+    
+    int mpi_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+
+    int res = Catch::Session().run();
+    
+    GA_Terminate();
+    MPI_Finalize();
+
+    return res;
 }

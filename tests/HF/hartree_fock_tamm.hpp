91a378ef56525dcbb1e2e39e9287cb3096434f06
Ajay Panyala <ajay.panyala@gmail.com>
2018-09-25 16:59:16 -0700

    TAMM HF works

--- tests/HF/hartree_fock_tamm.hpp
+++ tests/HF/hartree_fock_tamm.hpp
@@ -49,8 +49,8 @@ void diis(Matrix& F, Matrix& S, int iter, int max_hist,
 
 // an efficient Fock builder; *integral-driven* hence computes
 // permutationally-unique ints once
-// Matrix compute_2body_fock(const std::vector<libint2::Shell>& shells,
-//                           const Matrix& D);
+Matrix compute_2body_fock(const std::vector<libint2::Shell>& shells,
+                          const Matrix& D);
 
 template<typename T>
 std::ostream& operator<<(std::ostream& os, std::vector<T>& vec) {
@@ -250,6 +250,60 @@ auto print_2e(Args&&... args){
 ((std::cout << args << ", "), ...);
 }
 
+Matrix compute_1body_ints(const std::vector<libint2::Shell> &shells,
+                          libint2::Operator obtype,
+                          const std::vector<libint2::Atom> &atoms = std::vector<libint2::Atom>()){
+  using libint2::Shell;
+  using libint2::Engine;
+  using libint2::Operator;
+
+  const auto n = nbasis(shells);
+  Matrix result(n, n);
+
+  // construct the overlap integrals engine
+  Engine engine(obtype, max_nprim(shells), max_l(shells), 0);
+  // nuclear attraction ints engine needs to know where the charges sit ...
+  // the nuclei are charges in this case; in QM/MM there will also be classical charges
+  if (obtype == Operator::nuclear) {
+    std::vector<std::pair<double, std::array<double, 3>>> q;
+    for (const auto &atom : atoms) {
+      q.push_back({static_cast<double>(atom.atomic_number), {{atom.x, atom.y, atom.z}}});
+    }
+    engine.set_params(q);
+  }
+
+  auto shell2bf = map_shell_to_basis_function(shells);
+
+  // buf[0] points to the target shell set after every call  to engine.compute()
+  const auto &buf = engine.results();
+
+  // loop over unique shell pairs, {s1,s2} such that s1 >= s2
+  // this is due to the permutational symmetry of the real integrals over Hermitian operators: (1|2) = (2|1)
+  for (size_t s1 = 0; s1 != shells.size(); ++s1) {
+
+    auto bf1 = shell2bf[s1]; // first basis function in this shell
+    auto n1 = shells[s1].size();
+
+    for (size_t s2 = 0; s2 <= s1; ++s2) {
+
+      auto bf2 = shell2bf[s2];
+      auto n2 = shells[s2].size();
+
+      // compute shell pair; return is the pointer to the buffer
+      engine.compute(shells[s1], shells[s2]);
+
+      // "map" buffer to a const Eigen Matrix, and copy it to the corresponding blocks of the result
+      Eigen::Map<const Matrix> buf_mat(buf[0], n1, n2);
+      result.block(bf1, bf2, n1, n2) = buf_mat;
+      if (s1 != s2) // if s1 >= s2, copy {s1,s2} to the corresponding {s2,s1} block, note the transpose!
+        result.block(bf2, bf1, n2, n1) = buf_mat.transpose();
+
+    }
+  }
+
+  return result;
+}
+
 std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
   const string filename, Matrix& C, Matrix& F) {
     // Perform the simple HF calculation (Ed) and 2,4-index transform to get the
@@ -322,13 +376,7 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
 
         // buf[0] points to the target shell set after every call  to
         // engine.compute()
-        std::vector<std::pair<double, std::array<double, 3>>> q;
-        for(const auto& atom : atoms) {
-            q.push_back({static_cast<double>(atom.atomic_number),
-                         {{atom.x, atom.y, atom.z}}});
-        }
-        engine.set_params(q);
-
+      
         auto shell2bf   = map_shell_to_basis_function(shells);
         const auto& buf = engine.results();
 
@@ -461,7 +509,6 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
     auto mgr = MemoryManagerGA::create_coll(pg);
     Distribution_NW distribution;
     ExecutionContext* ec = new ExecutionContext{pg, &distribution, mgr};
-    Scheduler sch{ec};
 
     IndexSpace AO{range(0, N)};
     std::vector<unsigned int> AO_tiles;
@@ -477,16 +524,19 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
     Tensor<TensorType>::allocate(ec, H1);
 
 
-    // Core Hamiltonian = T + V
-    sch
-        (H1(mu, nu) = T1(mu, nu))
-        (H1(mu, nu) += V1(mu, nu))
-      .execute();
-
- 
     Matrix H, S; 
     H.setZero(N, N);
     S.setZero(N, N);
+
+    // auto T = compute_1body_ints(shells, Operator::kinetic);
+    // auto S = compute_1body_ints(shells, Operator::overlap);
+    // Matrix V = compute_1body_ints(shells, Operator::nuclear, atoms);
+    // Matrix H = T;
+
+    // Core Hamiltonian = T + V
+    Scheduler{ec}(H1(mu, nu) = T1(mu, nu)).execute();
+    //H =  H + V;
+    Scheduler{ec}(H1(mu, nu) += V1(mu, nu)).execute();
 
     tamm_to_eigen_tensor(H1, H);
     tamm_to_eigen_tensor(S1, S);
@@ -559,9 +609,6 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
     Tensor<TensorType>::allocate(ec, FSm12_tamm, Sp12D_tamm, SpFS_tamm,err_mat_tamm);
 
     eigen_to_tamm_tensor(D_tamm,D);
-    sch(ehf_tamm() = ehf)
-    (rmsd_tamm() = rmsd).execute();
-
     TensorType getehf_tamm = 0.0;
 
     F.setZero(N,N);
@@ -578,6 +625,8 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
     }
 
     do {
+
+        // Scheduler sch{ec};
         const auto tstart = std::chrono::high_resolution_clock::now();
         ++iter;
 
@@ -585,7 +634,7 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
         auto ehf_last = ehf;
         auto D_last   = D;
 
-        sch 
+        Scheduler{ec}(F1tmp() = 0)
            (D_last_tamm(mu,nu) = D_tamm(mu,nu)).execute();
 
         // build a new Fock matrix
@@ -887,12 +936,11 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
                 // TAMM------------------
 #endif
         block_for(ec->pg(), F1tmp(), comp_2bf_lambda);
-        // sch(F1tmp() = 0).execute();
         // eigen_to_tamm_tensor(F1tmp, Ftmp);
 
-        sch(F1(mu, nu) = H1(mu, nu)).execute();
         // F += Ftmp;
-        sch(F1(mu, nu) += F1tmp(mu, nu)).execute();
+        Scheduler{ec}(F1(mu, nu) = H1(mu, nu))
+                     (F1(mu, nu) += F1tmp(mu, nu)).execute();
 
         // S^-1/2
         Matrix Sm12 = S.pow(-0.5);
@@ -906,15 +954,13 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
         eigen_to_tamm_tensor(Sp12_tamm,Sp12);
         // eigen_to_tamm_tensor(D_last_tamm,D_last);
 
-        sch(FSm12_tamm() = 0)(FSm12_tamm(mu,nu) += F1(mu,ku) * Sm12_tamm(ku,nu)).execute();
-        sch(Sp12D_tamm() = 0)(Sp12D_tamm(mu,nu) += Sp12_tamm(mu,ku) * D_last_tamm(ku,nu)).execute();
-        sch(SpFS_tamm() = 0)(SpFS_tamm(mu,nu)  += Sp12D_tamm(mu,ku) * FSm12_tamm(ku,nu)).execute();
-
+        Scheduler{ec}(FSm12_tamm() = 0)(FSm12_tamm(mu,nu) += F1(mu,ku) * Sm12_tamm(ku,nu))
+        (Sp12D_tamm() = 0)(Sp12D_tamm(mu,nu) += Sp12_tamm(mu,ku) * D_last_tamm(ku,nu))
+        (SpFS_tamm() = 0)(SpFS_tamm(mu,nu)  += Sp12D_tamm(mu,ku) * FSm12_tamm(ku,nu))
         // Assemble: S^(-1/2)*F*D*S^(1/2) - S^(1/2)*D*F*S^(-1/2)
-        // Matrix err_mat = SpFS.transpose() - SpFS;
-        
-        sch(err_mat_tamm(mu,nu) = SpFS_tamm(nu,mu))
-           (err_mat_tamm(mu,nu) += -1.0 * SpFS_tamm(mu,nu)).execute();
+        // err_mat = SpFS.transpose() - SpFS;        
+        (err_mat_tamm(mu,nu) = SpFS_tamm(nu,mu))
+        (err_mat_tamm(mu,nu) += -1.0 * SpFS_tamm(mu,nu)).execute();
  
         tamm_to_eigen_tensor(F1,F);
         tamm_to_eigen_tensor(err_mat_tamm,err_mat);
@@ -924,11 +970,6 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
             diis(F, err_mat, iter, max_hist, idiis,
                 diis_hist, fock_hist);
         }
-
-        eigen_to_tamm_tensor(F1,F);
-        
-        // cout << "For F: ";
-        // compare_eigen_tamm_tensors(F1,F);
     
         // solve F C = e S C
         Eigen::GeneralizedSelfAdjointEigenSolver<Matrix>
@@ -943,11 +984,12 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
         D          = C_occ * C_occ.transpose();
 
         eigen_to_tamm_tensor(D_tamm,D);
+        eigen_to_tamm_tensor(F1,F);
 
         // compute HF energy 
         // e = D * (H+F);
         getehf_tamm = 0.0;
-        sch(ehf_tamm()=0)
+        Scheduler{ec}(ehf_tamm()=0)
            (ehf_tmp(mu,nu) = H1(mu,nu))
            (ehf_tmp(mu,nu) += F1(mu,nu))
            (ehf_tamm() += D_tamm() * ehf_tmp()).execute();
@@ -1044,5 +1086,135 @@ std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
         // cout << "-----------iter:" << iter << "--------------\n";
         // cout << err_mat << endl;
     }
+
+
+
+Matrix compute_2body_fock(const std::vector<libint2::Shell> &shells,
+                          const Matrix &D) {
+
+  using libint2::Shell;
+  using libint2::Engine;
+  using libint2::Operator;
+
+  std::chrono::duration<double> time_elapsed = std::chrono::duration<double>::zero();
+
+  const auto n = nbasis(shells);
+  Matrix G = Matrix::Zero(n, n);
+
+  // construct the 2-electron repulsion integrals engine
+  Engine engine(Operator::coulomb, max_nprim(shells), max_l(shells), 0);
+
+  auto shell2bf = map_shell_to_basis_function(shells);
+
+  const auto &buf = engine.results();
+
+  // The problem with the simple Fock builder is that permutational symmetries of the Fock,
+  // density, and two-electron integrals are not taken into account to reduce the cost.
+  // To make the simple Fock builder efficient we must rearrange our computation.
+  // The most expensive step in Fock matrix construction is the evaluation of 2-e integrals;
+  // hence we must minimize the number of computed integrals by taking advantage of their permutational
+  // symmetry. Due to the multiplicative and Hermitian nature of the Coulomb kernel (and realness
+  // of the Gaussians) the permutational symmetry of the 2-e ints is given by the following relations:
+  //
+  // (12|34) = (21|34) = (12|43) = (21|43) = (34|12) = (43|12) = (34|21) = (43|21)
+  //
+  // (here we use chemists' notation for the integrals, i.e in (ab|cd) a and b correspond to
+  // electron 1, and c and d -- to electron 2).
+  //
+  // It is easy to verify that the following set of nested loops produces a permutationally-unique
+  // set of integrals:
+  // foreach a = 0 .. n-1
+  //   foreach b = 0 .. a
+  //     foreach c = 0 .. a
+  //       foreach d = 0 .. (a == c ? b : c)
+  //         compute (ab|cd)
+  //
+  // The only complication is that we must compute integrals over shells. But it's not that complicated ...
+  //
+  // The real trick is figuring out to which matrix elements of the Fock matrix each permutationally-unique
+  // (ab|cd) contributes. STOP READING and try to figure it out yourself. (to check your answer see below)
+
+  // loop over permutationally-unique set of shells
+  for (size_t s1 = 0; s1 != shells.size(); ++s1) {
+
+    auto bf1_first = shell2bf[s1]; // first basis function in this shell
+    auto n1 = shells[s1].size();   // number of basis functions in this shell
+
+    for (size_t s2 = 0; s2 <= s1; ++s2) {
+
+      auto bf2_first = shell2bf[s2];
+      auto n2 = shells[s2].size();
+
+      for (size_t s3 = 0; s3 <= s1; ++s3) {
+
+        auto bf3_first = shell2bf[s3];
+        auto n3 = shells[s3].size();
+
+        const auto s4_max = (s1 == s3) ? s2 : s3;
+        for (size_t s4 = 0; s4 <= s4_max; ++s4) {
+
+          auto bf4_first = shell2bf[s4];
+          auto n4 = shells[s4].size();
+
+          // compute the permutational degeneracy (i.e. # of equivalents) of the given shell set
+          auto s12_deg = (s1 == s2) ? 1.0 : 2.0;
+          auto s34_deg = (s3 == s4) ? 1.0 : 2.0;
+          auto s12_34_deg = (s1 == s3) ? (s2 == s4 ? 1.0 : 2.0) : 2.0;
+          auto s1234_deg = s12_deg * s34_deg * s12_34_deg;
+
+          const auto tstart = std::chrono::high_resolution_clock::now();
+
+          engine.compute(shells[s1], shells[s2], shells[s3], shells[s4]);
+          const auto *buf_1234 = buf[0];
+          if (buf_1234 == nullptr)
+            continue; // if all integrals screened out, skip to next quartet
+
+          const auto tstop = std::chrono::high_resolution_clock::now();
+          time_elapsed += tstop - tstart;
+
+          // ANSWER
+          // 1) each shell set of integrals contributes up to 6 shell sets of the Fock matrix:
+          //    F(a,b) += (ab|cd) * D(c,d)
+          //    F(c,d) += (ab|cd) * D(a,b)
+          //    F(b,d) -= 1/4 * (ab|cd) * D(a,c)
+          //    F(b,c) -= 1/4 * (ab|cd) * D(a,d)
+          //    F(a,c) -= 1/4 * (ab|cd) * D(b,d)
+          //    F(a,d) -= 1/4 * (ab|cd) * D(b,c)
+          // 2) each permutationally-unique integral (shell set) must be scaled by its degeneracy,
+          //    i.e. the number of the integrals/sets equivalent to it
+          // 3) the end result must be symmetrized
+          for (size_t f1 = 0, f1234 = 0; f1 != n1; ++f1) {
+            const auto bf1 = f1 + bf1_first;
+            for (size_t f2 = 0; f2 != n2; ++f2) {
+              const auto bf2 = f2 + bf2_first;
+              for (size_t f3 = 0; f3 != n3; ++f3) {
+                const auto bf3 = f3 + bf3_first;
+                for (size_t f4 = 0; f4 != n4; ++f4, ++f1234) {
+                  const auto bf4 = f4 + bf4_first;
+
+                  const auto value = buf_1234[f1234];
+
+                  const auto value_scal_by_deg = value * s1234_deg;
+
+                  G(bf1, bf2) += D(bf3, bf4) * value_scal_by_deg;
+                  G(bf3, bf4) += D(bf1, bf2) * value_scal_by_deg;
+                  G(bf1, bf3) -= 0.25 * D(bf2, bf4) * value_scal_by_deg;
+                  G(bf2, bf4) -= 0.25 * D(bf1, bf3) * value_scal_by_deg;
+                  G(bf1, bf4) -= 0.25 * D(bf2, bf3) * value_scal_by_deg;
+                  G(bf2, bf3) -= 0.25 * D(bf1, bf4) * value_scal_by_deg;
+                }
+              }
+            }
+          }
+
+        }
+      }
+    }
+  }
+
+  // symmetrize the result and return
+  Matrix Gt = G.transpose();
+  return 0.5 * (G + Gt);
+}
 
 #endif // TAMM_TESTS_HF_TAMM_HPP_

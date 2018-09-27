ca3d95bb36443539697e4544d031c6afb83bd14d
Ajay Panyala <ajay.panyala@gmail.com>
2018-09-27 15:36:08 -0700

    Add eigensolver to build
    Former-commit-id: 03c553d7c87797a52c1d32b34fe60641add06422
    Former-commit-id: 496288867ecc4baf0ca07462e744493edafdd747
    Former-commit-id: 97f86b20d183419586831bab2b8aa80eb07055f2
    Former-commit-id: f0cbd948615415bda2143b8bd72a27ab4c74f46e [formerly cc636c11084060a302960902f676adb2facf73ae]
    Former-commit-id: cd10db4201874945d34b56b2c1d9bbaba2494841

--- eigensolver/include/sisubit.hpp
+++ eigensolver/include/sisubit.hpp
@@ -10,10 +10,10 @@ using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 MatrixXd cholQR(const MatrixXd &S, const MatrixXd &X0);
-#ifdef USE_MPI
-MatrixXd sisubit(MPI_Comm comm, const MatrixXd &HS, const MatrixXd &S, MatrixXd &X0, int *ipiv, int maxiter);
-#else
+#ifdef NO_MPI
 MatrixXd sisubit(const MatrixXd &HS, const MatrixXd &S, MatrixXd &X0, int *ipiv, int maxiter);
+#else
+MatrixXd sisubit(MPI_Comm comm, const MatrixXd &HS, const MatrixXd &S, MatrixXd &X0, int *ipiv, int maxiter);
 #endif
 MatrixXd RayleighRitz(const MatrixXd &H, const MatrixXd &X0);
 VectorXd GetEigvals(const MatrixXd &H, const MatrixXd &X);

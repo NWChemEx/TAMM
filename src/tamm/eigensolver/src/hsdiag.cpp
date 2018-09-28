83ab5cffd7ac3cd2f43adfad1afa0344ab29ec77
Chao Yang <CYang@lbl.gov>
2018-09-28 08:54:41 -0700

    use LAPACK to diagonalize if the dimension of the Fock matrix is less than 500
    Former-commit-id: 304fc5e4b92abfadd90e599b0d3ee5f8a75c790b
    Former-commit-id: 2dbabdeffcddadc1ce10122dcf445995b81ecd5c
    Former-commit-id: 399660a0070c2ac715d2c0f303a19a1f6adeabb0
    Former-commit-id: fab7064d3aeb744c92b203a92c23c2b97fa34233 [formerly c92420b888b8f0d3c9e1e60bf00ab107162c5b77]
    Former-commit-id: f3c8bd51668f50e4983c4e8dceeba71912117cc3

--- src/tamm/eigensolver/src/hsdiag.cpp
+++ src/tamm/eigensolver/src/hsdiag.cpp
@@ -13,11 +13,11 @@ void hsdiag(MPI_Comm comm, int iterscf, Matrix &H, Matrix &S, int nev, Matrix &e
    MPI_Comm_rank(comm, &rank);
    double t1 = MPI_Wtime();
 
+   const int limit = 500;
+
    // change to column major order
    MatrixXd HC, SC;
 
-   if (rank == 0) cout << "running spectrum slicing" << endl;
-   if (rank == 0) resultsfile.open("results.txt");
 
    int n = H.rows();
    int nshifts = std::ceil(nev/10.0);
@@ -28,62 +28,84 @@ void hsdiag(MPI_Comm comm, int iterscf, Matrix &H, Matrix &S, int nev, Matrix &e
    HC = H;
    SC = S;
 
-   shifts = getshifts(comm, HC, SC, nev, nshifts, &maxcnt);
-   subdim = 2*maxcnt;
+   if (n <= limit) {
+      // just call LAPACK eigensolver
+      VectorXd ev1(n);
 
-   if (rank == 0) {
-      std::cout << "nshifts = " << nshifts << std::endl;
-      std::cout << "nev = " << nev << std::endl;
-      std::cout << "subdim = " << subdim << std::endl;
+      char  lower = 'U', needv = 'V';
+      int   gtype = 1, lgvdwork = n*n, info = 0, ierr;
+      double *gvdwork;
+      gvdwork = new double[lgvdwork];
+
+      dsygv_(&gtype,&needv,&lower,&n,HC.data(),&n,SC.data(),&n,ev1.data(),
+             gvdwork,&lgvdwork,&ierr);
+      delete [] gvdwork;
+      // convert evals to a Matrix
+      Map<Matrix> evalmat1(ev1.data(),n,1); 
+      eps = evalmat1;
+      evecs = HC;
    }
+   else {
 
-   // set up probes 
-   SpectralProbe *SPs;
-   SPs = new SpectralProbe[nshifts];
-   Init_SPs(comm, SPs, shifts);
+      if (rank == 0) cout << "running spectrum slicing" << endl;
+      if (rank == 0) resultsfile.open("results.txt");
 
-   VectorXi *inds = NULL;
+      shifts = getshifts(comm, HC, SC, nev, nshifts, &maxcnt);
+      subdim = 2*maxcnt;
 
-   int maxiter = 5;
-   logOFS << "rank " << rank << " starting slicing" << endl;
-   slicing(comm, H, S, n, nev, subdim, maxiter, nshifts, SPs);
-   logOFS << "rank " << rank << " done slicing" << endl;
-   MPI_Barrier(comm);
+      if (rank == 0) {
+         std::cout << "nshifts = " << nshifts << std::endl;
+         std::cout << "nev = " << nev << std::endl;
+         std::cout << "subdim = " << subdim << std::endl;
+      }
 
-   sync_slices(comm, &nshifts, SPs);
+      // set up probes 
+      SpectralProbe *SPs;
+      SPs = new SpectralProbe[nshifts];
+      Init_SPs(comm, SPs, shifts);
 
-   // returns the indices of the selcted eigenvalues in inds
-   // and the total number of selected eigenvalues in nev
-   evalidate(comm, SPs, nshifts, n, nev);
+      VectorXi *inds = NULL;
 
-   // collect eigenvalues and residuals into a single array for printing
-   // this may change depending on how what type of information is
-   // needed by SCF
-   VectorXd evals;
-   evals.resize(nev);
-   MatrixXd evecmat;
-   evecmat.resize(n,nev);
-   VectorXd resnrms;
-   resnrms.resize(nev);
+      int maxiter = 5; // hard code for now
+      logOFS << "rank " << rank << " starting slicing" << endl;
+      slicing(comm, H, S, n, nev, subdim, maxiter, nshifts, SPs);
+      logOFS << "rank " << rank << " done slicing" << endl;
+      MPI_Barrier(comm);
+
+      sync_slices(comm, &nshifts, SPs);
+
+      // returns the indices of the selcted eigenvalues in inds
+      // and the total number of selected eigenvalues in nev
+      evalidate(comm, SPs, nshifts, n, nev);
+
+      // collect eigenvalues and residuals into a single array for printing
+      // this may change depending on how what type of information is
+      // needed by SCF
+      VectorXd evals;
+      evals.resize(nev);
+      MatrixXd evecmat;
+      evecmat.resize(n,nev);
+      VectorXd resnrms;
+      resnrms.resize(nev);
   
-   // nevf maybe less than nev
-   int nevf = collectevs(comm, nshifts, SPs, evals, evecmat, resnrms); 
+      // nevf maybe less than nev
+      int nevf = collectevs(comm, nshifts, SPs, evals, evecmat, resnrms); 
  
-   if (rank == 0) {
-      VectorXd evalsf = evals.head(nevf);
-      VectorXd resnrmf = resnrms.head(nevf);
-      print_results(resultsfile,0,evalsf,resnrmf);
-   }
+      if (rank == 0) {
+         VectorXd evalsf = evals.head(nevf);
+         VectorXd resnrmf = resnrms.head(nevf);
+         print_results(resultsfile,0,evalsf,resnrmf);
+      }
 
-   MPI_Barrier(comm);
+      MPI_Barrier(comm);
 
-   if (rank == 0) resultsfile.close();
+      if (rank == 0) resultsfile.close();
 
-   // convert evals to a Matrix
-   Map<Matrix> evalmat(evals.data(),nevf,1); 
-   eps = evalmat;
-   evecs = evecmat.block(0,0,n,nevf);
-
+      // convert evals to a Matrix
+      Map<Matrix> evalmat(evals.data(),nevf,1); 
+      eps = evalmat;
+      evecs = evecmat.block(0,0,n,nevf);
+   } // endif (n <= 500)
    double t2 = MPI_Wtime();
-   if (rank == 0) cout << "ELASPSED TIME: " << t2-t1 << endl;
+   // if (rank == 0) cout << "ELASPSED TIME: " << t2-t1 << endl;
 }

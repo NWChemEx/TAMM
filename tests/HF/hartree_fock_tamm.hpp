9956dd8b23c2c9533dc8849c0c99fbf0f7b9ff2d
Ajay Panyala <ajay.panyala@gmail.com>
2018-09-29 20:54:07 -0700

    Minor cleanup

--- tests/HF/hartree_fock_tamm.hpp
+++ tests/HF/hartree_fock_tamm.hpp
@@ -252,60 +252,6 @@ auto print_2e(Args&&... args){
 ((std::cout << args << ", "), ...);
 }
 
-Matrix compute_1body_ints(const std::vector<libint2::Shell> &shells,
-                          libint2::Operator obtype,
-                          const std::vector<libint2::Atom> &atoms = std::vector<libint2::Atom>()){
-  using libint2::Shell;
-  using libint2::Engine;
-  using libint2::Operator;
-
-  const auto n = nbasis(shells);
-  Matrix result(n, n);
-
-  // construct the overlap integrals engine
-  Engine engine(obtype, max_nprim(shells), max_l(shells), 0);
-  // nuclear attraction ints engine needs to know where the charges sit ...
-  // the nuclei are charges in this case; in QM/MM there will also be classical charges
-  if (obtype == Operator::nuclear) {
-    std::vector<std::pair<double, std::array<double, 3>>> q;
-    for (const auto &atom : atoms) {
-      q.push_back({static_cast<double>(atom.atomic_number), {{atom.x, atom.y, atom.z}}});
-    }
-    engine.set_params(q);
-  }
-
-  auto shell2bf = map_shell_to_basis_function(shells);
-
-  // buf[0] points to the target shell set after every call  to engine.compute()
-  const auto &buf = engine.results();
-
-  // loop over unique shell pairs, {s1,s2} such that s1 >= s2
-  // this is due to the permutational symmetry of the real integrals over Hermitian operators: (1|2) = (2|1)
-  for (size_t s1 = 0; s1 != shells.size(); ++s1) {
-
-    auto bf1 = shell2bf[s1]; // first basis function in this shell
-    auto n1 = shells[s1].size();
-
-    for (size_t s2 = 0; s2 <= s1; ++s2) {
-
-      auto bf2 = shell2bf[s2];
-      auto n2 = shells[s2].size();
-
-      // compute shell pair; return is the pointer to the buffer
-      engine.compute(shells[s1], shells[s2]);
-
-      // "map" buffer to a const Eigen Matrix, and copy it to the corresponding blocks of the result
-      Eigen::Map<const Matrix> buf_mat(buf[0], n1, n2);
-      result.block(bf1, bf2, n1, n2) = buf_mat;
-      if (s1 != s2) // if s1 >= s2, copy {s1,s2} to the corresponding {s2,s1} block, note the transpose!
-        result.block(bf2, bf1, n2, n1) = buf_mat.transpose();
-
-    }
-  }
-
-  return result;
-}
-
 std::tuple<int, int, double, libint2::BasisSet> hartree_fock(
   const string filename, Matrix& C, Matrix& F) {
     // Perform the simple HF calculation (Ed) and 2,4-index transform to get the

90521f243cca5e4145fab7cd1cef99a3dc91e820
Erdal Mutlu <erdalmutlu.su@gmail.com>
2018-10-24 14:45:44 -0700

    split up `utils.hpp`

--- src/tamm/work.hpp
+++ src/tamm/work.hpp
@@ -103,22 +103,6 @@ void block_for(ExecutionContext& ec, LabeledTensor<T> ltc, Lambda func,
     }
 }
 
-
-template<typename T, typename Func> 
-void fill(LabeledTensor<T> lt, Func lambda){
-    LabelLoopNest loop_nest{lt.labels()};
-
-    for(const auto& itval : loop_nest) {
-        const IndexVector blockid = internal::translate_blockid(itval, lt);  
-        size_t size               = lt.tensor().block_size(blockid);
-        std::vector<T> buf(size);
-
-        lambda(blockid, buf);
-
-        lt.tensor().put(blockid, buf);
-    }  
-}
-
 } // namespace tamm
 
 #endif // TAMM_WORK_HPP_

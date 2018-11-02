dea9f11b415cf7c544cbf9e0b512d916b41d9e9e
Marcin Zalewski <marcin.zalewski@gmail.com>
2018-11-02 14:22:55 -0700

    WIP: Working on changes to rutime engine.
    Simplifying things a bit.  Let's assume that permissions are separate from work
    on buffers.  We request permissions, and then we get buffers later using runtime
    engine interface.

--- src/tamm/runtime_engine.hpp
+++ src/tamm/runtime_engine.hpp
@@ -1,6 +1,60 @@
 #ifndef TAMM_EXECUTION_ENGINE_HPP_
 #define TAMM_EXECUTION_ENGINE_HPP_
 
+#include "utility"
+#include "tamm/types.hpp"
+#include "tamm/tensor.hpp"
+
+namespace tamm {
+
+template<typename T>
+using IndexedTensor = std::pair<tensor<T>, IndexVector>;
+
+enum class AccessMode {
+    KR, KW, KRW, AC
+};
+
+template<typename T> class Permission;
+
+template<typename T>
+class Permission<LabeledTensor<T>> {
+    Permission(LabeledTensor<T> lt, AccessMode acc) : lt(lt), acc(acc) {}
+private:
+    LabeledTensor lt;
+    AccessMode acc;
+};
+
+template<typename T>
+class Permission<IndexedTensor<T>> {
+    Permission(IndexedTensor<T> lt, AccessMode acc) : lt(lt) {}
+    Permission(IndexedTensor<T>::typename first_type tensor, 
+               IndexedTensor<T>::typename second_type index_vector,
+               AccessMode acc) : lt(tensor, index_vector), acc(acc) {}
+private:
+    IndexedTensor lt;
+    AccessMode acc;
+};
+
+template<typename T>
+class ReadAccess : public Permission<T> {
+    ReadAccess(T t) : Permission(T t, AccessMode::KR) {}
+}
+
+template<typename T>
+class ReadWriteAccess : public Permission<T> {
+    ReadWriteAccess(T t) : Permission(T t, AccessMode::KRW) {}
+}
+
+template<typename T>
+class WriteAccess : public Permission<T> {
+    WriteAccess(T t) : Permission(T t, AccessMode::KW) {}
+}
+
+template<typename T>
+class AccumAccess : public Permission<T> {
+    AccumAccess(T t) : Permission(T t, AccessMode::AC) {}
+}
+
 class RuntimeEngine {
 public:
     RuntimeEngine() = default;
@@ -11,7 +65,8 @@ public:
     void executeAllthreads();
 
 private:
-    TaskEngine* te;
 };
+
+} // namespace tamm
 
 #endif // TAMM_EXECUTION_ENGINE_HPP_

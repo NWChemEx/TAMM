772e31f5521091ec7d1db53f253d3c9f8d533a56
Ajay Panyala <ajay.panyala@gmail.com>
2018-11-05 19:15:33 -0800

    small update

--- tests/HF/hartree_fock_eigen.hpp
+++ tests/HF/hartree_fock_eigen.hpp
@@ -65,6 +65,12 @@ Matrix compute_schwarz_ints(
     typename libint2::operator_traits<Kernel>::oper_params_type params =
         libint2::operator_traits<Kernel>::default_params());
 
+
+template <libint2::Operator obtype, typename OperatorParams = 
+typename libint2::operator_traits<obtype>::oper_params_type>
+std::array<Matrix, libint2::operator_traits<obtype>::nopers> compute_1body_ints(
+    const libint2::BasisSet& obs, OperatorParams oparams = OperatorParams());
+
 // an Fock builder that can accept densities expressed a separate basis
 Matrix compute_2body_fock_general(
     const libint2::BasisSet& obs, const Matrix& D, const libint2::BasisSet& D_bs,
@@ -236,56 +242,60 @@ inline std::tuple<std::vector<Atom>, std::string, int, double, double, double, i
   return std::make_tuple(atoms, basis_set, maxiter, tol_int, conve, convd, diis_hist);
 }
 
-Matrix compute_1body_ints(const std::vector<libint2::Shell> &shells,
-                          libint2::Operator obtype,
-                          const std::vector<libint2::Atom> &atoms = std::vector<libint2::Atom>()){
-  using libint2::Shell;
-  using libint2::Engine;
-  using libint2::Operator;
+template <libint2::Operator obtype, typename OperatorParams>
+std::array<Matrix, libint2::operator_traits<obtype>::nopers> compute_1body_ints(
+    const libint2::BasisSet& obs, OperatorParams oparams) {
+  const auto n = obs.nbf();
+  const auto nshells = obs.size();
+  typedef std::array<Matrix, libint2::operator_traits<obtype>::nopers>
+      result_type;
+  const unsigned int nopers = libint2::operator_traits<obtype>::nopers;
+  result_type result;
+  for (auto& r : result) r = Matrix::Zero(n, n);
 
-  const auto n = nbasis(shells);
-  Matrix result(n, n);
-
-  // construct the overlap integrals engine
-  Engine engine(obtype, max_nprim(shells), max_l(shells), 0);
+  // construct the 1-body integrals engine
+  libint2::Engine engine = libint2::Engine(obtype, obs.max_nprim(), obs.max_l(), 0);
+  // pass operator params to the engine, e.g.
   // nuclear attraction ints engine needs to know where the charges sit ...
-  // the nuclei are charges in this case; in QM/MM there will also be classical charges
-  if (obtype == Operator::nuclear) {
-    std::vector<std::pair<double, std::array<double, 3>>> q;
-    for (const auto &atom : atoms) {
-      q.push_back({static_cast<double>(atom.atomic_number), {{atom.x, atom.y, atom.z}}});
+  // the nuclei are charges in this case; in QM/MM there will also be classical
+  // charges
+  engine.set_params(oparams);
+
+  auto shell2bf = obs.shell2bf();
+
+    const auto& buf = engine.results();
+
+    // loop over unique shell pairs, {s1,s2} such that s1 >= s2
+    // this is due to the permutational symmetry of the real integrals over
+    // Hermitian operators: (1|2) = (2|1)
+    for (auto s1 = 0l, s12 = 0l; s1 != nshells; ++s1) {
+      auto bf1 = shell2bf[s1];  // first basis function in this shell
+      auto n1 = obs[s1].size();
+
+      auto s1_offset = s1 * (s1+1) / 2;
+      for (auto s2: obs_shellpair_list[s1]) {
+        auto s12 = s1_offset + s2;
+        //if (s12 % nthreads != thread_id) continue;
+
+        auto bf2 = shell2bf[s2];
+        auto n2 = obs[s2].size();
+
+        auto n12 = n1 * n2;
+
+        // compute shell pair; return is the pointer to the buffer
+        engine.compute(obs[s1], obs[s2]);
+
+        for (unsigned int op = 0; op != nopers; ++op) {
+          // "map" buffer to a const Eigen Matrix, and copy it to the
+          // corresponding blocks of the result
+          Eigen::Map<const Matrix> buf_mat(buf[op], n1, n2);
+          result[op].block(bf1, bf2, n1, n2) = buf_mat;
+          if (s1 != s2)  // if s1 >= s2, copy {s1,s2} to the corresponding
+                         // {s2,s1} block, note the transpose!
+            result[op].block(bf2, bf1, n2, n1) = buf_mat.transpose();
+        }
+      }
     }
-    engine.set_params(q);
-  }
-
-  auto shell2bf = map_shell_to_basis_function(shells);
-
-  // buf[0] points to the target shell set after every call  to engine.compute()
-  const auto &buf = engine.results();
-
-  // loop over unique shell pairs, {s1,s2} such that s1 >= s2
-  // this is due to the permutational symmetry of the real integrals over Hermitian operators: (1|2) = (2|1)
-  for (size_t s1 = 0; s1 != shells.size(); ++s1) {
-
-    auto bf1 = shell2bf[s1]; // first basis function in this shell
-    auto n1 = shells[s1].size();
-
-    for (size_t s2 = 0; s2 <= s1; ++s2) {
-
-      auto bf2 = shell2bf[s2];
-      auto n2 = shells[s2].size();
-
-      // compute shell pair; return is the pointer to the buffer
-      engine.compute(shells[s1], shells[s2]);
-
-      // "map" buffer to a const Eigen Matrix, and copy it to the corresponding blocks of the result
-      Eigen::Map<const Matrix> buf_mat(buf[0], n1, n2);
-      result.block(bf1, bf2, n1, n2) = buf_mat;
-      if (s1 != s2) // if s1 >= s2, copy {s1,s2} to the corresponding {s2,s1} block, note the transpose!
-        result.block(bf2, bf1, n2, n1) = buf_mat.transpose();
-
-    }
-  }
 
   return result;
 }
@@ -564,25 +574,10 @@ std::tuple<int,int, double, libint2::BasisSet> hartree_fock(const string filenam
 
   auto hf_t1 = std::chrono::high_resolution_clock::now();
 
-  // compute overlap integrals
-  auto S = compute_1body_ints(shells, Operator::overlap);
-  // Eigen::EigenSolver<Matrix> diagS(S);
-  // cout << "S eigenvalues\n";
-  // cout << S.eigenvalues() << endl;
-
-//  cout << "\n\tOverlap Integrals:\n";
-//  cout << S << endl;
-
-  // compute kinetic-energy integrals
-  auto T = compute_1body_ints(shells, Operator::kinetic);
-//  cout << "\n\tKinetic-Energy Integrals:\n";
-//  cout << T << endl;
-
-  // compute nuclear-attraction integrals
-  Matrix V = compute_1body_ints(shells, Operator::nuclear, atoms);
-//  cout << "\n\tNuclear Attraction Integrals:\n";
-//  cout << V << endl;
-
+  // compute one-body integrals
+  auto S = compute_1body_ints<libint2::Operator::overlap>(shells)[0];
+  auto T = compute_1body_ints<libint2::Operator::kinetic>(shells)[0];
+  auto V = compute_1body_ints<libint2::Operator::nuclear>(shells, libint2::make_point_charges(atoms))[0];
 
   // Core Hamiltonian = T + V
   Matrix H = T + V;

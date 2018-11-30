8f326bcb2a6f7d3d34046a6cade167a46abe6dc7
Chao Yang <CYang@lbl.gov>
2018-11-30 14:28:49 -0800

    change LAPACK/BLAS calls from FORTRAN interface to C interface

--- src/tamm/eigensolver/src/cholQR.cpp
+++ src/tamm/eigensolver/src/cholQR.cpp
@@ -10,9 +10,12 @@ MatrixXd cholQR(const MatrixXd &S, const MatrixXd &X0)
    char   upper = 'U', right = 'R', notrans = 'N', nunit = 'N';
    double done = 1.0;
    int    ierr = 0;
-   dpotrf_(&upper, &ncols, G.data(), &ncols, &ierr);   
+   // dpotrf_(&upper, &ncols, G.data(), &ncols, &ierr);   
+   ierr = LAPACKE_dpotrf(LAPACK_COL_MAJOR,upper, ncols, G.data(), ncols);
    X = X0;
-   dtrsm_(&right, &upper, &notrans, &nunit, &n, &ncols, &done, G.data(), &ncols, X.data(), &n);  
+   // dtrsm_(&right, &upper, &notrans, &nunit, &n, &ncols, &done, G.data(), &ncols, X.data(), &n);  
+   cblas_dtrsm(CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit,
+               n, ncols, done, G.data(), ncols, X.data(), n);  
 
 //   LLT<MatrixXd> cholfac(G);
 //   MatrixXd R = cholfac.matrixU();

08c51fa4d2c194a8e0d84054ce07cb1efbc991cc
ryanmrichard <ryanmrichard1@gmail.com>
2018-12-07 10:53:12 -0600

    ghetto MPI support

---
+++ cmake/FindMPI.cmake
@@ -0,0 +1,21 @@
+set(mpi_languages C CXX)
+foreach(mpi_lang ${mpi_langugages})
+    _cpp_is_true(have_lang ${mpi_lang})
+    if(have_lang)
+       list(APPEND MPI_LIBRARIES ${MPI_${mpi_lang}_LIBRARIES})
+       list(APPEND MPI_INCLUDE_DIRS ${MPI_${mpi_lang}_INCLUDE_DIRS})
+       set(comp_flags MPI_${mpi_lang}_COMPILE_FLAGS)
+       if(NOT "${comp_flags}" STREQUAL "")
+           set()
+           string(REPLACE " " ";" ${comp_flags} ${comp_flags})
+       endif()
+       list(APPEND MPI_LINK_FLAGS ${MPI_${mpi_lang}_LINK_FLAGS})
+    endif()
+endforeach()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    MPI
+    DEFAULT_MSG
+    MPI_LIBRARIES MPI_INCLUDE_DIRS
+)

19df355fabf6b68bf83f69c747e903ffca9ae657
ryanmrichard <ryanmrichard1@gmail.com>
2018-12-07 12:08:37 -0600

    bare bones MPI find

--- cmake/FindMPI.cmake
+++ cmake/FindMPI.cmake
@@ -1,21 +1,12 @@
-set(mpi_languages C CXX)
-foreach(mpi_lang ${mpi_languages})
-    _cpp_is_true(have_lang ${mpi_lang})
-    if(have_lang)
-       list(APPEND MPI_LIBRARIES ${MPI_${mpi_lang}_LIBRARIES})
-       list(APPEND MPI_INCLUDE_DIRS ${MPI_${mpi_lang}_INCLUDE_DIRS})
-       set(comp_flags MPI_${mpi_lang}_COMPILE_FLAGS)
-       if(NOT "${comp_flags}" STREQUAL "")
-           set()
-           string(REPLACE " " ";" ${comp_flags} ${comp_flags})
-       endif()
-       list(APPEND MPI_LINK_FLAGS ${MPI_${mpi_lang}_LINK_FLAGS})
-    endif()
-endforeach()
+find_path(MPI_INCLUDE_DIR mpi.h)
+find_library(MPI_LIBRARY NAMES mpi)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-    MPI
-    DEFAULT_MSG
-    MPI_LIBRARIES MPI_INCLUDE_DIRS
+    MPI DEFAULT_MSG
+    MPI_LIBRARY MPI_INCLUDE_DIR
 )
+
+set(MPI_LIBRARIES ${MPI_LIBRARY})
+set(MPI_INCLUDE_DIRS ${MPI_INCLUDE_DIR})
+

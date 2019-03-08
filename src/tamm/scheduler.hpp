3729011302d751e0d1c013117e8d31063abb2821
Erdal Mutlu <erdalmutlu.su@gmail.com>
2019-03-07 17:16:51 -0800

    adds OP based leveling for parallel execution

--- src/tamm/scheduler.hpp
+++ src/tamm/scheduler.hpp
@@ -7,10 +7,12 @@
 #include "tamm/execution_context.hpp"
 #include "tamm/ops.hpp"
 #include "tamm/tensor.hpp"
+#include "ga-mpi.h"
 
 namespace tamm {
 
 using internal::DAGImpl;
+
 /**
  * @brief Scheduler to execute a list of operations.
  * @ingroup operations
@@ -61,12 +63,104 @@ public:
         return deallocate(tensors...);
     }
 
-    void execute() {
-        // for(auto& op : ops_) { op->execute(ec()->pg()); }
-        for(size_t i = start_idx_; i < ops_.size(); i++) {
-            ops_[i]->execute(ec());
-            start_idx_++;
+    template <typename T>
+    bool has_intersect(const std::vector<T>& lhs,
+                       const std::vector<T>& rhs) {
+        
+        for(const auto& l_item : lhs) {
+            for(const auto& r_item : rhs) {
+                if(l_item == r_item)
+                    return true;
+            }
         }
+        return false;
+    }
+    bool has_dependence(const std::vector<TensorBase*>& R1,
+                        const std::vector<TensorBase*>& W1,
+                        const std::vector<TensorBase*>& R2,
+                        const std::vector<TensorBase*>& W2) {
+        
+        return (has_intersect(R1, W2) || 
+                has_intersect(W1, R2) ||
+                has_intersect(W1, W2) );
+    }
+
+    std::vector<size_t> levelize(const std::vector<std::shared_ptr<Op>>& ops,
+                                 size_t start_id,
+                                 size_t end_id) {
+        EXPECTS(start_id >= 0 && start_id <= ops.size());
+        EXPECTS(end_id >= start_id && end_id <= ops.size());
+
+        std::vector<size_t> groups;
+
+        size_t group_start = start_id;
+        std::vector<TensorBase*> group_reads, group_writes;
+        for(size_t i = start_id; i < end_id; i++) {
+            std::vector<TensorBase*> reads, writes;
+            reads = std::vector<TensorBase*>(ops[i]->reads());
+            writes = std::vector<TensorBase*>{ops[i]->writes()};
+            
+            if(ops[i]->is_memory_barrier() ||
+               has_dependence(group_reads, group_writes, reads, writes)) {
+            
+                groups.push_back(i - group_start);
+                group_start = i;
+                group_reads = reads;
+                group_writes = writes;
+            
+            } else {
+                group_reads.insert(group_reads.end(), reads.begin(), reads.end());
+                group_writes.insert(group_writes.end(), writes.begin(), writes.end());
+            }
+        }
+
+        if(group_start < end_id) {
+            groups.push_back(end_id - group_start);
+        }
+
+        return groups;
+    }
+
+    void execute() {
+        auto groups = levelize(ops_, start_idx_, ops_.size());
+        // std::cerr << "Groups: [ ";
+        // for(const auto& sz : groups) {
+        //     std::cerr << sz << " ";
+        // }
+        // std::cerr << "]" << std::endl;
+
+        // AtomicCounter* ac = new AtomicCounterGA(ec().pg(), ops_.size() - start_idx_);
+        // ac->allocate(0);
+        
+        size_t off = start_idx_;
+        for(size_t g : groups) {
+            EXPECTS(g > 0);
+            AtomicCounter* ac = new AtomicCounterGA(ec().pg(), g);
+            ac->allocate(0);
+
+            for(size_t i = off; i < off + g; i++, start_idx_++) {
+                ec().set_ac(IndexedAC(ac, i - off));
+                ops_[i]->execute(ec());
+            }
+
+            ec().set_ac(IndexedAC(nullptr, 0));
+            ac->deallocate();
+            delete ac;
+
+            
+            //memory fence. for now GA_Sync()
+            // GA_Sync();
+            //pg.barrier()
+            ec().pg().barrier();
+            off += g;
+        }
+        // ac->deallocate();
+        // delete ac;
+        // // for(auto& op : ops_) { op->execute(ec()->pg()); }
+        // for(size_t i = start_idx_; i < ops_.size(); i++) {
+        //     ops_[i]->execute(ec());
+        //     start_idx_++;
+        // }
     }
 
     template<typename Func, typename... Args>

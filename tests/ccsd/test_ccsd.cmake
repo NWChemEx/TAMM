5d92afa47971b59913516f3a0213724536ed2105
Ajay Panyala <ajay.panyala@gmail.com>
2019-03-24 20:45:05 -0700

    new tgen kernels for ccsd(t)

--- tests/ccsd/test_ccsd.cmake
+++ tests/ccsd/test_ccsd.cmake
@@ -16,8 +16,7 @@ add_mpi_unit_test(Test_CCSD_Spin 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 if(NWX_CUDA)
     set(CCSD_T_SRCDIR ${CMAKE_CURRENT_SOURCE_DIR}/ccsd/ccsd_t)
     set(CCSD_T_CUDA_SRCS 
-        ${CCSD_T_SRCDIR}/memory.cu
-        ${CCSD_T_SRCDIR}/sd_t_total.cu   
+        ${CCSD_T_SRCDIR}/memory.cu  
         ${CCSD_T_SRCDIR}/header.hpp
         ${CCSD_T_SRCDIR}/hybrid.cpp
         ${CCSD_T_SRCDIR}/util_getppn.cpp
@@ -25,5 +24,8 @@ if(NWX_CUDA)
         ${CCSD_T_SRCDIR}/ccsd_t_singles_gpu.hpp
         ${CCSD_T_SRCDIR}/ccsd_t_doubles_gpu.hpp
         )
-    add_mpi_cuda_unit_test(Test_CCSD_T "${CCSD_T_CUDA_SRCS}" 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
+    set(CCSD_T_NWC_SRCS ${CCSD_T_CUDA_SRCS} ${CCSD_T_SRCDIR}/sd_t_total.cu)
+    set(CCSD_T_TGEN_SRCS ${CCSD_T_CUDA_SRCS} ${CCSD_T_SRCDIR}/sd_t_total_new.cu)
+    add_mpi_cuda_unit_test(Test_CCSD_T "${CCSD_T_NWC_SRCS}" 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
+    add_mpi_cuda_unit_test(Test_CCSD_T_tgen "${CCSD_T_TGEN_SRCS}" 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 endif()

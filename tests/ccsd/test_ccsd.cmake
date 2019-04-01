b4cba3b1903dd917a4318bce8b745eae9f949677
Ajay Panyala <ajay.panyala@gmail.com>
2019-03-31 22:07:02 -0700

    minor
    Former-commit-id: ea963aa6a1ab328742e9fba8f0ec6b38c7e1b177
    Former-commit-id: 5efa7cc4a078dcf44a94c9b12c1bb4c71426224f
    Former-commit-id: 553c3d8ae86b730a21bba15c10450594e0b16212
    Former-commit-id: b871e44ef0fc1453166717aefbe29214fe478151 [formerly 36eea9d98717aa37b6cce9de1374d23c05770657]
    Former-commit-id: 55a6e88db4c4ecb2511cf1e930810c2f6fbdb1c4

--- tests/ccsd/test_ccsd.cmake
+++ tests/ccsd/test_ccsd.cmake
@@ -1,7 +1,7 @@
 
 include(TargetMacros)
 # add_mpi_unit_test(Test_CD_SVD 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
-add_mpi_unit_test(Test_CD_SVD_GA 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
+# add_mpi_unit_test(Test_CD_SVD_GA 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 # add_mpi_unit_test(Test_CCSD_CD 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 add_mpi_unit_test(Test_CCSD_CD_RM 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 # add_mpi_unit_test(Test_CCSD_CD_SVD 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
@@ -25,7 +25,13 @@ if(NWX_CUDA)
         ${CCSD_T_SRCDIR}/ccsd_t_doubles_gpu.hpp
         )
     set(CCSD_T_NWC_SRCS ${CCSD_T_CUDA_SRCS} ${CCSD_T_SRCDIR}/sd_t_total.cu)
-    set(CCSD_T_TGEN_SRCS ${CCSD_T_CUDA_SRCS} ${CCSD_T_SRCDIR}/sd_t_total_new.cu)
+    set(CCSD_T_TGEN_SRCS ${CCSD_T_CUDA_SRCS} 
+            ${CCSD_T_SRCDIR}/sd_t_total_new.cu)
+    set(CCSD_T_TGEN_FUSED_SRCS ${CCSD_T_CUDA_SRCS} 
+            ${CCSD_T_SRCDIR}/sd_t_total.cu
+            ${CCSD_T_SRCDIR}/direct_sd1.cu
+            ${CCSD_T_SRCDIR}/direct_sd2.cu)            
     add_mpi_cuda_unit_test(Test_CCSD_T "${CCSD_T_NWC_SRCS}" 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
     add_mpi_cuda_unit_test(Test_CCSD_T_tgen "${CCSD_T_TGEN_SRCS}" 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
+    add_mpi_cuda_unit_test(Test_CCSD_T_tgen_fused "${CCSD_T_TGEN_FUSED_SRCS}" 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 endif()

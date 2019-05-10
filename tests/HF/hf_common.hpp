c8cc55a64f7864a848d6c4ca5f806dda41dbb461
David Williams-Young <dbwy@lbl.gov>
2019-05-09 22:39:31 -0400

    Funneled MPI Safe LAPACK calls

--- tests/HF/hf_common.hpp
+++ tests/HF/hf_common.hpp
@@ -256,28 +256,40 @@ std::tuple<Matrix, Matrix, size_t, double, double> gensqrtinv(
   }
 #else
 
+  auto world = GA_MPI_Comm();
+  int world_rank, world_size;
+  MPI_Comm_rank( world, &world_rank );
+  MPI_Comm_size( world, &world_size );
+
+  int64_t n_cond;
+  double condition_number, result_condition_number;
+  Matrix X, Xinv;
+
+  const int64_t N = S.rows();
+
+  if( world_rank == 0 ) {
+
   // Eigendecompose S -> VsV**T
   Eigen::MatrixXd V = S;
-  const int64_t N = S.rows();
   std::vector<double> s(N);
   linalg::lapack::syevd( 'V', 'L', N, V.data(), N, s.data() );
 
-  const auto condition_number = std::min(
+  condition_number = std::min(
     s.back() / std::max( s.front(), std::numeric_limits<double>::min() ),
     1.       / std::numeric_limits<double>::epsilon()
   );
 
   const auto threshold = s.back() / max_condition_number;
   auto first_above_thresh = std::find_if( s.begin(), s.end(), [&](const auto& x){ return x >= threshold; } );
-  const auto result_condition_number = s.back() / *first_above_thresh;
+  result_condition_number = s.back() / *first_above_thresh;
 
   const int64_t n_illcond = std::distance( s.begin(), first_above_thresh );
-  const int64_t n_cond    = N - n_illcond;
+  n_cond    = N - n_illcond;
 
   assert( n_cond == N ); //TODO: fix
 
   auto* V_cond = V.data() + n_illcond * N;
-  Matrix X( N, n_cond ), Xinv( N, n_cond );
+  X.resize( N, n_cond ); Xinv.resize( N, n_cond );
 
   // Form canonical X/Xinv
   for( auto i = 0; i < n_cond; ++i ) {
@@ -306,6 +318,23 @@ std::tuple<Matrix, Matrix, size_t, double, double> gensqrtinv(
     linalg::blas::gemm( 'N', 'N', N, N, n_cond, 1., V_cond, N, TMP.data(), n_cond, 0., X.data(), N );
 */
 
+  }
+  } // compute on root 
+
+
+  if( world_size > 1 ) {
+
+    // TODO: Should buffer this
+    MPI_Bcast( &n_cond,                  1, MPI_INT64_T, 0, world );
+    MPI_Bcast( &condition_number,        1, MPI_DOUBLE,  0, world );
+    MPI_Bcast( &result_condition_number, 1, MPI_DOUBLE,  0, world );
+
+    if( world_rank != 0 ) {
+      X.resize( N, n_cond ); Xinv.resize(N, n_cond);
+    }
+
+    MPI_Bcast( X.data(),    X.size(),    MPI_DOUBLE, 0, world );
+    MPI_Bcast( Xinv.data(), Xinv.size(), MPI_DOUBLE, 0, world );
   }
 
 #endif

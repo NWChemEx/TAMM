051368d848d998a17775c0b7ee4e743eab1d2e65
Ajay Panyala <ajay.panyala@gmail.com>
2019-09-14 00:28:26 +0000

    [WIP]: closed shell ccsd

--- tests/ccsd/test_ccsd.cmake
+++ tests/ccsd/test_ccsd.cmake
@@ -4,8 +4,9 @@ include(TargetMacros)
 # add_mpi_unit_test(Test_CD_SVD_GA 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 # add_mpi_unit_test(Test_CCSD_CD 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 add_mpi_unit_test(Test_CCSD_CD_RM 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
+add_mpi_unit_test(Test_CCSD_CD_CS 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 # add_mpi_unit_test(Test_CCSD_CD_SVD 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
-add_mpi_unit_test(Test_CCSD_Spin 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
+# add_mpi_unit_test(Test_CCSD_Spin 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 
 if(NWX_CUDA)
     set(CCSD_T_SRCDIR ${CMAKE_CURRENT_SOURCE_DIR}/ccsd/ccsd_t)
@@ -21,10 +22,10 @@ if(NWX_CUDA)
     set(CCSD_T_NWC_SRCS ${CCSD_T_CUDA_SRCS} ${CCSD_T_SRCDIR}/sd_t_total.cu)
     set(CCSD_T_TGEN_SRCS ${CCSD_T_CUDA_SRCS} 
             ${CCSD_T_SRCDIR}/sd_t_total_tgen.cu)
-    # set(CCSD_T_TGEN_FUSED_SRCS ${CCSD_T_CUDA_SRCS} 
-    #         ${CCSD_T_SRCDIR}/sd_t_total.cu
-    #         ${CCSD_T_SRCDIR}/direct_sd1.cu
-    #         ${CCSD_T_SRCDIR}/direct_sd2.cu)  
+    set(CCSD_T_TGEN_FUSED_SRCS ${CCSD_T_CUDA_SRCS} 
+            ${CCSD_T_SRCDIR}/sd_t_total.cu
+            ${CCSD_T_SRCDIR}/direct_sd1.cu
+            ${CCSD_T_SRCDIR}/direct_sd2.cu)  
     set(CCSD_T_TGEN_FUSED_SRCS ${CCSD_T_CUDA_SRCS}  
             ${CCSD_T_SRCDIR}/sd_t_total.cu
             ${CCSD_T_SRCDIR}/ccsd_t_gpu_tgen.hpp

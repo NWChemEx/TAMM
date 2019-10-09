250c7bf6dc62f5a37aaad59aaa52553af699f3ba
Ajay Panyala <ajay.panyala@gmail.com>
2019-10-09 22:34:31 +0000

    changes to sync with updates in cmakebuild

--- tests/methods/cc/test_cc.cmake
+++ tests/methods/cc/test_cc.cmake
@@ -3,7 +3,7 @@ include(TargetMacros)
 add_mpi_unit_test(CD_SVD_GA 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 add_mpi_unit_test(CD_CCSD_CS 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 
-if(NWX_CUDA)
+if(USE_CUDA)
     set(CCSD_T_SRCDIR ${CMAKE_CURRENT_SOURCE_DIR}/methods/cc/ccsd_t)
     set(CCSD_T_CUDA_SRCS 
         ${CCSD_T_SRCDIR}/memory.cu  

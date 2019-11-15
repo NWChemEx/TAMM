2d261a7825efd583744bdbaffc04e371829cb454
Ajay Panyala <ajay.panyala@gmail.com>
2019-11-14 20:38:43 -0800

    merge gadist

--- src/tamm/types.hpp
+++ src/tamm/types.hpp
@@ -85,6 +85,7 @@ using IntLabel = int32_t; //a simple integer label for indices
 using IntLabelVec = std::vector<IntLabel>;
 
 using SizeVec = std::vector<Size>;
+using ProcGrid = std::vector<Proc>;
 
 enum class AllocationStatus { invalid, created, attached, deallocated, orphaned };
 
@@ -213,6 +214,59 @@ namespace internal {
 } // namespace internal
 
 inline Label make_label() { static Label lbl = 0; return lbl++; }
+
+  /**
+   * Convert a TAMM element type to a GA element type
+   * @param eltype TAMM element type
+   * @return Corresponding GA element type
+   */
+  static int to_ga_eltype(ElementType eltype) {
+    int ret;
+    switch(eltype) {
+      case ElementType::single_precision:
+        ret = C_FLOAT;
+        break;
+      case ElementType::double_precision:
+        ret = C_DBL;
+        break;
+      case ElementType::single_complex:
+        ret = C_SCPL;
+        break;
+      case ElementType::double_complex:
+        ret = C_DCPL;
+        break;
+      case ElementType::invalid:
+      default:
+        UNREACHABLE();
+    }
+    return ret;
+  }
+
+  /**
+   * Convert a GA element type to a TAMM element type
+   * @param eltype GA element type
+   * @return Corresponding TAMM element type
+   */
+  static ElementType from_ga_eltype(int eltype) {
+    ElementType ret;
+    switch(eltype) {
+      case C_FLOAT:
+        ret = ElementType::single_precision;
+        break;
+      case C_DBL:
+        ret = ElementType::double_precision;
+        break;
+      case C_SCPL:
+        ret = ElementType::single_complex;
+        break;
+      case C_DCPL:
+        ret = ElementType::double_complex;
+        break;
+      default:
+        UNREACHABLE();
+    }
+    return ret;
+  }
 
 } // namespace tamm
 

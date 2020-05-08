e627c1c8a2d3fda27dda2248a2b6a2628604b0b3
Abhishek Bagusetty <abagusetty@anl.gov>
2020-05-08 13:46:01 -0500

    device interface support for HIP and SYCL
    Former-commit-id: 44f80e5ac42c99c076ab5f9de9d672527e4171f5
    Former-commit-id: fa2f08b7039ec165c70cd6daf58958d37f014e78
    Former-commit-id: 6158a04bdef64385d41f941e3ca532f2c8242bca
    Former-commit-id: e8eb71ef2dde146e8f7375a4829a2cfb4ccd561c [formerly f1e45f7e8a74a952f67942968eba86943f171938]
    Former-commit-id: 5f9f3f43f40c331289bfb0edd67f88baa73eb534

--- methods/cc/cc.cmake
+++ methods/cc/cc.cmake
@@ -6,7 +6,7 @@ add_mpi_unit_test(CD_CCSD_OS 2 "${CMAKE_SOURCE_DIR}/../inputs/h2o.nwx")
 if(USE_CUDA)
     set(CCSD_T_SRCDIR ${CMAKE_CURRENT_SOURCE_DIR}/cc/ccsd_t)
     set(CCSD_T_SRCS 
-        ${CCSD_T_SRCDIR}/memory.cu  
+        ${CCSD_T_SRCDIR}/memory.cpp
         ${CCSD_T_SRCDIR}/ccsd_t_common.hpp
         ${CCSD_T_SRCDIR}/hybrid.cpp
         ${CCSD_T_SRCDIR}/ccsd_t_unfused_driver.hpp

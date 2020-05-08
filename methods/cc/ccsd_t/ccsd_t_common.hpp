44f80e5ac42c99c076ab5f9de9d672527e4171f5
Abhishek Bagusetty <abagusetty@anl.gov>
2020-05-08 13:46:01 -0500

    device interface support for HIP and SYCL

--- methods/cc/ccsd_t/ccsd_t_common.hpp
+++ methods/cc/ccsd_t/ccsd_t_common.hpp
@@ -1,8 +1,15 @@
 #ifndef CCSD_T_COMMON_HPP_
 #define CCSD_T_COMMON_HPP_
 
+#ifdef USE_CUDA
 #include <cuda.h>
 #include <cuda_runtime.h>
+#elif defined(USE_HIP)
+#include <hip.h>
+#include <hip_runtime.h>
+#elif defined(USE_DPCPP)
+#include <CL/sycl.hpp>
+#endif
 
 #include <stdio.h>
 #include <sys/types.h>
@@ -15,16 +22,29 @@
 #endif
 #undef USE_TALSH
 
+#ifdef USE_CUDA
 #define CHECK_ERR(x) { \
     cudaError_t err = cudaGetLastError();\
     if (cudaSuccess != err) { \
         printf("%s\n",cudaGetErrorString(err)); \
         exit(1); \
-    } } 
+    } }
 
 #define CUDA_SAFE(x) if ( cudaSuccess != (x) ) {\
     printf("CUDA CALL FAILED AT LINE %d OF FILE %s error %s\n", __LINE__, __FILE__, cudaGetErrorString(cudaGetLastError()) ); exit(100);}
+#endif // USE_CUDA
 
+#ifdef USE_HIP
+#define CHECK_ERR(x) { \
+    hipError_t err = hipGetLastError();\
+    if (hipSuccess != err) { \
+        printf("%s\n",hipGetErrorString(err)); \
+        exit(1); \
+    } }
+
+#define HIP_SAFE(x) if ( hipSuccess != (x) ) {\
+    printf("HIP CALL FAILED AT LINE %d OF FILE %s error %s\n", __LINE__, __FILE__, hipGetErrorString(hipGetLastError()) ); exit(100);}
+#endif // USE_HIP
 
 typedef long Integer;
 //static int notset;
@@ -33,10 +53,17 @@ typedef long Integer;
 #define TG_MIN(x,y) ((x)<(y)?(x):(y))
 
 void initMemModule();
+
 void *getGpuMem(size_t bytes);
 void *getHostMem(size_t bytes);
 void freeHostMem(void *p);
 void freeGpuMem(void *p);
+#ifdef USE_DPCPP
+static cl::sycl::device& get_current_device() noexcept;
+static cl::sycl::queue& get_current_queue() noexcept;
+static cl::sycl::context& get_current_context() noexcept
+#endif
+
 void finalizeMemModule();
 
 #endif /*CCSD_T_COMMON_HPP_*/

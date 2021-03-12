d501577f4239df49ec96fd0ff16de6fd4e30bad1
Abhishek Bagusetty <abagusetty@anl.gov>
2021-03-12 16:42:47 +0000

    [SYCL] cleanup multi-tile vs single-tile discovery without the use of TAMM_INTEL_ATS macro

--- methods/cc/ccsd_t/ccsd_t_unfused_driver.hpp
+++ methods/cc/ccsd_t/ccsd_t_unfused_driver.hpp
@@ -97,16 +97,16 @@ std::tuple<double,double,double,double> ccsd_t_unfused_driver(ExecutionContext& 
     sycl::platform platform(device_selector);
     auto const& gpu_devices = platform.get_devices();
     for (auto &gpu_device : gpu_devices) {
-	if (gpu_device.is_gpu()) {
-#ifdef TAMM_INTEL_ATS
-            auto SubDevicesDomainNuma = gpu_device.create_sub_devices<sycl::info::partition_property::partition_by_affinity_domain>(sycl::info::partition_affinity_domain::numa);
-            for (const auto &tile : SubDevicesDomainNuma) {
-                dev_count_check++;
-            }
-#else
-            dev_count_check++;
-#endif
-	}
+      if (gpu_devices[i].is_gpu()) {
+        if(gpu_devices[i].get_info<cl::sycl::info::device::partition_max_sub_devices>() > 0) {
+          auto SubDevicesDomainNuma = gpu_devices[i].create_sub_devices<cl::sycl::info::partition_property::partition_by_affinity_domain>(
+            cl::sycl::info::partition_affinity_domain::numa);
+          dev_count_check += SubDevicesDomainNuma.size();
+        }
+        else {
+          dev_count_check++;
+        }
+      }
     }
     if(dev_count_check < iDevice) {
       if(nodezero) cout << "ERROR: Please check whether you have " << iDevice <<
@@ -118,11 +118,6 @@ std::tuple<double,double,double,double> ccsd_t_unfused_driver(ExecutionContext& 
                      "Terminating program..." << endl << endl;
       return std::make_tuple(-999,-999,0,0);
     }
-    // else if (dev_count_check > 1) {
-    //   if(nodezero) cout << "ERROR: TODO multi-device per node support for SYCL is not yet supported. "
-    //                     << " Terminating program..." << endl << endl;
-    //   return std::make_tuple(-999,-999,0,0);
-    // }
   }
 #else
   iDevice = 0;

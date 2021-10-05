05b7265a003244f5308f23c5a825eeaf30ab4848
Abhishek Bagusetty <abagusetty@anl.gov>
2021-10-05 21:38:58 +0000

    [SYCL] simple cleanup for SYCL1.2.1 deprecations

--- src/tamm/execution_context.cpp
+++ src/tamm/execution_context.cpp
@@ -16,7 +16,7 @@ auto sycl_asynchandler = [] (sycl::exception_list exceptions) {
             std::rethrow_exception(e);
         } catch (sycl::exception const& ex) {
             std::cout << "Caught asynchronous SYCL exception:" << std::endl
-            << ex.what() << ", OpenCL code: " << ex.get_cl_code() << std::endl;
+            << ex.what() << ", SYCL code: " << ex.code() << std::endl;
         }
     }
 };
@@ -61,14 +61,13 @@ ExecutionContext::ExecutionContext(ProcGroup pg, DistributionKind default_dist_k
 #endif
 
 #if defined(USE_DPCPP)
-  sycl::gpu_selector device_selector;
-  sycl::platform platform(device_selector);
+  sycl::platform platform(sycl::gpu_selector{});
   auto const& gpu_devices = platform.get_devices();
   for (int i = 0; i < gpu_devices.size(); i++) {
     if (gpu_devices[i].is_gpu()) {
-       if(gpu_devices[i].get_info<cl::sycl::info::device::partition_max_sub_devices>() > 0) {
-          auto SubDevicesDomainNuma = gpu_devices[i].create_sub_devices<cl::sycl::info::partition_property::partition_by_affinity_domain>(
-           cl::sycl::info::partition_affinity_domain::numa);
+       if(gpu_devices[i].get_info<sycl::info::device::partition_max_sub_devices>() > 0) {
+          auto SubDevicesDomainNuma = gpu_devices[i].create_sub_devices<sycl::info::partition_property::partition_by_affinity_domain>(
+           sycl::info::partition_affinity_domain::numa);
 	        ngpu_ += SubDevicesDomainNuma.size();
        }
        else {
@@ -92,9 +91,9 @@ ExecutionContext::ExecutionContext(ProcGroup pg, DistributionKind default_dist_k
   }  
   for (int i = 0; i < gpu_devices.size(); i++) {
     if (gpu_devices[i].is_gpu()) {
-       if(gpu_devices[i].get_info<cl::sycl::info::device::partition_max_sub_devices>() > 0) {
-         auto SubDevicesDomainNuma = gpu_devices[i].create_sub_devices<cl::sycl::info::partition_property::partition_by_affinity_domain>(
-           cl::sycl::info::partition_affinity_domain::numa);
+       if(gpu_devices[i].get_info<sycl::info::device::partition_max_sub_devices>() > 0) {
+         auto SubDevicesDomainNuma = gpu_devices[i].create_sub_devices<sycl::info::partition_property::partition_by_affinity_domain>(
+           sycl::info::partition_affinity_domain::numa);
          for (const auto &tile : SubDevicesDomainNuma) {
            vec_syclQue.push_back( new sycl::queue(tile, sycl_asynchandler,
                                                   sycl::property_list{sycl::property::queue::in_order{}}) );

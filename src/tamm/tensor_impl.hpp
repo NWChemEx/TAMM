c0cadbe537751818f824deb55c0f3b9c661daae3
jglabe <jglabe@gmail.com>
2022-11-15 08:32:05 -0800

    initial sparse_tensor branch commit

--- src/tamm/tensor_impl.hpp
+++ src/tamm/tensor_impl.hpp
@@ -510,6 +510,470 @@ protected:
 
 /**
  * @ingroup tensors
+ * @brief Implementation class for the Sparse versin of the TensorBase class
+ *
+ * @tparam T Element type of Tensor
+ */
+template<typename T>
+class SparseTensorImpl: public TensorBase {
+  public:
+  using TensorBase::TensorBase;
+  // Ctors
+  SparseTensorImpl() = default;
+
+  /**
+   * @brief Construct a new SparseTensorImpl object using a vector of
+   * TiledIndexSpace objects for each mode of the tensor
+   *
+   * @param [in] tis vector of TiledIndexSpace objects for each
+   * mode used to construct the tensor
+   */
+  SparseTensorImpl(const TiledIndexSpaceVec& tis): TensorBase{tis} {
+    has_spin_symmetry_    = false;
+    has_spatial_symmetry_ = false;
+    kind_                 = TensorBase::TensorKind::normal;
+  }
+
+  /**
+   * @brief Construct a new SparseTensorImpl object using a vector of
+   * TiledIndexSpace objects for each mode of the tensor
+   *
+   * @param [in] lbls vector of tiled index labels used for extracting
+   * corresponding TiledIndexSpace objects for each mode used to construct
+   * the tensor
+   */
+  SparseTensorImpl(const std::vector<TiledIndexLabel>& lbls): TensorBase{lbls} {
+    has_spin_symmetry_    = false;
+    has_spatial_symmetry_ = false;
+    kind_                 = TensorBase::TensorKind::normal;
+  }
+
+  /**
+   * @brief Construct a new TensorBase object recursively with a set of
+   * TiledIndexSpace objects followed by a lambda expression
+   *
+   * @tparam Ts variadic template for rest of the arguments
+   * @param [in] tis TiledIndexSpace object used as a mode
+   * @param [in] rest remaining part of the arguments
+   */
+  template<class... Ts>
+  SparseTensorImpl(const TiledIndexSpace& tis, Ts... rest): TensorBase{tis, rest...} {
+    num_modes_ = block_indices_.size();
+    construct_dep_map();
+    has_spin_symmetry_    = false;
+    has_spatial_symmetry_ = false;
+    kind_                 = TensorBase::TensorKind::normal;
+  }
+
+  // SpinTensor related constructors
+  /**
+   * @brief Construct a new SpinTensorImpl object using set of TiledIndexSpace
+   * objects and Spin attribute mask
+   *
+   * @param [in] t_spaces
+   * @param [in] spin_mask
+   */
+  SparseTensorImpl(TiledIndexSpaceVec t_spaces, SpinMask spin_mask): TensorBase(t_spaces) {
+    EXPECTS(t_spaces.size() == spin_mask.size());
+
+    // for(const auto& tis : t_spaces) { EXPECTS(tis.has_spin()); }
+
+    spin_mask_            = spin_mask;
+    has_spin_symmetry_    = true;
+    has_spatial_symmetry_ = false;
+    // spin_total_        = calculate_spin();
+    kind_ = TensorBase::TensorKind::spin;
+  }
+
+  /**
+   * @brief Construct a new SpinTensorImpl object using set of TiledIndexLabel
+   * objects and Spin attribute mask
+   *
+   * @param [in] t_labels
+   * @param [in] spin_mask
+   */
+  SparseTensorImpl(IndexLabelVec t_labels, SpinMask spin_mask): TensorBase(t_labels) {
+    EXPECTS(t_labels.size() == spin_mask.size());
+    // for(const auto& tlbl : t_labels) {
+    //     EXPECTS(tlbl.tiled_index_space().has_spin());
+    // }
+    spin_mask_            = spin_mask;
+    has_spin_symmetry_    = true;
+    has_spatial_symmetry_ = false;
+    // spin_total_        = calculate_spin();
+    kind_ = TensorBase::TensorKind::spin;
+  }
+
+  /**
+   * @brief Construct a new SpinTensorImpl object using set of TiledIndexSpace
+   * objects and Spin attribute mask
+   *
+   * @param [in] t_spaces
+   * @param [in] spin_mask
+   */
+  SparseTensorImpl(TiledIndexSpaceVec t_spaces, std::vector<size_t> spin_sizes): TensorBase(t_spaces) {
+    // EXPECTS(t_spaces.size() == spin_mask.size());
+    int spin_size_sum = std::accumulate(spin_sizes.begin(), spin_sizes.end(), 0);
+    EXPECTS(t_spaces.size() >= spin_size_sum);
+
+    EXPECTS(spin_sizes.size() > 0);
+    // for(const auto& tis : t_spaces) { EXPECTS(tis.has_spin()); }
+    SpinMask spin_mask;
+    size_t   upper  = spin_sizes[0];
+    size_t   lower  = spin_sizes.size() > 1 ? spin_sizes[1] : t_spaces.size() - upper;
+    size_t   ignore = spin_sizes.size() > 2 ? spin_sizes[1] : t_spaces.size() - (upper + lower);
+
+    for(size_t i = 0; i < upper; i++) { spin_mask.push_back(SpinPosition::upper); }
+
+    for(size_t i = 0; i < lower; i++) { spin_mask.push_back(SpinPosition::lower); }
+
+    for(size_t i = 0; i < ignore; i++) { spin_mask.push_back(SpinPosition::ignore); }
+
+    spin_mask_            = spin_mask;
+    has_spin_symmetry_    = true;
+    has_spatial_symmetry_ = false;
+    // spin_total_        = calculate_spin();
+    kind_ = TensorBase::TensorKind::spin;
+  }
+
+  /**
+   * @brief Construct a new SpinTensorImpl object using set of TiledIndexLabel
+   * objects and Spin attribute mask
+   *
+   * @param [in] t_labels
+   * @param [in] spin_mask
+   */
+  SparseTensorImpl(IndexLabelVec t_labels, std::vector<size_t> spin_sizes): TensorBase(t_labels) {
+    // EXPECTS(t_labels.size() == spin_mask.size());
+    int spin_size_sum = std::accumulate(spin_sizes.begin(), spin_sizes.end(), 0);
+    EXPECTS(t_labels.size() >= spin_size_sum);
+    EXPECTS(spin_sizes.size() > 0);
+    // for(const auto& tlbl : t_labels) {
+    //     EXPECTS(tlbl.tiled_index_space().has_spin());
+    // }
+
+    SpinMask spin_mask;
+    size_t   upper  = spin_sizes[0];
+    size_t   lower  = spin_sizes.size() > 1 ? spin_sizes[1] : t_labels.size() - upper;
+    size_t   ignore = spin_sizes.size() > 2 ? spin_sizes[1] : t_labels.size() - (upper + lower);
+
+    for(size_t i = 0; i < upper; i++) { spin_mask.push_back(SpinPosition::upper); }
+
+    for(size_t i = 0; i < lower; i++) { spin_mask.push_back(SpinPosition::lower); }
+
+    for(size_t i = 0; i < ignore; i++) { spin_mask.push_back(SpinPosition::ignore); }
+
+    spin_mask_            = spin_mask;
+    has_spin_symmetry_    = true;
+    has_spatial_symmetry_ = false;
+    // spin_total_        = calculate_spin();
+    kind_ = TensorBase::TensorKind::spin;
+  }
+
+  // Copy/Move Ctors and Assignment Operators
+  SparseTensorImpl(SparseTensorImpl&&)                 = default;
+  SparseTensorImpl(const SparseTensorImpl&)            = delete;
+  SparseTensorImpl& operator=(SparseTensorImpl&&)      = default;
+  SparseTensorImpl& operator=(const SparseTensorImpl&) = delete;
+
+  // Dtor
+  ~SparseTensorImpl() {
+    if(mpb_ != nullptr) { mpb_->allocation_status_ = AllocationStatus::orphaned; }
+  }
+
+  /**
+   * @brief Virtual method for deallocating a Tensor
+   *
+   */
+  virtual void deallocate() {
+    EXPECTS(allocation_status_ == AllocationStatus::created);
+    EXPECTS(mpb_);
+    ec_->unregister_for_dealloc(mpb_);
+    mpb_->dealloc_coll();
+    delete mpb_;
+    mpb_ = nullptr;
+    update_status(AllocationStatus::deallocated);
+  }
+
+  /**
+   * @brief Virtual method for allocating a Tensor using an ExecutionContext
+   *
+   * @param [in] ec ExecutionContext to be used for allocation
+   */
+  virtual void allocate(ExecutionContext* ec) {
+    {
+      EXPECTS(allocation_status_ == AllocationStatus::invalid);
+      auto          defd = ec->get_default_distribution();
+      Distribution* distribution =
+        ec->distribution(defd->get_tensor_base(), defd->get_dist_proc()); // defd->kind());
+      // Distribution* distribution    =
+      // ec->distribution(defd.tensor_base(), nproc );
+#if defined(USE_UPCXX_DISTARRAY) && defined(USE_UPCXX)
+      MemoryManager* memory_manager = ec->memory_manager(ec->hint());
+#else
+      MemoryManager* memory_manager = ec->memory_manager();
+#endif
+      EXPECTS(distribution != nullptr);
+      EXPECTS(memory_manager != nullptr);
+      ec_ = ec;
+      // distribution_ =
+      // DistributionFactory::make_distribution(*distribution, this,
+      // pg.size());
+      {
+        distribution_ =
+          std::shared_ptr<Distribution>(distribution->clone(this, memory_manager->pg().size()));
+      }
+#if 0
+        auto rank = memory_manager->pg().rank();
+        auto buf_size = distribution_->buf_size(rank);
+        auto eltype = tensor_element_type<T>();
+        EXPECTS(buf_size >= 0);
+        mpb_ = memory_manager->alloc_coll(eltype, buf_size);
+#else
+      auto           eltype         = tensor_element_type<T>();
+      if(proc_list_.size() > 0)
+        mpb_ = memory_manager->alloc_coll_balanced(eltype, distribution_->max_proc_buf_size(),
+                                                   proc_list_);
+      else mpb_ = memory_manager->alloc_coll_balanced(eltype, distribution_->max_proc_buf_size());
+
+#endif
+      EXPECTS(mpb_ != nullptr);
+      ec_->register_for_dealloc(mpb_);
+      update_status(AllocationStatus::created);
+    }
+  }
+
+  virtual const Distribution& distribution() const { return *distribution_.get(); }
+
+  // Tensor Accessors
+  /**
+   * @brief Tensor accessor method for getting values from a set of
+   * indices to specified memory span
+   *
+   * @tparam T type of the values hold on the tensor object
+   * @param [in] idx_vec a vector of indices to fetch the values
+   * @param [in] buff_span memory span where to put the fetched values
+   */
+
+  virtual void get(const IndexVector& idx_vec, span<T> buff_span) const {
+    EXPECTS(allocation_status_ != AllocationStatus::invalid);
+
+    if(!is_non_zero(idx_vec)) {
+      Size size = block_size(idx_vec);
+      EXPECTS(size <= buff_span.size());
+      for(size_t i = 0; i < size; i++) { buff_span[i] = (T) 0; }
+      return;
+    }
+
+    Proc   proc;
+    Offset offset;
+    std::tie(proc, offset) = distribution_->locate(idx_vec);
+    Size size              = block_size(idx_vec);
+    EXPECTS(size <= buff_span.size());
+    mpb_->mgr().get(*mpb_, proc, offset, Size{size}, buff_span.data());
+  }
+
+  /**
+   * @brief Tensor accessor method for getting values in nonblocking fashion
+   * from a set of indices to specified memory span
+   *
+   * @tparam T type of the values hold on the tensor object
+   * @param [in] idx_vec a vector of indices to fetch the values
+   * @param [in] buff_span memory span where to put the fetched values
+   */
+
+  virtual void nb_get(const IndexVector& idx_vec, span<T> buff_span,
+                      DataCommunicationHandlePtr data_comm_handle) const {
+    EXPECTS(allocation_status_ != AllocationStatus::invalid);
+
+    if(!is_non_zero(idx_vec)) {
+      Size size = block_size(idx_vec);
+      EXPECTS(size <= buff_span.size());
+      for(size_t i = 0; i < size; i++) { buff_span[i] = (T) 0; }
+      return;
+    }
+
+    Proc   proc;
+    Offset offset;
+    std::tie(proc, offset) = distribution_->locate(idx_vec);
+    Size size              = block_size(idx_vec);
+    EXPECTS(size <= buff_span.size());
+    mpb_->mgr().nb_get(*mpb_, proc, offset, Size{size}, buff_span.data(), data_comm_handle);
+  }
+
+  /**
+   * @brief Tensor accessor method for putting values to a set of indices
+   * with the specified memory span
+   *
+   * @tparam T type of the values hold on the tensor object
+   * @param [in] idx_vec a vector of indices to put the values
+   * @param [in] buff_span buff_span memory span for the values to put
+   */
+
+  virtual void put(const IndexVector& idx_vec, span<T> buff_span) {
+    EXPECTS(allocation_status_ != AllocationStatus::invalid);
+
+    if(!is_non_zero(idx_vec)) { return; }
+
+    Proc   proc;
+    Offset offset;
+    std::tie(proc, offset) = distribution_->locate(idx_vec);
+    Size size              = block_size(idx_vec);
+    EXPECTS(size <= buff_span.size());
+    mpb_->mgr().put(*mpb_, proc, offset, Size{size}, buff_span.data());
+  }
+
+  /**
+   * @brief Tensor accessor method for putting values in nonblocking fashion
+   * to a set of indices with the specified memory span
+   *
+   * @tparam T type of the values hold on the tensor object
+   * @param [in] idx_vec a vector of indices to put the values
+   * @param [in] buff_span buff_span memory span for the values to put
+   */
+
+  virtual void nb_put(const IndexVector& idx_vec, span<T> buff_span,
+                      DataCommunicationHandlePtr data_comm_handle) {
+    EXPECTS(allocation_status_ != AllocationStatus::invalid);
+
+    if(!is_non_zero(idx_vec)) { return; }
+
+    Proc   proc;
+    Offset offset;
+    std::tie(proc, offset) = distribution_->locate(idx_vec);
+    Size size              = block_size(idx_vec);
+    EXPECTS(size <= buff_span.size());
+    mpb_->mgr().nb_put(*mpb_, proc, offset, Size{size}, buff_span.data(), data_comm_handle);
+  }
+
+  /**
+   * @brief Tensor accessor method for adding svalues to a set of indices
+   * with the specified memory span
+   *
+   * @tparam T type of the values hold on the tensor object
+   * @param [in] idx_vec a vector of indices to put the values
+   * @param [in] buff_span buff_span memory span for the values to put
+   */
+
+  virtual void add(const IndexVector& idx_vec, span<T> buff_span) {
+    EXPECTS(allocation_status_ != AllocationStatus::invalid);
+
+    if(!is_non_zero(idx_vec)) { return; }
+
+    Proc   proc;
+    Offset offset;
+    std::tie(proc, offset) = distribution_->locate(idx_vec);
+    Size size              = block_size(idx_vec);
+    EXPECTS(size <= buff_span.size());
+    mpb_->mgr().add(*mpb_, proc, offset, Size{size}, buff_span.data());
+  }
+
+  /**
+   * @brief Tensor accessor method for adding svalues in nonblocking fashion
+   * to a set of indices with the specified memory span
+   *
+   * @tparam T type of the values hold on the tensor object
+   * @param [in] idx_vec a vector of indices to put the values
+   * @param [in] buff_span buff_span memory span for the values to put
+   */
+
+  virtual void nb_add(const IndexVector& idx_vec, span<T> buff_span,
+                      DataCommunicationHandlePtr data_comm_handle) {
+    EXPECTS(allocation_status_ != AllocationStatus::invalid);
+
+    if(!is_non_zero(idx_vec)) { return; }
+
+    Proc   proc;
+    Offset offset;
+    std::tie(proc, offset) = distribution_->locate(idx_vec);
+    Size size              = block_size(idx_vec);
+    EXPECTS(size <= buff_span.size());
+    mpb_->mgr().nb_add(*mpb_, proc, offset, Size{size}, buff_span.data(), data_comm_handle);
+  }
+
+  /**
+   * @brief Virtual method for getting the diagonal values in a Tensor
+   *
+   * @returns a vector with the diagonal values
+   * @warning available for tensors with 2 modes
+   */
+  // virtual std::vector<T> diagonal() {
+  //     EXPECTS(num_modes() == 2);
+  //     std::vector<T> dest;
+  //     for(const IndexVector& blockid : loop_nest()) {
+  //         if(blockid[0] == blockid[1]) {
+  //             const TAMM_SIZE size = block_size(blockid);
+  //             std::vector<T> buf(size);
+  //             get(blockid, buf);
+  //             auto block_dims1  = block_dims(blockid);
+  //             auto block_offset = block_offsets(blockid);
+  //             auto dim          = block_dims1[0];
+  //             auto offset       = block_offset[0];
+  //             size_t i          = 0;
+  //             for(auto p = offset; p < offset + dim; p++, i++) {
+  //                 dest.push_back(buf[i * dim + i]);
+  //             }
+  //         }
+  //     }
+  //     return dest;
+  // }
+
+#if !defined(USE_UPCXX)
+  virtual int ga_handle() {
+    const MemoryRegionGA& mr = static_cast<const MemoryRegionGA&>(*mpb_);
+    return mr.ga();
+  }
+#endif
+
+  virtual void put_raw(int64_t* lo, int64_t* hi, void* buf, int64_t* buf_ld) { abort(); }
+
+  virtual void get_raw(int64_t* lo, int64_t* hi, void* buf, int64_t* buf_ld) { abort(); }
+
+  virtual T* access_local_buf() {
+    EXPECTS(mpb_);
+    return static_cast<T*>(mpb_->mgr().access(*mpb_, Offset{0}));
+  }
+
+  virtual const T* access_local_buf() const {
+    EXPECTS(mpb_);
+    return static_cast<T*>(mpb_->mgr().access(*mpb_, Offset{0}));
+  }
+
+  virtual size_t local_buf_size() const {
+    EXPECTS(mpb_);
+    return mpb_->local_nelements().value();
+  }
+
+  virtual size_t total_buf_size(Proc proc) const {
+    EXPECTS(distribution_);
+    return distribution_->buf_size(proc).value();
+  }
+
+  MemoryRegion* memory_region() const {
+    EXPECTS(mpb_);
+    return mpb_;
+  }
+
+  virtual int64_t size() const {
+    EXPECTS(distribution_ != nullptr);
+    return distribution_->total_size().value();
+  }
+
+  bool is_allocated() const { return (allocation_status_ == AllocationStatus::created); }
+
+  void set_proc_list(const ProcList& proc_list) { proc_list_ = proc_list; }
+
+  virtual bool is_block_cyclic() { return false; }
+
+protected:
+  std::shared_ptr<Distribution> distribution_;  /**< shared pointer to associated Distribution */
+  MemoryRegion*                 mpb_ = nullptr; /**< Raw pointer memory region (default null) */
+  ProcList                      proc_list_ = {};
+
+}; // SparseTensorImpl
+
+/**
+ * @ingroup tensors
  * @brief Implementation class for TensorBase with Lambda function construction
  *
  * @tparam T Element type of Tensor

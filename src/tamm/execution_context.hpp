50fed5d616a9992f38eecff1636d0eab3f1dfb84
Abhishek Bagusetty <59661409+abagusetty@users.noreply.github.com>
2024-04-10 15:06:04 -0500

    Update execution_context.hpp

--- src/tamm/execution_context.hpp
+++ src/tamm/execution_context.hpp
@@ -359,25 +359,6 @@ public:
 
   meminfo mem_info() const { return minfo_; }
 
-  std::string getHostName() {
-    char         CPUBrandString[0x40];
-    unsigned int CPUInfo[4] = {0, 0, 0, 0};
-
-    __cpuid(0x80000000, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
-    unsigned int nExIds = CPUInfo[0];
-
-    memset(CPUBrandString, 0, sizeof(CPUBrandString));
-
-    for(unsigned int i = 0x80000000; i <= nExIds; ++i) {
-      __cpuid(i, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
-
-      if(i == 0x80000002) memcpy(CPUBrandString, CPUInfo, sizeof(CPUInfo));
-      else if(i == 0x80000003) memcpy(CPUBrandString + 16, CPUInfo, sizeof(CPUInfo));
-      else if(i == 0x80000004) memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
-    }
-    return CPUBrandString;
-  }
-
   void print_mem_info() {
     if(pg_.rank() != 0) return;
     std::cout << "Memory information" << std::endl;

75f86bd329a80433057e407f82aaa80ff5e1768e
Ajay Panyala <ajay.panyala@gmail.com>
2024-07-01 10:36:43 -0700

    minor

--- src/tamm/execution_context.hpp
+++ src/tamm/execution_context.hpp
@@ -380,7 +380,7 @@ public:
     std::string pheader = "ID;Level;OP;total_op_time_min;total_op_time_max;total_op_time_avg;";
     pheader += "get_time_min;get_time_max;get_time_avg;";
     pheader += "gemm_time_min;gemm_time_max;gemm_time_avg;";
-    pheader += "copy_time_min;copy_time_max; copy_time_avg;";
+    pheader += "copy_time_min;copy_time_max;copy_time_avg;";
     pheader += "acc_time_min;acc_time_max;acc_time_avg";
     return pheader;
   }
